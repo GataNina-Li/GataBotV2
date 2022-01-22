dc0c00a9950f3c6f787bf6ea9f2cfcb904e3336e
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-21 21:48:46 -0600

    Update enphoto.js

--- plugins/enphoto.js
+++ plugins/enphoto.js
@@ -1,17 +1,17 @@
 let fetch = require('node-fetch')
 let split = '|'
 let handler = async (m, { conn, args: [effect], text: txt }) => {
-  let { effects } = await (await (fetch(global.API('xteam', '/textpro')))).json()
-  if (!effect) throw '*[❗] Lista de efectos disponibles*\n\n*Ejemplo de uso:*\n*#logos neon Shadow*\n\n' + effects.sort((a, b) => a - b).join('\n')
+  let { effects } = await (await (fetch(global.API('xteam', '/ephoto')))).json()
+  if (!effect) throw '*List Effect*\n\n' + effects.sort((a, b) => a - b).join('\n')
   effect = effect.toLowerCase()
-  if (!effect in effects) throw `*[❗] El efecto ${effect} no se encuentra en la base de datos*`
-  let [text, ...text2] = txt.replace(effect, '').trimStart().split(split)
-  text2 = text2.join(split)
-  let url = global.API('xteam', '/textpro/' + effect, { text, text2 }, 'APIKEY')
-  await conn.sendFile(m.chat, url, 'Error.jpg', `*DISEÑO TERMINADO*\n*Efecto:* ${effect}`, m)
+  if (!effect in effects) throw `Efek *${effect}* tidak ditemukan`
+  let [text, text2, ...text3] = txt.replace(effect, '').trimStart().split(split)
+  text3 = text3.join(split)
+  let url = global.API('xteam', '/ephoto/' + effect, { text, text2, text3 }, 'APIKEY')
+  await conn.sendFile(m.chat, url, 'ephoto.jpg', `*ENPHOTO360*\n*Effect:* ${effect}`, m)
 }
-handler.help = ['textpro'].map(v => v + ' <effect> <text>|[text2]')
+handler.help = ['enphoto'].map(v => v + ' <effect> <text>|[text2]|[text3]')
 handler.tags = ['tools']
-handler.command = /^(logos)$/i
+handler.command = /^(en?photo(360)?)$/i
 
 module.exports = handler

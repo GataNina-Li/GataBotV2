2de09f1ad407507fede5d0ce53d6fc09242e77a5
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-21 23:12:41 -0600

    ©ShadowBot

---
+++ plugins/hola.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/bot.mp3'
+conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.command = /^(hola|ola)$/i
+
+module.exports = handler

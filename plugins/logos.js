1b87efdc1322687c67d083125629c7108acc6cf0
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-21 22:33:20 -0600

    ©ShadowBot

--- plugins/logos.js
+++ plugins/logos.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let split = '|'
 let handler = async (m, { conn, args: [effect], text: txt }) => {
   let { effects } = await (await (fetch(global.API('xteam', '/textpro')))).json()
-  if (!effect) throw '*[❗] Lista de efectos disponibles*\n\n*Ejemplo de uso:*\n*#logos neon Shadow*\n\n*[ ⚠ ️] Si en algun momento hace falta texto en la imagen prueba agregando otro texto con la division |*\n*Ejemplo:*\n*#logos wolflogogalaxy Shadow|Brokers*\n\n' + effects.sort((a, b) => a - b).join('\n')
+  if (!effect) throw '*[❗] Lista de efectos disponibles*\n\n*Ejemplo de uso:*\n*#logo neon Shadow*\n\n*[ ⚠ ️] Si en algun momento hace falta texto en la imagen prueba agregando otro texto con la division |*\n*Ejemplo:*\n*#logo wolflogogalaxy Shadow|Brokers*\n\n' + effects.sort((a, b) => a - b).join('\n')
   effect = effect.toLowerCase()
   if (!effect in effects) throw `*[❗] El efecto ${effect} no se encuentra en la base de datos*`
   let [text, ...text2] = txt.replace(effect, '').trimStart().split(split)
@@ -12,6 +12,6 @@ let handler = async (m, { conn, args: [effect], text: txt }) => {
 }
 handler.help = ['textpro'].map(v => v + ' <effect> <text>|[text2]')
 handler.tags = ['tools']
-handler.command = /^(logos)$/i
+handler.command = /^(logos|logo)$/i
 
 module.exports = handler

5a9f14a812c947a17ded0665d4451412b21c81bf
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-21 20:30:58 -0600

    Update textpro.js

--- plugins/textpro.js
+++ plugins/textpro.js
@@ -2,13 +2,13 @@ let fetch = require('node-fetch')
 let split = '|'
 let handler = async (m, { conn, args: [effect], text: txt }) => {
   let { effects } = await (await (fetch(global.API('xteam', '/textpro')))).json()
-  if (!effect) throw '*List Effect*\n\n' + effects.sort((a, b) => a - b).join('\n')
+  if (!effect) throw '*Lista de efectos disponibles*\n\n' + effects.sort((a, b) => a - b).join('\n')
   effect = effect.toLowerCase()
-  if (!effect in effects) throw `Efek *${effect}* tidak ditemukan`
+  if (!effect in effects) throw `*[❗] El efecto ${effect} no se encuentra en la base de datos*`
   let [text, ...text2] = txt.replace(effect, '').trimStart().split(split)
   text2 = text2.join(split)
   let url = global.API('xteam', '/textpro/' + effect, { text, text2 }, 'APIKEY')
-  await conn.sendFile(m.chat, url, 'textpro.jpg', `*TEXTPRO*\n*Effect:* ${effect}`, m)
+  await conn.sendFile(m.chat, url, 'Error.jpg', `*DISEÑO TERMINADO*\n*Efecto:* ${effect}`, m)
 }
 handler.help = ['textpro'].map(v => v + ' <effect> <text>|[text2]')
 handler.tags = ['tools']

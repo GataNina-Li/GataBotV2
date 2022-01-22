03a4905d1b5bc6df7d1a44d2d371690ac32c29bf
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-21 22:40:29 -0600

    ©ShadowBot

---
+++ plugins/setdesc.js
@@ -0,0 +1,18 @@
+let handler = async (m, { conn, args }) => {
+ await conn.groupUpdateDescription(m.chat, `${args.join(" ")}`);
+  m.reply('*✅ La descripción del grupo se modifico correctamente*')
+}
+
+handler.help = ['Setdesk <text>']
+handler.tags = ['group']
+handler.command = /^setdesk|setdesc$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = true
+handler.private = false
+handler.register = false
+handler.admin = true
+handler.botAdmin = true
+
+module.exports = handler

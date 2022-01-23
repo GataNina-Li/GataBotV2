4d3007d1d5db95129322de945f5f2878f9b5b5f1
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-23 14:37:42 -0600

    ©ShadowBot

---
+++ plugins/sponsor.js
@@ -0,0 +1,20 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+let info = `
+*------ 𝐁𝚬𝚬𝐓•𝚴𝚯𝐃𝐄𝐒 ------*
+*=> ¿ℚ𝕦𝕚𝕖𝕣𝕖𝕤 𝕥𝕖𝕟𝕖𝕣 𝕦𝕟 𝔹𝕠𝕥 𝕒𝕔𝕥𝕚𝕧𝕒𝕕𝕠 𝟚𝟟/𝟟 𝕪 𝕤𝕚𝕟 𝕦𝕥𝕚𝕝𝕚𝕫𝕒𝕣 𝕥𝕖𝕣𝕞𝕦𝕩?* 
+*➤ Beet-Nodes viene para quedarse!! Beet-Nodes es un servidor/host que mantendrá tu Bot activado 24/7 y sin la necesidad de que este viendo que no se apague, con una interfaz muy sencilla, precios accesibles y un soporte excelente podrás activar cualquier Bot para tenerlo a tu disposición* 
+
+*➤ Para mas informacion contacta a Hiro al wa.me/447309247974*
+*➤ ¿Necesitas ayuda? unete a su grupo OFC https://chat.whatsapp.com/KYGR5bIPuMA3tL9OzjMWmN*
+
+_Coᴘʏʀɪɢʜᴛ 2021-2022_
+_Cᴏɴғᴜ ᴘʀᴏᴘɪᴇᴅᴀᴅ ᴅᴇ ʜɪʀᴏ_
+_Hɪʀᴏ ᴘʀᴏᴘɪᴇᴅᴀᴅ ᴅᴇ ᴄᴏɴғᴜ_
+_Tᴏᴅᴏs ʟᴏs ᴅᴇʀᴇᴄʜᴏs ʀᴇsᴇʀᴠᴀᴅᴏs_
+`.trim() 
+
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*🔥 BEET-NODES 🔥*', 'status@broadcast')
+}
+handler.command = /^(sponsor|patrocinador|publicidad|beetnodes|beet-nodes)$/i
+
+module.exports = handler

72a7c19c09ece129293dc9cb8f64c39129d0d407
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-24 10:18:03 -0600

    ©ShadowBot

--- plugins/hidetag 2.js
+++ plugins/hidetag 2.js
@@ -24,7 +24,7 @@ let handler = async (m, { conn, text, participants }) => {
 }
 handler.help = ['pengumuman', 'announce', 'hidetag'].map(v => v + ' [teks]')
 handler.tags = ['group']
-handler.command = /^(noti|notify)$/i
+handler.command = /^(opengumuman|oannounce|ohiddentag|hidetag|notificar|noti|notify)$/i
 
 handler.group = true
 handler.admin = false

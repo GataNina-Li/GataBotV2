4756d772dd7e1400a34b94fb13cbd2f43979e38c
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-24 10:37:52 -0600

    ©ShadowBot

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function start(file) {
   })
   p.on('exit', code => {
     isRunning = false
-    console.error('Exited with code:', code)
+    console.error('Ocurrio un error inesperado:', code)
     if (code === 0) return
     fs.watchFile(args[0], () => {
       fs.unwatchFile(args[0])

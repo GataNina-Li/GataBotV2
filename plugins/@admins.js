efcbbab61b1348393865d09e642ce0c9621f5ebb
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-24 20:54:36 -0600

    ©ShadowBot

---
+++ plugins/@admins.js
@@ -0,0 +1,38 @@
+let handler = async (m, { conn, participants, groupMetadata, args }) => {
+
+    const getGroupAdmins = (participants) => {
+        admins = []
+        for (let i of participants) {
+            i.isAdmin ? admins.push(i.jid) : ''
+        }
+        return admins
+    }
+
+    let pp = './src/admins.jpg'
+    try {
+        pp = await conn.getProfilePicture(m.chat)
+    } catch (e) {
+    } finally {
+        //let { isBanned, welcome, antivirtex, detect, sWelcome, sBye, sPromote, sDemote, antiLink } = global.DATABASE.data.chats[m.chat]
+        const groupAdmins = getGroupAdmins(participants)
+        let pesan = args.join` `
+        let oi = `Mensaje: ${pesan}`
+        let listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.split('@')[0]}`).join('\n')
+        let text = `
+━「 Invocando admins 」━
+
+${oi}
+${listAdmin}
+
+[ ⚠ ️] Solo usar este comando cuando se trate de una emergencia
+`.trim()
+        ownernya = [`${m.chat.split`-`[0]}@s.whatsapp.net`]
+        let mentionedJid = groupAdmins.concat(ownernya)
+        conn.sendFile(m.key.remoteJid, pp, 'pp.jpg', text, m, false, { contextInfo: { mentionedJid } })
+    }
+}
+handler.command = /^(admins|@admins)$/i
+
+handler.group = true
+
+module.exports = handler

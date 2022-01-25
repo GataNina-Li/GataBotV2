4f745238d87600996e9ea2814c007bde1bc3200f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-24 21:28:47 -0600

    ©ShadowBot

---
+++ plugins/mp3_labiblia.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/ora.mp3'
+conn.sendFile(m.chat, vn, 'ora.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true
+})
+}
+handler.customPrefix = /laoracion|La biblia|La oración|La biblia|La oración|la biblia|La Biblia/
+handler.command = new RegExp
+module.exports = handler

d25fd82898cbd1cae2473fd2ddb9e6efe4b7b324
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-25 20:01:02 -0600

    ©ShadowBot

--- plugins/bot.shadow.vos.js
+++ plugins/bot.shadow.vos.js
@@ -8,6 +8,6 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /the shadow brokers - bot|The Shadow Brokers - Bot|tsbb|TSBB|Shadow Brokers - Bot|shadow brokers - bot|Shadow bot|shadow bot|theshadowbrokersbot|Shadow Bot|Hola Bot|hola bot|Hola bot|hola bot/
+handler.customPrefix = /the shadow brokers - bot|The Shadow Brokers - Bot|tsbb|TSBB|Shadow Brokers - Bot|shadow brokers - bot|Shadow bot|shadow bot|theshadowbrokersbot|Shadow Bot|Hola Bot|hola bot|Hola bot|hola bot|ShadowBot|shadowbot/
 handler.command = new RegExp
 module.exports = handler

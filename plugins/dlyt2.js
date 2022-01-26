2b95f81f50e77bfba36e5184bc269d958eb7a193
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-26 10:13:04 -0600

    ©ShadowBot

---
+++ plugins/dlyt2.js
@@ -0,0 +1,39 @@
+let limit = 30
+let fetch = require('node-fetch')
+const { servers, yt } = require('../lib/y2mate')
+
+let handler = async (m, { conn, args, isPrems, isOwner }) => {
+let fs = require('fs')
+let y = fs.readFileSync('./Menu2.jpg')
+  if (!args || !args[0]) throw '*[❗] Inserte un enlace de YouTube*\n\n*Ejemplo:*\n*#ytmp4 https://www.youtube.com/watch?v=8jvDzEIVpjg*'
+  let chat = global.DATABASE.data.chats[m.chat]
+  let quality = args[1] || '360'
+  let server = (args[2] || servers[0]).toLowerCase()
+  let { dl_link, thumb, title, filesize, filesizeF } = await yt(args[0], quality + 'p', 'mp4', quality, servers.includes(server) ? server : servers[0])
+  //let isLimit = (isPrems || isOwner ? 99 : limit) * 99888898 < filesize
+let _thumb = {}
+  try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
+  catch (e) { }
+  conn.sendMessage(m.chat, `*⏯ ️Descargador By ShadowBot ⏯️*\n\n*🔥Titulo:* ${title}\n*📂Tamaño del archivo:* ${filesizeF}` , 'conversation', {quoted: m, thumbnail: y, contextInfo:{externalAdReply: {title: 'Simple WhatsApp bot', body: `© ${conn.user.name}`, sourceUrl: 'enviando...', thumbnail: y}}})
+ conn.sendFile(m.chat, dl_link, `By ${conn.user.name}.mp4`, `
+ *${title}*
+ *©${conn.user.name}*
+`.trim(), m, false, {
+  
+ptt: false, duration: 999999999999, thumbnail: y , asDocument: chat.useDocument})
+}
+handler.command = /^yt(v|mp4)?$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+handler.limit = false
+
+module.exports = handler

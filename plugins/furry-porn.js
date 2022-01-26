44bb12efcae9204b390a01320d32a292427ac2f0
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-25 19:49:43 -0600

    ©ShadowBot

---
+++ plugins/furry-porn.js
@@ -0,0 +1,21 @@
+const axios = require('axios')
+ let handler = async(m, { conn }) => {
+let les = await axios.get('https://meme-api.herokuapp.com/gimme/Furryporn')
+            conn.sendFile(m.chat, `${les.data.url}`, '', `*¿Si me pongo de perrito de adoptas? 🔥🥴*`, m)
+  }
+handler.command = /^(furry|furro|furr@|furra|furrys|furros|furr@s|furras|furri|furris)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.register = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+handler.limit = false
+
+module.exports = handler

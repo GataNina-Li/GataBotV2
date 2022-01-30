34183c6bed2836e352f24c5acb2f460f9d1fea3a
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-30 11:44:47 -0600

    ©ShadowBot

--- plugins/atencion a clientes.js
+++ plugins/atencion a clientes.js
@@ -4,8 +4,8 @@ let path = require('path')
 let handler = async (m, { conn }) => {
 let vn = './media/insultar.mp3'
 conn.sendFile(m.chat, vn, 'insultar.mp3', null, m, true, {
-type: 'audioMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+type: 'audioMessage', 
+ptt: true
 })
 }
 handler.customPrefix = /bot puto|Bot puto|Bot CTM|Bot ctm|bot CTM|bot ctm|bot pendejo|Bot pendejo|Bot gay|bot de mierda/

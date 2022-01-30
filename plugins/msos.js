d22257d82b5239fd11b200af1ac16125995e9a57
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-30 00:46:36 -0600

    ©ShadowBot

---
+++ plugins/msos.js
@@ -0,0 +1,9 @@
+let fetch = require('node-fetch')
+     let handler  = async (m, { conn, usedPrefix, command }) => {
+    heum = await fetch(`https://api-alc.herokuapp.com/api/chicas/muslos?&apikey=ConfuMods`)
+    json = await heum.buffer()
+   conn.sendButtonImg(m.chat, json, '*By ShadowBot and ConfuMods*\n*https://api-alc.herokuapp.com/api/chicas/muslos?&apikey=ConfuMods*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+}
+handler.command = /^(muslitos)$/i
+
+module.exports = handler

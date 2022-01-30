82e175904bf4d815c37a22cd4c7b09925d49184c
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-30 11:49:21 -0600

    ©ShadowBot

--- plugins/es putoo.js
+++ plugins/es putoo.js
@@ -4,8 +4,8 @@ let path = require('path')
 let handler = async (m, { conn }) => {
 let vn = './media/Es putoo.mp3'
 conn.sendFile(m.chat, vn, 'Es putoo.mp3', null, m, true, {
-type: 'audioMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+type: 'audioMessage', 
+ptt: true 
 })
 }
 handler.customPrefix = /es puto|eeesss putoo|es putoo|esputoo/i

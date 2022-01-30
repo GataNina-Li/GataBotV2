19c351a58cc8796cc00e2d9ddc6e2b23fcd636db
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-30 12:00:13 -0600

    ©ShadowBot

--- plugins/buenos dias.js
+++ plugins/buenos dias.js
@@ -4,8 +4,8 @@ let path = require('path')
 let handler = async (m, { conn }) => {
 let vn = './media/Buenos-dias-2.mp3'
 conn.sendFile(m.chat, vn, 'Buenos-dias-2.mp3', null, m, true, {
-type: 'audioMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+type: 'audioMessage', 
+ptt: true 
 })
 }
 handler.customPrefix = /buenos dias|Buenos dias|buenos días|Buenos días/

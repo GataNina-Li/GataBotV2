7012028be53a80a5afc8fad7436012a6480cd756
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-30 11:48:28 -0600

    ©ShadowBot

--- plugins/diagnosticadocongay_mp3.js
+++ plugins/diagnosticadocongay_mp3.js
@@ -4,8 +4,8 @@ let path = require('path')
 let handler = async (m, { conn }) => {
 let vn = './media/DiagnosticadoConGay.mp3'
 conn.sendFile(m.chat, vn, 'DiagnosticadoConGay.mp3', null, m, true, {
-type: 'audioMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+type: 'audioMessage',
+ptt: true 
 })
 }
 handler.customPrefix = /giagnosticadocongay|diagnosticado con gay|diagnosticado gay|te diagnóstico con gay|diagnóstico gay|te diagnostico con gay|te diagnóstico con gay|te diagnosticó con gay|te diagnostico con gay/i

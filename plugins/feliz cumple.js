00d7e1a60c095bd24144599324b72d1e7be35b1f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-30 11:49:44 -0600

    ©ShadowBot

--- plugins/feliz cumple.js
+++ plugins/feliz cumple.js
@@ -4,8 +4,8 @@ let path = require('path')
 let handler = async (m, { conn }) => {
 let vn = './media/Feliz cumpleaños.mp3'
 conn.sendFile(m.chat, vn, 'Feliz cumpleaños.mp3', null, m, true, {
-type: 'audioMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+type: 'audioMessage', 
+ptt: true 
 })
 }
 handler.customPrefix = /feliz cumpleaños|felizcumpleaños|happy birthday/i

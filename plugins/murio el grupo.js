95a62fe5dd74a6e2007abc9c03442ae0ae103a99
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-30 11:44:05 -0600

    ©ShadowBot

--- plugins/murio el grupo.js
+++ plugins/murio el grupo.js
@@ -4,8 +4,8 @@ let path = require('path')
 let handler = async (m, { conn }) => {
 let vn = './media/Murio.m4a'
 conn.sendFile(m.chat, vn, 'Murio.m4a', null, m, true, {
-type: 'audioMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+type: 'audioMessage', 
+ptt: true 
 })
 }
 handler.customPrefix = /Murió el grupo|Murio el grupo|murio el grupo|murió el grupo|Grupo muerto|grupo muerto/

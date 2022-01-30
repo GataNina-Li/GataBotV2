bf51ba8833e4b30437bfabf2515e477c1ac9bb88
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-30 00:53:55 -0600

    ©ShadowBot

--- plugins/pinterestvideo.js
+++ plugins/pinterestvideo.js
@@ -2,14 +2,14 @@ const { pin } = require('../lib/scrape')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-    if (!args[0]) throw `*Perintah ini untuk mengunduh video dari pinterest dengan link*\n\ncontoh:\n${usedPrefix + command} https://id.pinterest.com/pin/267893877823775677/`
-    if (!args[0].match(/https:\/\/.*pinterest.com\/pin|pin.it/gi)) throw `*Link salah! Perintah ini untuk mengunduh video dari pinterest dengan link*\n\ncontoh:\n${usedPrefix + command} https://id.pinterest.com/pin/267893877823775677/`
+    if (!args[0]) throw `*Este comando para descargar videos de pinterest se debe usar con link del video*\n\n*Ejemplo:*\n${usedPrefix + command} https://id.pinterest.com/pin/267893877823775677/`
+    if (!args[0].match(/https:\/\/.*pinterest.com\/pin|pin.it/gi)) throw `*¡Link incorrecto! Este comando para descargar videos de pinterest se debe usar con link del video*\n\n*Ejemplo:*\n${usedPrefix + command} https://id.pinterest.com/pin/267893877823775677/`
 
     pin(args[0]).then(async res => {
         let pin = JSON.stringify(res)
         let json = JSON.parse(pin)
-        if (!json.status) throw `Tidak dapat diunduh`
-        await conn.sendVideo(m.chat, json.data.url, `*BOTSTYLE*`, m, { thumbnail: Buffer.alloc(0) })
+        if (!json.status) throw `*El video no se pudo descargar*`
+        await conn.sendVideo(m.chat, json.data.url, `*The Shadow Brokers - Bot*`, m, { thumbnail: Buffer.alloc(0) })
     })
 
 }

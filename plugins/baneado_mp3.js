a5aca737075d762d054d511bff2f8fa4517995d1
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-30 12:01:45 -0600

    ©ShadowBot

--- plugins/baneado_mp3.js
+++ plugins/baneado_mp3.js
@@ -4,8 +4,8 @@ let path = require('path')
 let handler = async (m, { conn }) => {
 let vn = './media/baneado.mp3'
 conn.sendFile(m.chat, vn, 'baneado.mp3', null, m, true, {
-type: 'audioMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+type: 'audioMessage', 
+ptt: true 
 })
 }
 handler.customPrefix = /baneado|Baneado/

3d8c5118dbec6c1959b5e5683e59332c10a2de7e
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-30 11:48:58 -0600

    ©ShadowBot

--- plugins/elmo.js
+++ plugins/elmo.js
@@ -4,8 +4,8 @@ let path = require('path')
 let handler = async (m, { conn }) => {
 let vn = './media/Elmo.mp3'
 conn.sendFile(m.chat, vn, 'Elmo.mp3', null, m, true, {
-type: 'audioMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+type: 'audioMessage', 
+ptt: true 
 })
 }
 handler.customPrefix = /Pasa pack|vendes tu nudes|pasa video hot|pasa tu pack|pasa fotos hot|vendes tu pack|Vendes tu pack|Vendes tu pack?|vendes tu pack|Pasa Pack Bot|pasa pack Bot|pasa tu pack Bot|Pásame tus fotos desnudas|pásame tu pack|me pasas tu pak|me pasas tu pack|pasa pack/

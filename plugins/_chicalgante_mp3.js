98d46047582e68dd617f1dfee1b1c7bef59aa0bb
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-30 12:02:32 -0600

    ©ShadowBot

--- plugins/_chicalgante_mp3.js
+++ plugins/_chicalgante_mp3.js
@@ -4,10 +4,10 @@ let path = require('path')
 let handler = async (m, { conn }) => {
 let vn = './media/chica lgante.mp3'
 conn.sendFile(m.chat, vn, 'chica lgante.mp3', null, m, true, {
-type: 'audioMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+type: 'audioMessage', 
+ptt: true
 })
 }
-handler.customPrefix = /chica lgante|Chica lgante|Chicalgante|chicalgante/
+handler.customPrefix = /chica lgante|Chica lgante|Chicalgante|chicalgante|chical gante|Chical gante/
 handler.command = new RegExp
 module.exports = handler

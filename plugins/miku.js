9bd5edfa961a174b7b48eb846031f348fd5d8d52
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-30 12:34:39 -0600

    ©ShadowBot

--- plugins/miku.js
+++ plugins/miku.js
@@ -1,8 +1,8 @@
-let handler = async (m, { conn }) => {
-  //await m.reply(wait)
-  let res = `https://api-reysekha.herokuapp.com/api/wallpaper/miku?apikey=APIKEY`
-  conn.sendFile(m.chat, res, 'miku.jpg', `*©The Shadow brokers - Bot*`, m)
-}
+const axios = require('axios')
+ let handler = async(m, { conn }) => {
+let les = await axios.get('https://meme-api.herokuapp.com/gimme/miku')
+            conn.sendFile(m.chat, `${les.data.url}`, '', `*Hatsune Miku*`, m)
+  }
 handler.help = ['miku']
 handler.tags = ['random']
 handler.command = /^(miku)$/i

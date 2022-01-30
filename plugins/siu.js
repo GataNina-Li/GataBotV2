c99291657f3adb1b477028dbe1fe81bbd082454f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-30 11:56:24 -0600

    ©ShadowBot

--- plugins/siu.js
+++ plugins/siu.js
@@ -4,8 +4,8 @@ let path = require('path')
 let handler = async (m, { conn }) => {
 let vn = './media/siu.mp3'
 conn.sendFile(m.chat, vn, 'siu.mp3', null, m, true, {
-type: 'audioMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+type: 'audioMessage', 
+ptt: true 
 })
 }
 handler.customPrefix = /siu|siiuu|ssiiuu|siuuu|siiuuu|siiiuuuu|siuuuu|siiiiuuuuu|siu|SIIIIUUU/i

4af3c11e4486732adcf4b888b206c25519966145
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-30 19:45:59 -0600

    ©ShadowBot

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 function handler(m) {
-  //this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
+  this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
   this.sendContact(m.chat, '14502335923', 'SHADOW N1 - CREADOR - OFICIAL', m)
   this.sendContact(m.chat, '17722386341', 'SHADOW N2 - CREADOR - OFICIAL', m)
   this.sendContact(m.chat, '14503280343', 'SHADOW N3 - CREADOR - OFICIAL', m)

b894310cccce6e374e6c0404ce218ea223378220
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-31 14:06:42 -0600

    ©ShadowBot

--- plugins/play1.js
+++ plugins/play1.js
@@ -5,12 +5,12 @@ const { servers, yta, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, command, text }) => {
   if (!text) throw '*✳️ Inserte el nombre/título del video o audio a bucar*\n\n*Ejemplo:*\n*#play Juan Solo - Querido Corazón*'
   let results = await yts(text)
+  m.reply('*⏳Procesando⏳*\n\n*[❗] Si no obtiene ningun resultado o le sale algun error intente con otro nombre*')
   let vid = results.all.find(video => video.seconds < 3600)
   if (!vid) throw '*Video/Audio No encontrado* '
   let isVideo = /2$/.test(command)
   let { dl_link, thumb, title, filesize, filesizeF} = await (isVideo ? ytv : yta)(vid.url, 'id4')
   //let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesizesLimit
-m.reply('*⏳Procesando⏳*\n\n*[❗] Si no obtiene ningun resultado o le sale algun error intente con otro nombre*')
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
 *⏯ ️Reproductor By Shadow Brokers - Bot ⏯️*
 *${title}*

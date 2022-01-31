1b4413d53263bf59a5d08118ef0825012d6d11cc
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-01-31 12:31:49 -0600

    ©ShadowBot

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, participants }) => {
   // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
-    if (!(m.chat in global.DATABASE._data.chats)) return m.reply('Este chat no está registrado en la BASE DE DATOS!')
+    if (!(m.chat in global.DATABASE._data.chats)) return m.reply('*Este chat no está registrado en la base de datos!*')
     let chat = global.DATABASE._data.chats[m.chat]
-    if (chat.isBanned) return m.reply('Este chat ha sido baneado por imcumplir alguna regla!')
+    if (chat.isBanned) return m.reply('*Este chat ya estaba baneado previamente*\n*Si desea desbanearlo use /unbanchat*')
     chat.isBanned = true
-    m.reply('Listo!')
+    m.reply('*Este chat ha sido baneado a desicion de un administrador del grupo*')
   // } else m.reply('Aquí hay un número de un host...')
 }
 handler.help = ['banchat']

3c75c6bdc04651bf8ac3469016ad59aceb38f421
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-01 05:49:33 -0600

    ©ShadowBot

--- plugins/fb.js
+++ plugins/fb.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
-const { fbdl } = require('./lib/fb')
+const { fb } = require('./lib/fb.js')
 let handler = async(m, { conn, usedPrefix, args, command, watermark }) => {
   if (!args[0]) throw `Masukkan URL Facebook yang ingin di download!\n\nContoh: ${usedPrefix + command} https://www.facebook.com/100009307660961/videos/2850837675236460/`
-  let v = await fbdl(args[0])
+  let v = await fb(args[0])
 await conn.sendFile(m.chat, v.hasil.link_high, 'fb.mp4', watermark, m)
 }
 handler.help = ['fb'].map(v => v + ' <url>')

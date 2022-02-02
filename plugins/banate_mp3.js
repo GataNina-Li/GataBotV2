871c76cc882f420c51d3706f46a1ceb15d70cb0f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-02 12:52:02 -0600

    ©ShadowBot

--- plugins/banate_mp3.js
+++ plugins/banate_mp3.js
@@ -2,8 +2,8 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './media/Bañate.mp3'
-conn.sendFile(m.chat, vn, 'Bañate.mp3', null, m, true, {
+let vn = './media/Banate.mp3'
+conn.sendFile(m.chat, vn, 'Banate.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true
 })

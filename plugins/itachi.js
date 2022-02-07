569519dc9ab0937949c0512a5b25cab38fba580c
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-07 13:03:59 -0600

    ©ShadowBot

--- plugins/itachi.js
+++ plugins/itachi.js
@@ -1,23 +1,9 @@
-const axios = require('axios')
- let handler = async(m, { conn }) => {
-let les = await axios.get('https://meme-api.herokuapp.com/gimme/itachi')
-            conn.sendFile(m.chat, `${les.data.url}`, '', `*Incluso el más fuerte de los oponentes tiene siempre una debilidad*`, m)
-  }
-handler.help = ['Itachi']
-handler.tags = ['General']
+let fetch = require('node-fetch')
+     let handler  = async (m, { conn, usedPrefix, command }) => {
+    heum = await fetch(`https://api-reysekha.herokuapp.com/api/wallpaper/itachi?apikey=APIKEY`)
+    json = await heum.buffer()
+   conn.sendButtonImg(m.chat, json, '*Incluso el más fuerte de los oponentes tiene siempre una debilidad*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+}
 handler.command = /^(itachi)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.register = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
-handler.limit = false
 
 module.exports = handler

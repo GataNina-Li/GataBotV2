ff71069ec67cf6a1b9c9e3e70fe0b1c9c67c5c69
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-07 13:17:36 -0600

    ©ShadowBot

---
+++ plugins/booty.js
@@ -0,0 +1,8 @@
+const axios = require('axios')
+ let handler = async(m, { conn }) => {
+let les = await axios.get('https://meme-api.herokuapp.com/gimme/booty')
+            conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
+  }
+handler.command = /^(booty|culo|culos|culo2|culos2)$/i
+
+module.exports = handler

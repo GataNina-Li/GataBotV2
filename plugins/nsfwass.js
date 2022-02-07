c6698348c728a06de2a61f7f36e8dabb12654d1f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-07 12:59:32 -0600

    ©ShadowBot

--- plugins/nsfwass.js
+++ plugins/nsfwass.js
@@ -1,11 +1,9 @@
-let handler = async (m, { conn }) => {
-  //await m.reply(wait)
-  let res = `https://server-api-rey.herokuapp.com/api/nsfw/ass?apikey=apirey`
-  conn.sendFile(m.chat, res, 'ass.jpg', `*Disfrutalo!!*`, m)
+let fetch = require('node-fetch')
+     let handler  = async (m, { conn, usedPrefix, command }) => {
+    heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/ass?apikey=apirey`)
+    json = await heum.buffer()
+   conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
 }
-handler.help = ['nsfwass']
-handler.tags = ['nsfw']
-handler.command = /^(nsfwass)$/i
-handler.limit = false
+handler.command = /^(hentai|hentay)$/i
 
 module.exports = handler

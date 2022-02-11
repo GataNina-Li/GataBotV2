03155b148fd7db7791c5f4bf4af2cfa1d02eebd2
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-11 00:52:44 -0600

    Update @@_Prueba.js

--- plugins/@@_Prueba.js
+++ plugins/@@_Prueba.js
@@ -17,7 +17,8 @@ let pp = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gam
   } catch (e) {
 
   } finally {
-m.reply(m.chat, '*[ ⚠️ ]  Cargando menú...*\n\n*[❗] Si no puede visualizar el menú, use el comando ${usedPrefix}menu1.2*)'}}
+m.reply(m.chat, '*[ ⚠️ ]  Cargando menú...*\n\n*[❗] Si no puede visualizar el menú, use el comando ${usedPrefix}menu1.2*)'
+}}
 let username = conn.getName(who)
 let menu =`
 ╭══〘 ✯✯✯✯✯✯✯✯ 〙═╮

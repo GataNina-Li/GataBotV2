28e4ee2150dbda71b08c67fc5869b1a177ea528d
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-15 17:21:39 -0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,6 @@
 function handler(m) {
   this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
   this.sendContact(m.chat, '593968585383', 'Gata Dios', m)
-  this.sendContact(m.chat, '593993684821', 'GD - BOT1', m)
   this.sendContact(m.chat, '994407312387', 'GD - BOT2', m)
   }
 handler.help = ['contacto']

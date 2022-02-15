56fcee4645cdf29e57bebde3e0477ac5552c4842
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-15 14:50:39 -0500

    Update estado.js

--- plugins/estado.js
+++ plugins/estado.js
@@ -1,12 +1,12 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command, text, usedPrefix }) => {
   await conn.send3Button(m.chat, `
-*ミ💖 Hola estimado usuario 💖彡*
+*ミ💖 Hola estimado/a usuario/a 💖彡*
 
 *ミ🤖 Estado del Bot 🤖彡*
-*=> Bot activo ✔️*
-*=> Bot uso público ✔️*
-`.trim(), '©The Shadow Borkers - Bot', '🔷️ MENÚ 🔷', `${usedPrefix}menu`, '🔶️ MENÚ SIMPLE 🔶️', `${usedPrefix}menusimple`, '💠️ MENÚ AUDIOS 💠️', `${usedPrefix}menuaudios`)
+*=> Bot activo ✅*
+*=> Bot uso público ✅*
+`.trim(), 'Gata Dios', '🟢 MENÚ 🟢', `${usedPrefix}menu`, '🟠 MENÚ SIMPLE 🟠', `${usedPrefix}menusimple`, '🔵 MENÚ DE AUDIOS 🔵', `${usedPrefix}menuaudios`)
 }
 handler.command = /^(estado|status|estate|state|stado|stats)$/i
 

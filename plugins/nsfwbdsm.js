887d24e939fc6da7940754841f9c0db552402c86
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-16 12:09:20 -0500

    Update nsfwbdsm.js

--- plugins/nsfwbdsm.js
+++ plugins/nsfwbdsm.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
-    heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/bdsm?apikey=apirey`)
+    heum = await fetch(`https://api.lolhuman.xyz/api/pinterestdl?apikey`)
     json = await heum.buffer()
    conn.sendButtonImg(m.chat, json, '*¡Disfrutalo!*', 'Gata Dios', 'SIGUIENTE 🔄', `${usedPrefix + command}`, m, false)
 }

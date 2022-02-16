87ec9e6edaa3e3f227be0972833ca5b5edcef728
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-15 21:30:43 -0500

    Update hentairandom.js

--- plugins/hentairandom.js
+++ plugins/hentairandom.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
     heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/hentai?apikey=apirey`)
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '*Hentai By ShadowBot*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+   conn.sendButtonImg(m.chat, json, '*Hentai By TheBrokers*', 'Gata Dios', 'SIGUIENTE 🔄', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(hentai|hentay)$/i
 

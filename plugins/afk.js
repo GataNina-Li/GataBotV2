0e5348c05c6c22740e07b9ec104c66d02237fac7
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-16 21:37:00 -0500

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -2,7 +2,7 @@ let handler = async (m, { text }) => {
   let user = global.DATABASE.data.users[m.sender]
   user.afk = + new Date
   user.afkReason = text
-  m.reply(`*[ ⚠️ ] El usuario ${conn.getName(m.sender)} estará inactivo (AFK) no lo etiqueten*\n\n*🔰 Motivo de la inactividad${text ? ': ' + text : ''}*
+  m.reply(`*❰ ❕ ❱ El usuario ${conn.getName(m.sender)} estará inactivo (AFK) NO lo etiqueten*\n\n*❰ ❕ ❱ Motivo de la inactividad${text ? ': ' + text : ''}*
 `)
 }
 handler.help = ['afk [alasan]']

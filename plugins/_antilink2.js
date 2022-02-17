9a5489a67b9ed2d1287d9e2ca3675b93d0d42b75
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-16 22:50:58 -0500

    Update _antilink2.js

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -7,17 +7,15 @@ handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antiLink2 && isGroupLink) {
-    await m.reply(`*「 ANTI LINKS 」*\n*Hasta la vista baby👋, ${await this.getName(m.sender)} rompiste las reglas serás exterminado....!!*`)
-    await m.reply(`*Tienes 3 segundos para eliminar el link y retractarte...!!!!*`)
-    await m.reply(`*3!!*`)
-    await m.reply(`*2!!*`)
-    await m.reply(`*1!!*`)
-    if (isAdmin) return m.reply('*Te salvaste cagon(a) eres admin, no puedo eliminarte :v*')
-    if (!isBotAdmin) return m.reply('*El bot no es admin, no puede exterminar a las personas*')
+    await m.reply(`*「❗️ANTI LINKS❗️」*\n*Eso no se hace 🤨, ${await this.getName(m.sender)} ¡No respetas las reglas!*`)
+    await m.reply(`**Procedo a eliminar 😌*`)
+    await m.reply(`*Fuera!!*`)
+    if (isAdmin) return m.reply('*Que basado/a eres Admin, no puedo eliminarte 😐*')
+    if (!isBotAdmin) return m.reply('*El Bot no es admin, no puede eliminar 😅*')
     let linkGC = ('https://chat.whatsapp.com/' + await this.groupInviteCode(m.chat))
     let isLinkThisGc = new RegExp(linkGC, 'i')
     let isgclink = isLinkThisGc.test(m.text)
-    if (isgclink) return m.reply('*Lol.. enviaste el enlace de este grupo :v*')
+    if (isgclink) return m.reply('*REbasado/a enviaste el enlace de este grupo 🧐')
     await this.groupRemove(m.chat, [m.sender])
   }
   return true

de8470b5b55812b06aa252bcd41d4c309e8d4efd
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-17 13:24:39 -0500

    Update caja_fuerte.js

--- plugins/caja_fuerte.js
+++ plugins/caja_fuerte.js
@@ -67,10 +67,10 @@ handler.command = /^(CajaFuerte)$/i
 handler.rowner = false
 handler.mods = false
 handler.premium = false
-handler.group = false
+handler.group = true
 handler.private = false
 
-handler.admin = true
+handler.admin = false
 handler.botAdmin = false
 
 handler.fail = null

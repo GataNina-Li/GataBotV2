de0bd72d0662adde9f3d4334b0d4eba39cc5c7a6
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-16 23:13:26 -0500

    Update menu de comandos.js

--- plugins/menu de comandos.js
+++ plugins/menu de comandos.js
@@ -145,9 +145,9 @@ let handler = async (m, { conn, usedPrefix }) => {
 ┣ ↠🔐 _${usedPrefix}grupo *abierto / cerrado*_
 ┣ ↠🔒 _${usedPrefix}enable welcome_
 ┣ ↠🔐 _${usedPrefix}disable welcome_
-┣ ↠🔒 _${usedPrefix}enable antilink_
-┣ ↠🔐 _${usedPrefix}disable antilink_
-┣ ↠🔒 _${usedPrefix}enable antilink2_
+┣ ↠🔒 _${usedPrefix}enable antilink_ *(WhatsApp)*
+┣ ↠🔐 _${usedPrefix}disable antilink_ 
+┣ ↠🔒 _${usedPrefix}enable antilink2_ *(https:)*
 ┣ ↠🔐 _${usedPrefix}disable antilink2_
 ┣ ↠🔒 _${usedPrefix}enable delete_
 ┣ ↠🔐 _${usedPrefix}disable  delete_ 

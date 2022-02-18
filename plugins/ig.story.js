06a4ee9ee8c5d3ee62fc62c952a54dcf6ea3709f
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-18 18:38:16 -0500

    Update ig.story.js

--- plugins/ig.story.js
+++ plugins/ig.story.js
@@ -1,23 +1,31 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn, args, command, usedPrefix }) => {
-  if (!args[0]) throw `uhm.. username nya mana?\n\ncontoh:\n${usedPrefix + command} stikerinbot`
-  let res = await fetch(global.API('xteam', '/dl/ighighlight', {
+let handler = async (m, { conn, args }) => {
+  if (!args[0]) throw 'Uhm...url nya mana?'
+  let res = await fetch(global.API('xteam', '/dl/igs', {
     nama: args[0]
   }, 'APIKEY'))
-  if (!res.ok) throw eror
   let json = await res.json()
+  if (res.status != 200) throw json
   if (json.result.error) throw json.result.message
-  let { username, items } = json.result
-  for (let { thumbnail, isVideo, url } of items) {
-    thumbnail = await (await fetch(thumbnail)).buffer()
-    conn.sendFile(m.chat, url, 'ig' + (isVideo ? '.mp4' : '.jpg'), '', m, 0, {
-      thumbnail
-    })
+  let { username, storylist } = json.result
+  let dateConfig = {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
   }
+  for (let { url, type, taken_at } of storylist)
+    conn.sendFile(m.chat, url, 'ig' + (type == 'video' ? '.mp4' : '.jpg'), `
+@${username}
+Memposting pada ${new Date(taken_at * 1000).toLocaleDateString('id', dateConfig)}
+`, m)
+  throw json.result
 }
-handler.help = ['ighighlight'].map(v => v + ' <username>')
+handler.help = ['igstory'].map(v => v + ' <username>')
 handler.tags = ['downloader']
 
-handler.command = /^(ighighlight?)$/i
+handler.command = /^(igs(tory)?)$/i
 
 module.exports = handler

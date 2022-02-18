367877fa1b8cc42adb5cef805007680c081d3716
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-18 14:12:45 -0500

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,18 +1,15 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const { servers, yta, ytv } = require('../lib/y2mate')
 let yts = require('yt-search')
 let fetch = require('node-fetch')
+
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `*✳️ Uhm.. que estas buscando?*\n*👉🏻 Ingrese un texto o enlace de YT*\n\n*✅ Ejemplo:*\n*${usedPrefix + command} Begin you*`
-//  let chat = global.db.data.chats[m.chat]
+  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} toxic friends`
   let results = await yts(text)
   let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw '*El video no se encontró, intente ingresar el nombre original de la canción o video*'
-  let isVideo = /2$/.test(command)
+  if (!vid) throw 'konten tidak ditemukan'
   let yt = false
   let yt2 = false
   let usedServer = servers[0]
-  m.reply('*⏳Procesando⏳*\n\n*[❗] Si no obtiene ningun resultado o le sale algun error intente con otro nombre*')
   for (let i in servers) {
     let server = servers[i]
     try {
@@ -21,22 +18,18 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
       usedServer = server
       break
     } catch (e) {
-      m.reply(`*El servidor ${server} fallo!, reintentando con otro servidor*${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
+      console.log(`server ${server} error!${servers.length >= i + 1 ? '' : '\ntrying another server...'}`)
     }
   }
-  if (yt === false) throw '*Todos los servidores fallaron*'
-  if (yt2 === false) throw '*Todos los servidores fallaron*'
+  if (yt === false || yt2 === false) throw 'all servers failed!'
   let { dl_link, thumb, title, filesize, filesizeF } = yt
-  await conn.send2ButtonLoc(m.chat, await (await fetch(thumb)).buffer(), `
-*🔥 Titulo:* _${title}_
-*📂 Peso del audio:* _${filesizeF}_
-*📂 Peso del video:* _${yt2.filesizeF}_
-`.trim(), '©The Shadow Borkers - Bot', '🎵 AUDIO 💽 ', `.yta ${vid.url}`, '🎥 VIDEO 🎞️', `.yt ${vid.url}`)
+  await conn.send2ButtonLoc(m.chat, thumb, `
+*judul:* ${title}
+*server y2mate:* ${usedServer}
+`.trim(), wm, `audio (${filesizeF})`, '.yta ' + vid.url, `video (${yt2.filesizeF})`, '.yt ' + vid.url, m)
 }
-handler.help = ['play'].map(v => v + ' <pencarian>')
-handler.tags = ['downloader']
-handler.command = /^(reproducir|reproducir2|reproductor|Reproducir|Reproducir2|Reproductor|play3|Play3|playvid|Playvid|playaudio|Playaudio)$/i
-
-handler.exp = 0
+handler.help = ['play'].map(v => v + ' <teks>')
+handler.tags = ['internet', 'download']
+handler.command = /^(p|play)$/i
 
 module.exports = handler

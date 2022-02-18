4b043b1c859e194fddecf74044469b9287a50bc8
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-18 01:21:16 -0500

    Update pinterest2.js

--- plugins/pinterest2.js
+++ plugins/pinterest2.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw *[❗️] Que desea buscar?*\n*Inserte un texto*\n${usedPrefix + command} Texto
+  if (!text) throw `*[❗️] Que desea buscar?*\n*Inserte un texto*\n${usedPrefix + command} imagen`
    if (text) m.reply('*[ ✔️ ] Buscando, aguarde un momento....*\n\n*[❗️] Si llega a dar algun problema intente de nuevo con otro texto*')
   let res = await fetch(global.API('zeks', '/api/pinimg', {
     q: text
@@ -9,8 +9,9 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   let json = await res.json()
   if (!json.status) throw json
   let pint = json.data[Math.floor(Math.random() * json.data.length)];
-  conn.sendFile(m.chat, pint, '', 'Gata Dios', m, 0, { thumbnail: await (await fetch(pint)).buffer() })
+  conn.sendFile(m.chat, pint, '', '© stikerin', m, 0, { thumbnail: await (await fetch(pint)).buffer() })
 }
+module.exports = handler
 handler.help = ['pinterest2 <query>']
 handler.tags = ['image']
 handler.command = /^pinterest2$/i

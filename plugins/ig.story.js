1f87214c8b36d0069297779cb5011a6fc537e650
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-18 18:29:27 -0500

    Update ig.story.js

--- plugins/ig.story.js
+++ plugins/ig.story.js
@@ -1,23 +1,22 @@
-const { igdl } = require('../lib/scrape')
+const { igstory } = require('../lib/scrape')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-  if (!args[0]) throw `Pengunaan:\n${usedPrefix + command} <url>\n\nContoh:\n${usedPrefix + command} https://www.instagram.com/p/CQU21b0JKwq/`
-  if (!args[0].match(/https:\/\/www.instagram.com\/.*(p|reel|tv)/gi)) throw `url salah, perintah ini untuk mengunduh post/reel/tv`
+  if (!args[0]) throw `Pengunaan:\n${usedPrefix + command} <url>\n\nContoh:\n\n${usedPrefix + command} stikerinbot`
+  if (args[0].startsWith('http') || args[0].startsWith('@')) throw `username salah`
 
-  igdl(args[0]).then(async res => {
-    let igdl = JSON.stringify(res)
-    let json = JSON.parse(igdl)
+  igstory(args[0]).then(async res => {
+    let igs = JSON.stringify(res)
+    let json = JSON.parse(igs)
     await m.reply(global.wait)
-    for (let { downloadUrl, type } of json) {
+    for (let { downloadUrl, type } of json)
       conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '© stikerin', m)
-    }
-  })
-}
-handler.help = ['ig'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command = /^(ig|instagram)$/i
 
-handler.limit = 5
+  })
+
+}
+handler.help = ['igstory'].map(v => v + ' <username>')
+handler.tags = ['downloader']
+handler.command = /^(igs(tory)?)$/i
 
 module.exports = handler

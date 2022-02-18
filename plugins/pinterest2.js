429cc32bdf12316226c72ad6c1876c34672e4d87
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-18 17:27:09 -0500

    Update pinterest2.js

--- plugins/pinterest2.js
+++ plugins/pinterest2.js
@@ -1,27 +1,29 @@
 let fetch = require('node-fetch')
-     let handler  = async (m, { conn, args, text }) => {
-     if (!text) throw '*[❗] Que desea buscar?*\n*Inserte un texto*'
- if (text) m.reply('*[ ✔️ ] Buscando, aguarde un momento....*\n\n*[❗] Si llega a dar algun problema intente de nuevo con otro texto*')
-    heum = await require('../lib/scraper').pinterest(text)
-    json = await heum
-    random = json.result[Math.floor(Math.random() * json.result.length)]
-    if (json.result.length == 0) return conn.sendFile(m.chat, './Menu2.jpg', 'error not found', 'ERROR 404 NOT FOUND', m)
-    data = await fetch('https://api.imgbb.com/1/upload?key=c93b7d1d3f7a145263d4651c46ba55e4&image='+random).then(v => v.json())
-   get = await conn.getFile(data.data.url)
-   conn.sendMessage(m.chat, get.data, 'imageMessage', { quoted: m, mimetype: get.mime, caption: data.data.url })
+let handler = async (m, { conn, command, text, usedPrefix, isPrems }) => {
+  if (!text) throw `Gunakan format *${usedPrefix}${command} [gambar|jumlah]*
+Contoh *${usedPrefix}${command} Minecraft|1*`
+  let [query,  jumlah] = text.split`|`
+  if (isNaN(jumlah) || (jumlah * 1) < 1) jumlah = 1
+  if ((jumlah * 1) > 30) jumlah = 30
+  if ((jumlah * 1) > 1 && !isPrems) {
+      jumlah = 1
+      m.reply('Untuk jumlah yang lebih banyak,  anda harus menjadi member *premium!!*')
+  } 
+  let res = await fetch(global.API('bg', '/pins', { q: query }))
+  let json = await res.json()
+  if (json.status !== true) throw json
+  for (let i = 0; i < jumlah; i++) {
+      try {
+         let url = json.result[Math.round(Math.random() * json.result.length)]
+         conn.sendFile(m.chat, url,  'MetroBot-pinterest.jpg', `*Url:* ${url}`, m)
+      } catch (e) {
+          console.log(e)
+          m.reply('Error in sending image')
+      }
+  }
 }
-handler.help = ['pinterest2 <query>']
-handler.tags = ['image']
-handler.command = /^pinterest2$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.limit = false
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
+handler.help = ['pinterest'].map(v => v + ' [teks]')
+handler.tags = ['downloader']
+handler.command = /^(pinterest|pin)$/i
 
 module.exports = handler

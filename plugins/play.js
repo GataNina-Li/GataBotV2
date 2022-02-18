602221c989fa1d4d2d2d9c911aee27096cc4bca9
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-18 18:42:22 -0500

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,35 +1,54 @@
-const { servers, yta, ytv } = require('../lib/y2mate')
+let limit = 30
 let yts = require('yt-search')
 let fetch = require('node-fetch')
-
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} toxic friends`
+const { servers, yta, ytv } = require('../lib/y2mate')
+let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
+  if (!text) throw 'Cari apa?'
+  let chat = global.db.data.chats[m.chat]
   let results = await yts(text)
   let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'konten tidak ditemukan'
+  if (!vid) throw 'Video/Audio Tidak ditemukan'
+  let isVideo = /2$/.test(command)
   let yt = false
-  let yt2 = false
   let usedServer = servers[0]
   for (let i in servers) {
     let server = servers[i]
     try {
-      yt = await yta(vid.url, server)
-      yt2 = await ytv(vid.url, server)
+      yt = await (isVideo ? ytv : yta)(vid.url, server)
       usedServer = server
       break
     } catch (e) {
-      console.log(`server ${server} error!${servers.length >= i + 1 ? '' : '\ntrying another server...'}`)
+      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
     }
   }
-  if (yt === false || yt2 === false) throw 'all servers failed!'
+  if (yt === false) throw 'Semua server tidak bisa :/'
   let { dl_link, thumb, title, filesize, filesizeF } = yt
-  await conn.send2ButtonLoc(m.chat, thumb, `
-*judul:* ${title}
-*server y2mate:* ${usedServer}
-`.trim(), wm, `audio (${filesizeF})`, '.yta ' + vid.url, `video (${yt2.filesizeF})`, '.yt ' + vid.url, m)
+  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
+  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
+*Title:* ${title}
+*Filesize:* ${filesizeF}
+*Source:* ${vid.url}
+*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
+*Server y2mate:* ${usedServer}
+`.trim(), m)
+let _thumb = {}
+try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
+catch (e) { }
+if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
+*Title:* ${title}
+*Filesize:* ${filesizeF}
+*Source:* ${vid.url}
+*Server y2mate:* ${usedServer}
+`.trim(), m, false,  {
+  ..._thumb,
+  asDocument: chat.useDocument
+})
 }
-handler.help = ['play'].map(v => v + ' <teks>')
-handler.tags = ['internet', 'download']
-handler.command = /^(p|play)$/i
+handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
+handler.tags = ['downloader']
+handler.command = /^play2?$/i
+
+handler.exp = 0
+handler.limit = true
 
 module.exports = handler

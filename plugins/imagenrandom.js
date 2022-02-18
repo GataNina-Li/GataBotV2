39434e99627d6d17d65191d074a5436c3b60224b
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-18 12:50:04 -0500

    Update imagenrandom.js

--- plugins/imagenrandom.js
+++ plugins/imagenrandom.js
@@ -2,6 +2,7 @@ const axios = require('axios')
  let handler = async(m, { conn }) => {
 let les = await axios.get('https://meme-api.herokuapp.com/gimme/sticker')
             conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), '*¡¡LLAMEN A LA POLICÍA!! 🚨️*', 'Gata Bot', 'SIGUIENTE 🔄', '/loli', m)
   }
 handler.help = ['imagenrandom']
 handler.tags = ['images']

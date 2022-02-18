37b357bb72ae29e362ef70da3f19ab66d4ae9e23
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-18 16:51:11 -0500

    Update grupos.js

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -8,8 +8,11 @@ Wa.me/593968585383
 *_➡️ GRUPO(S) EN DONDE ESTÁ EL BOT:_*
 ✅ https://chat.whatsapp.com/BlasuG7z02d16wEaCf61pa
 
-*_➡️GRUPO(S) DE COLABORACIÓN:_*
+*_➡️ GRUPO(S) DE COLABORACIÓN:_*
 ✅ https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ
+
+*_➡️ GRUPO(S) QUE PUEDA INTERESARTE_*
+✅ https://chat.whatsapp.com/BguFLFciPa44D9h9RGX1eN
 `.trim() 
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈', 'status@broadcast')

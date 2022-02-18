3bee030a589d2a15c4f7bd7d25bd9c0d1f0b2e3b
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-18 14:16:12 -0500

    Update pinterest2.js

--- plugins/pinterest2.js
+++ plugins/pinterest2.js
@@ -1,20 +1,36 @@
-let fetch = require('node-fetch')
-let handler = async(m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `Example: ${usedPrefix + command} minecraft`
-  let res = await fetch(global.API('zeks', '/api/pinimg', {
-    q: encodeURI(text)
-  }, 'apikey'))
-  if (!res.ok) throw await `${res.status} ${res.statusText}`
-  let json = await res.json()
-  if (!json.status) throw json
-  let pint = json.data[Math.floor(Math.random() * json.data.length)];
-  conn.sendFile(m.chat, pint, '', `
-*Search result*
-${text}
-`.trim(), m)
+let handler = async (m, { conn, command, text, usedPrefix, isPrems }) => {
+    let chat = db.data.chats[m.chat]
+    if (chat.download) {
+        await conn.sendButton(m.chat, 'turn off auto download to use this command', wm, 'Off', '.0 download', m)
+        throw 0
+    }
+    if (!text) throw `uhm.. where the url?\n\nusage:\n${usedPrefix + command} ${isPrems ? 'url, url, ...' : 'url'}\nexample:\n${usedPrefix + command} https://pin.it/4NwPw8E`
+    if (!/https?:\/\/(www\.)?(pinterest\.com\/pin|pin\.it)/i.test(text)) throw `wrong url!`
+    if (isPrems) {
+        let array = text.split(',')
+        await m.reply(`downloading ${array.length > 1 ? array.length + ' medias' : 'media'} from pinterest`)
+        for (let url of array) {
+            let json = await amel.pin(url.trim()).then(res => {
+                let json = JSON.parse(JSON.stringify(res))
+                console.log(json)
+                return json
+            })
+            await conn.sendFile(m.chat, json.data[0].url, json.data[0].url, '', m)
+        }
+        return !0
+    }
+    let json = await amel.pin(text).then(async res => {
+        let json = JSON.parse(JSON.stringify(res))
+        console.log(json)
+        return json
+    })
+    await m.reply('downloading media from pinterest')
+    await conn.sendFile(m.chat, json.data[0].url, json.data[0].url, '', m)
 }
-handler.help = ['pinterest <keyword>']
+handler.help = ['pinterest'].map(v => v + ' <url>')
 handler.tags = ['internet']
-handler.command = /^(pinterest)$/i
+handler.command = /^(pinterest|pin)$/i
+
+handler.limit = 1
 
 module.exports = handler

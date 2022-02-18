b2aa91ca6d3ca4aed843607c51666adb3cdd1688
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-18 01:01:36 -0500

    Update pinterest2.js

--- plugins/pinterest2.js
+++ plugins/pinterest2.js
@@ -1,27 +1,17 @@
 let fetch = require('node-fetch')
-     let handler  = async (m, { conn, args, text }) => {
-     if (!text) throw '*[❗] Que desea buscar?*\n*Inserte un texto*'
- if (text) m.reply('*[ ✔️ ] Buscando, aguarde un momento....*\n\n*[❗] Si llega a dar algun problema intente de nuevo con otro texto*')
-    heum = await require('../lib/scraper').pinterest(text)
-    json = await heum
-    random = json.result[Math.floor(Math.random() * json.result.length)]
-    if (json.result.length == 0) return conn.sendFile(m.chat, './Menu2.jpg', 'error not found', 'ERROR 404 NOT FOUND', m)
-    data = await fetch('https://api.lolhuman.xyz/api/pinterestdl?apikey=56c3f2f2254d87b84051ab78&url='+random).then(v => v.json())
-   get = await conn.getFile(data.data.url)
-   conn.sendMessage(m.chat, get.data, 'imageMessage', { quoted: m, mimetype: get.mime, caption: data.data.url })
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} logo`
+  let res = await fetch(global.API('zeks', '/api/pinimg', {
+    q: text
+  }, 'apikey'))
+  if (!res.ok) throw eror
+  let json = await res.json()
+  if (!json.status) throw json
+  let pint = json.data[Math.floor(Math.random() * json.data.length)];
+  conn.sendFile(m.chat, pint, '', '© stikerin', m, 0, { thumbnail: await (await fetch(pint)).buffer() })
 }
-handler.help = ['pinterest2 <query>']
-handler.tags = ['image']
-handler.command = /^pinterest2$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.limit = false
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
+handler.help = ['pinterest <pencarian>']
+handler.tags = ['internet']
+handler.command = /^(pint(erest)?)$/i
 
 module.exports = handler

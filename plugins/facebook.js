c3348e03eaf046b8cd8a9ced27583d094a545d11
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-18 17:40:20 -0500

    Update facebook.js

--- plugins/facebook.js
+++ plugins/facebook.js
@@ -3,7 +3,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, command, text, usedPrefix }) => {
     if (!text) throw `uhm.. urlnya mana?\n\npenggunaan:\n${usedPrefix + command} url\n\ncontoh:\n${usedPrefix + command} https://www.facebook.com/100003873289819/videos/625313175459585/`
     let res = await fetch(API('amel', '/fb', { url: text }, 'apikey'))
-    if (!res.ok) throw down
+    if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json
     await m.reply(wait)

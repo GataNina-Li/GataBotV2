8b7bcbfc51ca0b06b5ea7da2ec81b0ba07652bd8
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-17 22:46:32 -0500

    Update love.js

--- plugins/love.js
+++ plugins/love.js
@@ -2,8 +2,8 @@ let handler = async (m, { conn, command, text }) => {
   conn.reply(m.chat, `
 *❤️❤️ MEDIDOR DE AMOR ❤️❤️*
 
-*El amor de ${text} por ti es de* *${Math.floor(Math.random() * 100)}%* *de un 100%*
-*Deberias pedirle que sea tu  novia/o ?*
+*💗 El amor 💗 de ${text} por ti es de* *${Math.floor(Math.random() * 100)}%* *de un 100%*
+*Deberias pedirle que sea tu  novio/a?* 😲
 `.trim(), m, m.mentionedJid ? {
     contextInfo: {
       mentionedJid: m.mentionedJid

9b38ad86a357bd6fd51bf1f83b19b728f5543a8b
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-18 18:49:37 -0500

    Update ig.story.js

--- plugins/ig.story.js
+++ plugins/ig.story.js
@@ -1,31 +1,30 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'Uhm...url nya mana?'
-  let res = await fetch(global.API('xteam', '/dl/igs', {
-    nama: args[0]
-  }, 'APIKEY'))
-  let json = await res.json()
-  if (res.status != 200) throw json
-  if (json.result.error) throw json.result.message
-  let { username, storylist } = json.result
-  let dateConfig = {
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
-  }
-  for (let { url, type, taken_at } of storylist)
-    conn.sendFile(m.chat, url, 'ig' + (type == 'video' ? '.mp4' : '.jpg'), `
-@${username}
-Memposting pada ${new Date(taken_at * 1000).toLocaleDateString('id', dateConfig)}
-`, m)
-  throw json.result
-}
-handler.help = ['igstory'].map(v => v + ' <username>')
-handler.tags = ['downloader']
+let axios = require("axios");
+let handler = async(m, { conn, text }) => {
 
-handler.command = /^(igs(tory)?)$/i
+    if (!text) return conn.reply(m.chat, 'Uhm... urlnya mana?', m)
+
+    conn.reply(m.chat, 'Searching...', m)
+        axios.get(`https://st4rz.herokuapp.com/api/ig?url=` + text)
+            .then((res) => {
+              let dl_link = res.data.result.videos
+                    // conn.reply(m.chat, `*Link:* ${dl_link} `, m)
+                conn.sendFile(m.chat, dl_link, 'video.mp4', `*Title:* ${text}\n*Link:* ${dl_link}`, m)
+    })
+}
+handler.help = ['ig']
+handler.tags = ['downloader']
+handler.command = /^ig$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+handler.limit = true
 
 module.exports = handler

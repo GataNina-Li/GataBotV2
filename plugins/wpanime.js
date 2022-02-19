6f6a0cddb980058c2d9e5d4dd1dfe14ccb9d0305
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-18 22:16:36 -0500

    Update wpanime.js

--- plugins/wpanime.js
+++ plugins/wpanime.js
@@ -4,6 +4,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
     if (!text) throw `uhm.. urlnya mana?\n\npenggunaan:\n${usedPrefix + command} url\ncontoh:\n${usedPrefix + command} https://vt.tiktok.com/ZGJBtcsDq/`
     if (!/https?:\/\/(www\.|v(t|m)\.|t\.)?tiktok\.com/i.test(text)) throw `url salah!`
     let res = await fetch(API('amel', '/tiktok', { url: text }, 'apikey'))
+    throw eror = 404
     if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json

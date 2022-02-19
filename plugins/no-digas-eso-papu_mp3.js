1431d6237d84c8b4c25886b0140a2aa585cfb4f4
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-19 16:24:28 -0500

    Create no-digas-eso-papu_mp3.js

---
+++ plugins/no-digas-eso-papu_mp3.js
@@ -0,0 +1,16 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/no-digas-eso-papu.mp3'
+conn.sendFile(m.chat, vn, 'no-digas-eso-papu.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /NO DIGAS ESO PAPU|no digas eso papu|No gigas eso papu|NO PAPU|No papu|NO papu|no papu/i
+handler.command = new RegExp
+
+handler.fail = null
+handler.exp = 100
+module.exports = handler

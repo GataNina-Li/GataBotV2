03b09cdff14d1e4139d49a550dbd60570569297f
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-19 11:53:45 -0500

    Update anana_.mp3.js

--- plugins/anana_.mp3.js
+++ plugins/anana_.mp3.js
@@ -8,6 +8,6 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /ANANA/
+handler.customPrefix = /ANANA/i
 handler.command = new RegExp
 module.exports = handler

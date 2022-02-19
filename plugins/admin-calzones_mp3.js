c6a35530b75d03d17c8a710f45df7a59c0e10a0a
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-19 15:29:41 -0500

    Create admin-calzones_mp3.js

---
+++ plugins/admin-calzones_mp3.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/admin-calzones.mp3'
+conn.sendFile(m.chat, vn, 'admin-calzones.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /TENGO LOS CALZONES|Tengo los calzones|tengo los calzones/i
+handler.command = new RegExp
+module.exports = handler

b98ed5d6f506bf7c64cf62464f907b9fada13c88
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-19 11:49:37 -0500

    Update and rename anana.mp3_.js to anana_.mp3.js

--- plugins/anana_.mp3.js
+++ plugins/anana_.mp3.js
@@ -8,6 +8,6 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /anana/
+handler.customPrefix = /ANANA/
 handler.command = new RegExp
 module.exports = handler

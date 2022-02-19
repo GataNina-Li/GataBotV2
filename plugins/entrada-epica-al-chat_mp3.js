4ff283c7b6ecd4da55b02e431855954b179fd238
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-19 15:48:38 -0500

    Create entrada-epica-al-chat_mp3.js

---
+++ plugins/entrada-epica-al-chat_mp3.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/entrada-epica-al-chat.mp3'
+conn.sendFile(m.chat, vn, 'entrada-epica-al-chat.mp3', null, m, true, {
+type: 'audioMessage', // paksa tanpa convert di ffmpeg
+ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+})
+}
+handler.customPrefix = /Entra épicamente|entra épicamente|ENTRA ÉPICAMENTE|Entrada épica|entrada épica|ENTRADA ÉPICA|/
+handler.command = new RegExp
+module.exports = handler

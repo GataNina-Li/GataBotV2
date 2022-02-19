8af7680356ffdbddb2c017ba7611090b1e7004b0
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-19 12:06:40 -0500

    Update and rename anana.js to anana_mp3.js

--- plugins/anana_mp3.js
+++ plugins/anana_mp3.js
@@ -4,10 +4,10 @@ let path = require('path')
 let handler = async (m, { conn }) => {
 let vn = './media/anana.mp3'
 conn.sendFile(m.chat, vn, 'anana.mp3', null, m, true, {
-type: 'audioMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+type: 'audioMessage', 
+ptt: true
 })
 }
-handler.customPrefix = /anana/i
+handler.customPrefix = /So/
 handler.command = new RegExp
 module.exports = handler

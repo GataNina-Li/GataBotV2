9c3c48d78d1b88d56a3a397dd0220bd3cd2a2dce
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-19 11:39:43 -0500

    Create anana.mp3_.js

---
+++ plugins/anana.mp3_.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/anana.mp3'
+conn.sendFile(m.chat, vn, 'anana.mp3', null, m, true, {
+type: 'audioMessage', // paksa tanpa convert di ffmpeg
+ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+})
+}
+handler.customPrefix = /anana/
+handler.command = new RegExp
+module.exports = handler

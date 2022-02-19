460a5a3e09238b5cf40e114e2f9065499a52d7d5
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-19 14:01:09 -0500

    Create pasen-contexto_mp3.js

---
+++ plugins/pasen-contexto_mp3.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/contexto.mp3'
+conn.sendFile(m.chat, vn, 'contexto.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /Pasen contexto|PASEN CONTEXTO|pasen contexto|Y el contexto|Y EL CONTEXTO|y el contexto/i
+handler.command = new RegExp
+module.exports = handler

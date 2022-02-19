af97b6bfe9772942fa9bc4e1645219f2e76082a9
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-18 21:58:49 -0500

    Update wpanime.js

--- plugins/wpanime.js
+++ plugins/wpanime.js
@@ -1,23 +1,19 @@
-const { MessageType } = require('@adiwajshing/baileys')
-const fetch = require('node-fetch')
+let fetch = require('node-fetch')
 
-let handler = async (m, { conn }) => {
-    try {
-        let res = await fetch(global.API('xteam', '/randomimage/wpmobile', {}, 'APIKEY'))
-        let img = await res.buffer()
-        conn.sendMessage(m.chat, img, MessageType.image, {
-            quoted: m, caption: '*✨The Shadow Brokers - Bot✨*'
-        })
-    } catch (e) {
-        console.log(e)
-        throw '_*El propietario no ha pagado la factura de esta función.*_'
-    }
+let handler = async (m, { conn, command, usedPrefix, text }) => {
+    if (!text) throw `teksnya mana?\n\n${usedPrefix + command} amel|cantik`
+    let text1 = text.split`|`[0]
+    let text2 = text.split`|`[1]
+    if (!text1) throw `teks nya mana?\n\n${usedPrefix + command} amel|cantik`
+    if (!text2) throw `teks2 nya mana?\n\n${usedPrefix + command} amel|cantik`
+    let res = await fetch(API('amel', '/textpro/grafity', { text: text1, text2 }, 'apikey'))
+    if (!res.ok) throw eror
+    let img = await res.buffer()
+    if (!img) throw img
+    conn.sendFile(m.chat, img, 'grafity.jpg', wm, m)
 }
-handler.help = ['wallpaperanime']
-handler.tags = ['internet']
-handler.command = /^(wallpaper|wp)anime$/i
-handler.limit = false
-module.register = false
-module.group = true
+handler.help = ['grafity'].map(v => v + ' <teks>|<teks2>')
+handler.tags = ['maker']
+handler.command = /^(grafity)$/
 
 module.exports = handler

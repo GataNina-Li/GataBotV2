3e1c49fd225046760a4a4cd468b5fea9f25b0d90
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-18 22:21:28 -0500

    Update wpanime.js

--- plugins/wpanime.js
+++ plugins/wpanime.js
@@ -1,20 +1,14 @@
 let fetch = require('node-fetch')
 
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-    if (!text) throw `uhm.. urlnya mana?\n\npenggunaan:\n${usedPrefix + command} url\ncontoh:\n${usedPrefix + command} https://vt.tiktok.com/ZGJBtcsDq/`
-    if (!/https?:\/\/(www\.|v(t|m)\.|t\.)?tiktok\.com/i.test(text)) throw `url salah!`
-    let res = await fetch(API('amel', '/tiktok', { url: text }, 'apikey'))
+let handler = async (m, { conn, usedPrefix, command }) => {
+    let res = await fetch(API('amel', '/nsfwmasturbation', {}, 'apikey'))
     if (!res.ok) throw eror
-    throw eror = 404
     let json = await res.json()
     if (!json.status) throw json
-    await m.reply(wait)
-    await conn.sendFile(m.chat, json.videoSD, 'tiktok.mp3', json.deksripsi, m)
+    conn.sendButtonImg(m.chat, json.url, 'Tobat banh', wm, 'lagi', `${usedPrefix + command}`, m)
 }
-handler.help = ['tiktok'].map(v => v + ' <url>')
-handler.tags = ['download']
-handler.command = /^(tiktok|tt)$/i
-
-handler.limit = 1
+handler.help = ['masturbation']
+handler.tags = ['dewasa']
+handler.command = /^(masturbation)$/i
 
 module.exports = handler

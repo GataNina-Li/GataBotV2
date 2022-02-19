012a85ad92c5f645d8cf451c082929897326bab2
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-19 16:47:13 -0500

    Create masivo-cancion_mp3.js

---
+++ plugins/masivo-cancion_mp3.js
@@ -0,0 +1,16 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/masivo-cancion.mp3'
+conn.sendFile(m.chat, vn, 'masivo-cancion.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /MA MA MASIVO|ma ma masivo|Ma ma masivo|Bv|BV|bv|masivo|Masivo|MASIVO/i
+handler.command = new RegExp
+
+handler.fail = null
+handler.exp = 100
+module.exports = handler

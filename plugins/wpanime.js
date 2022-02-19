c2ff258445efb008dde229e0709a7d5d00e780c9
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-18 22:23:16 -0500

    Update wpanime.js

--- plugins/wpanime.js
+++ plugins/wpanime.js
@@ -1,14 +1,14 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-    let res = await fetch(API('amel', '/nsfwmasturbation', {}, 'apikey'))
+    let res = await fetch(API('amel', '/nsfworgy', {}, 'apikey'))
     if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json
     conn.sendButtonImg(m.chat, json.url, 'Tobat banh', wm, 'lagi', `${usedPrefix + command}`, m)
 }
-handler.help = ['masturbation']
+handler.help = ['orgy']
 handler.tags = ['dewasa']
-handler.command = /^(masturbation)$/i
+handler.command = /^(orgy)$/i
 
 module.exports = handler

1bbc8e8ecca8f35184f5a5072e2450077e08935a
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-19 12:36:27 -0500

    Create flash_mp3.js

---
+++ plugins/flash_mp3.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/flash.mp3'
+conn.sendFile(m.chat, vn, 'flash.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /Me voy|me voy|ME VOY|Me fui|me fui|ME FUI|Se fue|se fue|SE FUE|Adios|adios|ADIOS|Chao|chao|CHAO/i
+handler.command = new RegExp
+module.exports = handler

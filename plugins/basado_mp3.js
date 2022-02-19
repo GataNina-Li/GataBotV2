8390488698b7878fc9694716c9384c4ca6c6292c
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-19 16:54:41 -0500

    Create basado_mp3.js

---
+++ plugins/basado_mp3.js
@@ -0,0 +1,16 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/basado.mp3'
+conn.sendFile(m.chat, vn, 'basado.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /Cada|Basado|Basada|Basadisimo|BASADO|QUE BASADO|QUE BASADA|Que basada|Que basado|basado|basada|/i
+handler.command = new RegExp
+
+handler.fail = null
+handler.exp = 100
+module.exports = handler

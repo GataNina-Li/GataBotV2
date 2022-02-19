e014a39490dad874614b5f79f0a82c6d85f5cc63
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-19 16:15:15 -0500

    Create yoshi-cancion_mp3.js

---
+++ plugins/yoshi-cancion_mp3.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/yoshi-cancion.mp3'
+conn.sendFile(m.chat, vn, 'yoshi-cancion.mp3', null, m, true, {
+type: 'audioMessage', // paksa tanpa convert di ffmpeg
+ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+})
+}
+handler.customPrefix = /YOSHI|Yoshi|YoShi|yoshi|/i
+handler.command = new RegExp
+module.exports = handler

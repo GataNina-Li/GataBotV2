2bea7796f58be15cf97a62906d29a94d098b1609
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-18 22:01:57 -0500

    Update wpanime.js

--- plugins/wpanime.js
+++ plugins/wpanime.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, command, usedPrefix, text }) => {
     if (!text1) throw `teks nya mana?\n\n${usedPrefix + command} amel|cantik`
     if (!text2) throw `teks2 nya mana?\n\n${usedPrefix + command} amel|cantik`
     let res = await fetch(API('amel', '/textpro/grafity2', { text: text1, text2 }, 'apikey'))
-    if (!res.ok) throw eror
+
     let img = await res.buffer()
     if (!img) throw img
     conn.sendFile(m.chat, img, 'grafity2.jpg', wm, m)

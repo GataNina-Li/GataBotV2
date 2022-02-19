10344a66d54ec00e3ae988e76e69ac70c8ddc9c3
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-18 22:00:55 -0500

    Update wpanime.js

--- plugins/wpanime.js
+++ plugins/wpanime.js
@@ -6,14 +6,14 @@ let handler = async (m, { conn, command, usedPrefix, text }) => {
     let text2 = text.split`|`[1]
     if (!text1) throw `teks nya mana?\n\n${usedPrefix + command} amel|cantik`
     if (!text2) throw `teks2 nya mana?\n\n${usedPrefix + command} amel|cantik`
-    let res = await fetch(API('amel', '/textpro/grafity', { text: text1, text2 }, 'apikey'))
+    let res = await fetch(API('amel', '/textpro/grafity2', { text: text1, text2 }, 'apikey'))
     if (!res.ok) throw eror
     let img = await res.buffer()
     if (!img) throw img
-    conn.sendFile(m.chat, img, 'grafity.jpg', wm, m)
+    conn.sendFile(m.chat, img, 'grafity2.jpg', wm, m)
 }
-handler.help = ['grafity'].map(v => v + ' <teks>|<teks2>')
+handler.help = ['grafity2'].map(v => v + ' <teks>|<teks2>')
 handler.tags = ['maker']
-handler.command = /^(grafity)$/
+handler.command = /^(grafity2)$/
 
 module.exports = handler

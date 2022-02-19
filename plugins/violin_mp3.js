d7c9fee6636edbcdcd4e7f880d16f8958219762f
Liliana <94010909+carryfax@users.noreply.github.com>
2022-02-19 13:20:05 -0500

    Create violin_mp3.js

---
+++ plugins/violin_mp3.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/violin.mp3'
+conn.sendFile(m.chat, vn, 'violin.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /Estoy triste|ESTOY TRISTE|estoy triste|Triste|TRISTE|triste|Troste|TROSTE|troste|Truste|TRUSTE|truste/i
+handler.command = new RegExp
+module.exports = handler

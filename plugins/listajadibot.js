62b35eeb4fd1520f81deaf65515c0245258586a2
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-21 20:33:42 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/listajadibot.js
+++ plugins/listajadibot.js
@@ -1,7 +1,7 @@
 async function handler(m, { usedPrefix }) {
   let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
   m.reply( '*🤖 Lista de algunos sub bots (jadibot/serbot) 🤖️*\n\n*🔰 Puedes contactarlos para ver si se unen a tu grupo*\n\n*Considera por favor:*\n*✅ Ser amable*\n*✅ No insistir ni discutir*\n\n*✳ ️Si le aparece el siguiente texto en blanco es que no hay ningún sub bot disponible en este momento inténtelo mas tarde*\n\n*❰ ❗️ ❱ _NOTA: ️SON PERSONAS QUE NO CONOCEMOS. EL EQUIPO DE GATA DIOS NO SE HACE RESPONSABLE DE LO QUE PUEDA OCURRIR._*')
-  m.reply(users.map(v => 'Wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=#estado \n(${v.name})\n\n`).join('\n'))
+  m.reply(users.map(v => 'https://wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=#estado \n(${v.name})\n`).join('\n'))
 }
 handler.command = handler.help = ['listjadibot','bots','subsbots']
 handler.tags = ['jadibot']

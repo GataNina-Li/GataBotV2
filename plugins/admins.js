20277b8e9ef63408ef9e6e8bdef5a26c9ed99fb0
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-22 16:03:35 -0500

    Update admins.js

--- plugins/admins.js
+++ plugins/admins.js
@@ -16,15 +16,15 @@ let handler = async (m, { conn, participants, groupMetadata, args }) => {
         //let { isBanned, welcome, antivirtex, detect, sWelcome, sBye, sPromote, sDemote, antiLink } = global.DATABASE.data.chats[m.chat]
         const groupAdmins = getGroupAdmins(participants)
         let pesan = args.join` `
-        let oi = `Mensaje: ${pesan}`
+        let oi = `*Mensaje:* ${pesan}`
         let listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.split('@')[0]}`).join('\n')
         let text = `
-━「 Invocando admins 」━
+             🔱 *INVOCANDO ADMINS* 🔱
 
 ${oi}
 ${listAdmin}
 
-[ ⚠ ️] Solo usar este comando cuando se trate de una emergencia
+❰ ⚠️ ❱ *Usar este comando en caso de emergencia.* 
 `.trim()
         ownernya = [`${m.chat.split`-`[0]}@s.whatsapp.net`]
         let mentionedJid = groupAdmins.concat(ownernya)

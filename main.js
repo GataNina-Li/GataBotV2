6e514a9da00bcdc88029d219b7c28ddc3eee65b9
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-21 22:27:13 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-require('./config.js')
+require('./config.js') U
 let { WAConnection: _WAConnection } = require('@adiwajshing/baileys')
 let { generate } = require('qrcode-terminal')
 let syntaxerror = require('syntax-error')

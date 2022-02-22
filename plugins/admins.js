b86a23b3dcd3c0a09aa8ae6d43c26c62e8988f8e
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-22 16:04:50 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/admins.js
+++ plugins/admins.js
@@ -19,7 +19,7 @@ let handler = async (m, { conn, participants, groupMetadata, args }) => {
         let oi = `*Mensaje:* ${pesan}`
         let listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.split('@')[0]}`).join('\n')
         let text = `
-             🔱 *INVOCANDO ADMINS* 🔱
+                                   🔱 *INVOCANDO ADMINS* 🔱
 
 ${oi}
 ${listAdmin}

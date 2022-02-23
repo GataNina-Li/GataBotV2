1e116d10a29f6cadbc9664d4e25c64b765f115c5
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-23 13:47:51 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/estado.js
+++ plugins/estado.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
   await conn.send3Button(m.chat, `
 *ミ💖 ¡Hola! estimado/a usuario/a 💖彡*
 
-*ミ📳 Estado del Bot 📳彡*
+*ミ🤖 Estado del Bot 🤖彡*
 *=> ✅ Bot activo*
 *=> ✅ Bot uso público*
 `.trim(), 'Gata Dios', '🌀 MENÚ 🌀', `${usedPrefix}menu`, '🔆 MENÚ SIMPLE 🔆', `${usedPrefix}menusimple`, '♨️ MENÚ DE AUDIOS ♨️', `${usedPrefix}menuaudios`)

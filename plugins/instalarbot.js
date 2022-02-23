f762e033f6e6b8cbb4f0268eed046d3695077fb4
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-23 11:05:30 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/instalarbot.js
+++ plugins/instalarbot.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ⚡▁ ▂ ▄ ▅ ▆ ▇ █ 🚆 █ ▇ ▆ ▅ ▄ ▂ ▁⚡
 
-*Esta Edición del Bot no está disponible por el momento.* u
+*Esta Edición del Bot no está disponible por el momento.* 
 
 ⚡▁ ▂ ▄ ▅ ▆ ▇ █ 🚆 █ ▇ ▆ ▅ ▄ ▂ ▁⚡
                             🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈

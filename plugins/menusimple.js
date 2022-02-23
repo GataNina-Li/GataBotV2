847886f489042f63b5859ddcd393d5dc56d56ad2
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-23 17:11:59 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/menusimple.js
+++ plugins/menusimple.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, usedPrefix }) => {
   let pp = './Menu2.jpg'
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   try {
-//    pp = await conn.getProfilePicture(who)
+//   pp = await conn.getProfilePicture(who)
   } catch (e) {
 
   } finally {

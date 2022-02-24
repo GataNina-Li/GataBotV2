3978b5b903469062e662f6980059db27666f6975
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-24 16:46:27 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/acortar.js
+++ plugins/acortar.js
@@ -1,16 +1,16 @@
 let axios = require("axios");
 let fetch = require('node-fetch')
 let handler = async(m, { conn, text, xteamkey }) => {
-if (!text) throw '*Inserte el link/enlace que desee acortar*'
+if (!text) throw '*Inserte el link/enlace que desee acortar/reducir*'
 let json = await (await fetch(`https://api.xteam.xyz/shorturl/tinyurl?url=${text}&apikey=cb15ed422c71a2fb`)).json()
 if (!json.status) throw json
-let hasil = `*Link acortado correctamente!!*\n\n*Link anterior:*\n${text}\n*Link nuevo:*\n*${json.result}*`.trim()
+let hasil = `✅ *Link acortado correctamente!!*\n\n😺 *Link anterior:*\n${text}\n😸 *Link nuevo:*\n*${json.result}*`.trim()
           
 m.reply(hasil)
 }
 handler.help = ['tinyurl','short2'].map(v => v + ' <link>')
 handler.tags = ['tools']
-handler.command = /^(tinyurl|short2|acortar|corto)$/i
+handler.command = /^(reducir|Reducir|acortar|Acortar)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false

907e63be723087cd017ba8a7494478693abad07f
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-24 00:08:57 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/pajer@.js
+++ plugins/pajer@.js
@@ -11,7 +11,7 @@ _*${text}* *es 😏💦* *${Math.floor(Math.random() * 1039193829)}%* *${command
 handler.help = ['gay', 'lesbi'].map(v => 'how' + v + ' siapa?')
 handler.tags = ['kerang']
 handler.command = /^(pajero|pajera)/i
-handler.owner = false
+handler.owner = false 
 handler.mods = false
 handler.premium = false
 handler.group = false

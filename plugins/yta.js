b9d95f7432e38c2035e791e146827e5165c498b5
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-24 12:08:19 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/yta.js
+++ plugins/yta.js
@@ -8,24 +8,24 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
   //let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   //m.reply(isLimit ? `*🔰 Tamaño del audio: ${filesizeF}*\n\n*✳️ Tamaño máximo para poder enviar: ${limit} MB*\n\n*Puede descargar usted mismo el audio a través de este enlace:*\n*→ ${dl_link}*\n*👉🏻 Al entrar automáticamente se le descargará*` : global.wait)
   await m.reply(`*✳️ Espere un momento, estoy descargando su audio*\n\n*⚠️ Si su audio no es envíado después de 5 minutos, por favor inténtelo nuevamente, si el error perdura intente con un audio de menor tamaño*`)
-  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-❒═════❬ YTMP3 ❭═════╾❒
-┬
-├‣*✨Nombre:* ${title}
-┴
-├‣*📂Tamaño:* ${filesizeF}
-┴
-├‣*${isLimit ? 'Pakai ': ''}Link de descarga (usar si el Bot no manda el archivo mp3):* ${dl_link}
-┴
-`.trim(), m)
-//conn.sendFile(m.chat, dl_link, title + '.mp3', `
+  //conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
 //❒═════❬ YTMP3 ❭═════╾❒
 //┬
 //├‣*✨Nombre:* ${title}
 //┴
 //├‣*📂Tamaño:* ${filesizeF}
 //┴
-//`.trim(), m, null, {
+//├‣*${isLimit ? 'Pakai ': ''}Link de descarga (usar si el Bot no manda el archivo mp3):* ${dl_link}
+//┴
+//`.trim(), m)
+conn.sendFile(m.chat, dl_link, title + '.mp3', `
+❒═════❬ YTMP3 ❭═════╾❒
+┬
+├‣*✨Nombre:* ${title}
+┴
+├‣*📂Tamaño:* ${filesizeF}
+┴
+`.trim(), m, null, {
 //  asDocument: chat.useDocument
 })
 }

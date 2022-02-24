7869d0508a0aa73039ed16a530ba1933a64fa06f
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-24 00:17:28 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/rata.js
+++ plugins/rata.js
@@ -15,7 +15,7 @@ handler.owner = false
 handler.mods = false
 handler.premium = false
 handler.group = false
-handler.private = false
+handler.private = false 
 
 handler.admin = false
 handler.botAdmin = false

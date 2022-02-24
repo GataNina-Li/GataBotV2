6ff13b7217906d147f12da584a9a2526c1a60086
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-24 12:33:28 -0500

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -1,15 +1,15 @@
 let limit = 30
 const { servers, yta } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) return m.reply('Uhm... urlnya mana?')
-  let chat = global.DATABASE._data.chats[m.chat]
+  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+  let chat = global.db.data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : servers[0])
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
 *Title:* ${title}
 *Filesize:* ${filesizeF}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
+*${isLimit ? 'Pakai ': ''}Link:* ${await shortlink(dl_link)}
 `.trim(), m)
   if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
 *Title:* ${title}
@@ -32,5 +32,11 @@ handler.botAdmin = false
 
 handler.fail = null
 handler.exp = 0
+handler.limit = true
 
 module.exports = handler
+
+async function shortlink(url) {
+isurl = /https?:\/\//.test(url)
+return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''
+}

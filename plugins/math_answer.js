bcdf1f0c087852290bfbb29d9697ad1af8fc392f
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-23 23:41:13 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/math_answer.js
+++ plugins/math_answer.js
@@ -3,21 +3,21 @@ let handler = async (m, { conn }) => {
   let id = m.chat
   if (!m.quoted) return
   if (m.quoted.sender != conn.user.jid) return
-  if (!/^Cuanto es el resultado de/i.test(m.quoted.text)) return
+  if (!/^❓ Cuánto es el resultado de/i.test(m.quoted.text)) return
   if (!(m.chat in global.math)) return conn.reply(m.chat, '*Esa pregunta ha terminado*', m)
   if (m.quoted.id == global.math[id][0].id) {
   let math = global.math[id][1]
   if (m.text == math.result) {
-    conn.reply(m.chat, `*Respuesta correcta!*\n+${math.bonus} XP`, m)
+    conn.reply(m.chat, `✅ *Respuesta correcta!*\n+${math.bonus} XP`, m)
     global.DATABASE._data.users[m.sender].exp += math.bonus
     clearTimeout(global.math[id][3])
     delete global.math[id]
   } else {
     if (--global.math[id][2] == 0) {
-      conn.reply(m.chat, `*Se acabo el tiempo!*\nRespuesta: *${math.result}*`, m)
+      conn.reply(m.chat, `🙀 *Se acabo el tiempo!*\nRespuesta: *${math.result}*`, m)
       clearTimeout(global.math[id][3])
       delete global.math[id]
-    } else conn.reply(m.chat, `*Respuesta incorrecta!*\nAun disponible ${global.math[id][2]} oportunidades`, m)
+    } else conn.reply(m.chat, `❎ *Respuesta incorrecta!*\nAun disponible ${global.math[id][2]} oportunidades`, m)
   }
  }
 }

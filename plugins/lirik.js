110a16f02322565963eb5c1331ee2d06fe72864a
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-24 13:21:21 -0500

    Update lirik.js

--- plugins/lirik.js
+++ plugins/lirik.js
@@ -2,7 +2,7 @@ let { MessageType } = require('@adiwajshing/baileys')
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, DevMode }) => {
     try {
-        if (!args || !args[0] || args.length < 1) return m.reply('Judul lagunya apa om?')
+        if (!args || !args[0] || args.length < 1) return m.reply('*[❗] Ingrese un texto*\n\n*Ejemplo:*\n#letra JuanSolo QueridoCorazon*')
         let res = await fetch(global.API('bg', '/lirik', { 
             title: args[0],
             artist: args[1] || '' 
@@ -10,12 +10,13 @@ let handler = async (m, { conn, args, usedPrefix, DevMode }) => {
         let json = await res.json()
         if (json.status !== true) throw json
         m.reply(`
-*Lyrics ${args[0]}*
+*〘 ${args[0]} 〙*
 
+*Letra:*
 \`\`\`${json.result}\`\`\`
 `.trim())
     } catch (e) {
-        m.reply('Error!!')
+        m.reply('*[ ⚠️ ] Error!*\n\n*[❗] Recuerda poner el título/nombre de la canción sin dejar espacio*\n*Ejemplo:*\n*#letra JuanSolo QueridoCorazón*')
         console.log(e)
         if (DevMode) {
             let file = require.resolve(__filename)
@@ -28,6 +29,6 @@ let handler = async (m, { conn, args, usedPrefix, DevMode }) => {
     
 handler.help = ['lirik', 'lyrics'].map(v => ' [title] [artist]')
 handler.tags = ['internet']
-handler.command = /^(l(irik|yrics))$/i
+handler.command = /^(l(irik|yrics)|letra)$/i
 
 module.exports = handler

be7214ec671ad30455679a5e27f603a1bc6071dc
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-24 18:04:37 -0500

    Update ig.story.js

--- plugins/ig.story.js
+++ plugins/ig.story.js
@@ -1,30 +1,23 @@
-let axios = require("axios");
-let handler = async(m, { conn, text }) => {
-
-    if (!text) return conn.reply(m.chat, 'Uhm... urlnya mana?', m)
-
-    conn.reply(m.chat, 'Searching...', m)
-        axios.get(`https://st4rz.herokuapp.com/api/ig?url=` + text)
-            .then((res) => {
-              let dl_link = res.data.result.videos
-                    // conn.reply(m.chat, `*Link:* ${dl_link} `, m)
-                conn.sendFile(m.chat, dl_link, '.mp4', `*Title:* ${text}\n*Link:* ${dl_link}`, m)
-    })
-}
-handler.help = ['ig']
-handler.tags = ['downloader']
-handler.command = /^ig$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
-handler.limit = true
-
+let handler = async (m, { usedPrefix, command, conn, args }) => {
+if (!args[0]) throw `*Formato correcto de uso: ${usedPrefix + command} [username]*\n*Ejemplo: ${usedPrefix + command} luisitocomunica*`
+let res = await igstory(args[0])
+if (!res.length) throw '*[ ⚠️ ] Error, por favor vuelva a intentarlo..*\n*1.- Compruebe que el nombre de usuario este escrito correctamente*\n\n*[❗] Posibles errores por la cual no recibe nada:*\n*1.- Las historias esten en modo pivado o solo mejores amigos*\n*2.- El usuaio no tiene ninguna historia*\n*3.- No se encontro el usuario*'
+for (let { url, type } of res)
+conn.sendFile(m.chat, url, 'ig' + (type == 'video' ? '.mp4' : '.jpg'), `
+@${args[0]}
+`.trim(), m)}
+handler.command = /^(igs(tory)?)$/i
 module.exports = handler
+const axios = require('axios')
+const cheerio = require('cheerio')
+const fetch = require('node-fetch')
+async function igstory(username) {
+username = username.replace(/https:\/\/instagram.com\//g, '')
+let { data } = await axios.get(`https://www.instadownloader.org/data.php?username=${username}&t=${new Date * 1}`)
+const $ = cheerio.load(data)
+let results = []
+$('body > center').each(function (i, el) {
+results.push({
+url: $(el).find('a.download-btn').attr('href'),
+type: $(el).find('video').html() ? 'video' : 'image'})})
+return results}

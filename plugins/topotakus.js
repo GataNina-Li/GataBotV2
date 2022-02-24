85a9348dcd35ad05c9ec39a6c074245ae1bfd122
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-23 23:25:26 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/topotakus.js
+++ plugins/topotakus.js
@@ -31,7 +31,7 @@ function handler(m, { groupMetadata }) {
     let j
     do j = ps[Fl(R() * ps.length)]
     while (b === a)
-    m.reply(`*_🌸 TOP 10 OTAKUS DEL GRUPO 🌸_*
+    m.reply(`*_🌸TOP 10 OTAKUS DEL GRUPO🌸_*
     
 *_1.- 💮${toM(a)}💮_*
 *_2.- 🌷${toM(b)}🌷_*

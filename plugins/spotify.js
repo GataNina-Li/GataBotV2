2a206dcdfa6ac11c22235ba50ab824ce5ed77cc9
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-24 20:06:24 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/spotify.js
+++ plugins/spotify.js
@@ -6,22 +6,26 @@ if (!res.ok) throw await res.text()
 let json = await res.json()
 if(!json.data[0]) throw json
 let { title, artists, album, thumb, url, preview_mp3 } = json.data[0]
-let spotifyi = `❒══════❬ SPOTIFY ❭══════╾❒
-┇
-┇➠ 🌸 *Titulo:* ${title}
-┇
-┇➠ 🗣️ *Artista:* ${artists}
-┇
-┇➠ 🎆 *Album:* ${album}
-┇
-┇➠ ⚡️ *URL*: ${url}
-┇
-┇➠ 💥 *Dirección de URL:* ${preview_mp3}\n┇\n┗╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍❒\n\n*_⏳ Enviando música de previsualización_*\n\n🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈`
+let spotifyi = `❒═════❬ SPOTIFY ❭═════╾❒
+┬
+├‣✨ *Titulo:* ${title}
+┴
+┬
+├‣🗣️ *Artista:* ${artists}
+┴
+┬
+├‣🎆 *Album:* ${album}
+┴
+┬
+├‣🌐 *URL*: ${url}
+┴
+┬
+├‣💚 *Direct URL:* ${preview_mp3}\n┴\n\n*_- Enviando música de previsualización_*\n\n🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈`
 
 await conn.sendFile(m.chat, thumb, '', spotifyi, m)
 await conn.sendFile(m.chat, preview_mp3, 'spotify.mp3', spotifyi, m)
 }
-handler.command = /^(spotify|spotimusica)$/i
+handler.command = /^(spotify|music)$/i
 
 
 module.exports = handler

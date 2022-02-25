0d4d3360058eb542f36e73121a07d3cc64d27213
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-24 19:03:50 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/spotify.js
+++ plugins/spotify.js
@@ -6,7 +6,7 @@ if (!res.ok) throw await res.text()
 let json = await res.json()
 if(!json.data[0]) throw json
 let { title, artists, album, thumb, url, preview_mp3 } = json.data[0]
-let spotifyi = `  ┇❒═════❬ SPOTIFY ❭═════╾❒
+let spotifyi = ` ┇❒═════❬ SPOTIFY ❭═════╾❒
 ┇
 ┇➠ 🌸 *Titulo:* ${title}
 ┇
@@ -16,7 +16,7 @@ let spotifyi = `  ┇❒═════❬ SPOTIFY ❭═════╾❒
 ┇
 ┇➠ ⚡️ *URL*: ${url}
 ┇
-┇➠ 💥 *Dirección de URL:* ${preview_mp3}\n┇\n┗╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍❒\n\n*_⏳ Enviando música de previsualización_*\n\n🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈`
+┇➠ 💥 *Dirección de URL:* ${preview_mp3}\n┇\n┗╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍❒\n\n*_⏳ Enviando música de previsualización_*\n\n🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈`
 
 await conn.sendFile(m.chat, thumb, '', spotifyi, m)
 await conn.sendFile(m.chat, preview_mp3, 'spotify.mp3', spotifyi, m)

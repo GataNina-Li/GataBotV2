fbb004707e6829369b81a6527f232057b074119c
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-25 14:05:19 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/promote.js
+++ plugins/promote.js
@@ -1,14 +1,14 @@
 let handler = async (m, { conn,usedPrefix, text, args }) => {
 	if(isNaN(text) && !text.match(/@/g)){
-		return conn.reply(m.chat, `*[❗] USO APROPIADO*\n\n*#daradmin @tag*\n*#daradmin -> responder a un mensaje*`, m)
+		return conn.reply(m.chat, `*❰ ❗️ ❱ USO CORRECTO*\n\n*#daradmin @tag*\n*#darpoder ➡️ responder a un mensaje*`, m)
 	}else if(isNaN(text)) {
 		var number = text.split`@`[1]
 	}else if(!isNaN(text)) {
 		var number = text
 	}
 	
-	if(!text && !m.quoted) return conn.reply(m.chat, `*[❗] USO APROPIADO*\n\n*┯┷*\n*┠≽ ${usedPrefix}daradmin @tag*\n*┠≽ ${usedPrefix}daradmin -> responder a un mensaje*\n*┷┯*`, m)
-	if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `*[ ⚠️ ] El número ingresado es incorrecto, por favor ingrese el número correcto*`, m)
+	if(!text && !m.quoted) return conn.reply(m.chat, `*❰ ❗️ ❱ USO APROPIADO*\n\n*┯┷*\n*┠≽ ${usedPrefix}daradmin @tag*\n*┠≽ ${usedPrefix}darpoder ➡️ responder a un mensaje*\n*┷┯*`, m)
+	if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `*❰ ⚠️ ❱ El número ingresado es incorrecto, por favor ingrese el número correcto*`, m)
 	
 try {
 	if(text) {
@@ -21,12 +21,12 @@ try {
 } catch (e) {
 		} finally {
 			conn.groupMakeAdmin(m.chat, [user]).catch(console.log)
-			conn.reply(m.chat, `*[ ✅ ] ÓRDENES RECIBIDAS*`, m)
+			conn.reply(m.chat, `*✅ ÓRDENES RECIBIDAS*`, m)
 	}	
 }
 handler.help = ['*593xxx*','*@usuario*','*responder chat*'].map(v => 'promote ' + v)
 handler.tags = ['group']
-handler.command = /^(promote|daradmin|admin|darpoder)$/i
+handler.command = /^(promote|daradmin|darpoder)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false

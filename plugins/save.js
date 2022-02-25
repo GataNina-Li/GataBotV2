4f03b376701446f97375008dd636da42ffa0388e
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-25 13:33:17 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/save.js
+++ plugins/save.js
@@ -3,13 +3,13 @@ let handler = async (m, { conn, text }) => {
   let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
-  if (!who) throw '*[❗] Etiqueta a una persona*\n\n*Uso correcto de comando:*\n*#save @tag Shadow - Bot*'
+  if (!who) throw '❰ ❗️ ❱ *Etiqueta a una persona*\n\n*Uso correcto de comando:*\n*#save @tag 🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈*'
   txt = text.replace('@' + who.split`@`[0], '').trimStart()
   return conn.sendContact(m.chat, who, txt || conn.getName(who), m)
 }
 handler.help = ['save'].map(v => v + ' @mention <ContactName>')
 handler.tags = ['']
 
-handler.command = /^save$/
+handler.command = /^save|agendar|guardar$/
 
 module.exports = handler

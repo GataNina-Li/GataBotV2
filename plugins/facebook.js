d3ce0d4806703def11dcd858230df5bd9579701b
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-25 00:04:02 -0500

    Update facebook.js

--- plugins/facebook.js
+++ plugins/facebook.js
@@ -1,20 +1,53 @@
-const fetch = require('node-fetch')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://www.facebook.com/alanwalkermusic/videos/277641643524720`
-  if (/^https?:\/\/.*(fb.watch|facebook.com)/i.test(m.text)) throw `url salah`
+let fetch = require('node-fetch')
 
-  let res = await fetch(API('neoxr', '/api/download/fb', { url: args[0] }, 'apikey'))
-  if (!res.ok) throw eror
+let handler = async (m, { conn, args }) => {
+  if (!args[0]) throw 'Uhm...url nya mana?'
+  let res = await fetch(global.API('xteam', '/dl/fb', {
+    url: args[0]
+  }, 'APIKEY'))
+  if (res.status !== 200) {
+    res.text()
+    throw res.status
+  }
   let json = await res.json()
-  if (!json.status) throw json
-  await m.reply(wait)
-  await conn.sendFile(m.chat, json.data.sd.url, '', `HD: ${json.data.hd.url}\nUkuran: ${json.data.hd.size}\n\n© stikerin`, m)
+  if (!json.result) throw json
+  let { name, author, description, uploadDate, duration, url, isFamilyFriendly, genre, keywords, contentSize, videoQuality, commentCount } = json.result
+  let { name: authorname, url: authorlink } = author || {}
+  let dateConfig = {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  }
+  let unknown = '_Unknown_'
+  let none = '_None_'
+  let caption = `
+Konten${isFamilyFriendly ? ' ' : ' *Tidak* '}Family Friendly
+Post oleh ${name} (${authorname || ''}) (${authorlink || ''})
+Diposting pada ${new Date(uploadDate).toLocaleDateString('id', dateConfig)}
+Size: ${contentSize || unknown}
+Durasi: ${clockString(+ new Date(duration))}
+Genre: ${genre || none}
+Kualitas: ${videoQuality ? videoQuality : unknown}
+
+${description}
+
+Keyword: ${keywords || none}
+`.trim()
+  conn.sendFile(m.chat, url, 'media-fb', caption, m)
 }
 handler.help = ['fb'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
 handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
 
-handler.limit = true
-
 module.exports = handler
+
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
+}

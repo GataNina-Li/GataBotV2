c69aba97a1f5bf9bdceb3814978070f2801a311f
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-24 23:30:23 -0500

    Update menu de comandos.js

--- plugins/menu de comandos.js
+++ plugins/menu de comandos.js
@@ -133,8 +133,9 @@ let handler = async (m, { conn, usedPrefix }) => {
 ┣ ↠☀️ _${usedPrefix}wpanime | fondoanime_
 ┣ ↠❄️ _${usedPrefix}verinstagram | verig |igver *usuario*_
 ┣ ↠☀️ _${usedPrefix}ighistorias|historiasig *usuario*_
-┣ ↠❄️ _${usedPrefix}wikipedia | wiki | internet *texto*_
-┣ ↠☀️ _${usedPrefix}spotify | spotimusica *autor, cancion*_
+┣ ↠❄️ _${usedPrefix}twittervideo | twvid *link de twitter*
+┣ ↠☀️ _${usedPrefix}wikipedia | wiki | internet *texto*_
+┣ ↠❄️ _${usedPrefix}spotify | spotimusica *autor, cancion*_
 ┗━━━━━━━━━━━━━━━━━━┛
 ┏━━━━━━━━━━━━━━━━━━┓
 ┃ *GESTION DE GRUPOS* 

5f5ca9aad91ce171c0c581d47a2a6be382928a28
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-24 22:55:50 -0500

    Update yt-comment.js

--- plugins/yt-comment.js
+++ plugins/yt-comment.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw '❰ ❗️ ❱ *Estas usando mal el comando!!*\n*Ejemplo:* *${usedPrefix}${command}* Gata Dios'
+  if (!text) throw '❰ ❗️ ❱ *Estas usando mal el comando!!*\n*Ejemplo:* ${usedPrefix + command} Gata Dios'
   conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/youtube-comment', {
     avatar: await conn.getProfilePicture(m.sender).catch(_ => ''),
     comment: text,

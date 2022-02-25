103b7e62363f9824dccca060a1ee6c178dede110
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-25 17:04:50 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/setdesc.js
+++ plugins/setdesc.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, args }) => {
  await conn.groupUpdateDescription(m.chat, `${args.join(" ")}`);
-  m.reply('*✅ La descripción del grupo se modifico correctamente*')
+  m.reply('*✅ La descripción del grupo se modifico correctamente*') 
 }
 
 handler.help = ['Setdesk <text>']

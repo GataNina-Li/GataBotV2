19d76e9644f9f5033313b4e2b5a09cf30f0a3aef
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-25 13:20:16 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/add.js
+++ plugins/add.js
@@ -26,7 +26,7 @@ let handler = async (m, { conn, text, participants, usedPrefix, command }) => {
         mentionedJid: conn.parseMention(teks)
       }
     })
-    await conn.sendGroupV4Invite(m.chat, jid, invite_code, invite_code_exp, false, 'Hey!! Hola, me presento, soy The Shadow Brokers - Bot, y soy un Bot para WhatsApp, una persona del grupo utilizo el comando para añadirte al grupo, pero no pude agregarte, asi que te mando la invitacion para que te agregues, te esperamos!!', jpegThumbnail ? {
+    await conn.sendGroupV4Invite(m.chat, jid, invite_code, invite_code_exp, false, '¡Hola!, me presento, soy 🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈, y soy un Bot para WhatsApp, una persona del grupo utilizo el comando para añadirte al grupo, pero no pude agregarte, asi que te mando la invitacion para que te agregues, te esperamos!!', jpegThumbnail ? {
       jpegThumbnail
     } : {})
   }

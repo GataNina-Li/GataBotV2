786359da5b8f6037e2dd71d172b6a55812d30bc1
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-25 16:48:28 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/setwelcome.js
+++ plugins/setwelcome.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text }) => {
   if (text) {
     conn.welcome = text
     m.reply('Bienvenida configurada correctamente\n Use @user para mencionar al usuario')
-  } else throw 'Teksnya mana?'
+  } else throw 'Y el texto?'
 }
 handler.help = ['setwelcome <teks>']
 handler.tags = ['owner']

fa6c470ffca2649656f34654e3550d24be6fe76a
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-25 13:24:36 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/kick.js
+++ plugins/kick.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args }) => {
   let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
   for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
 }
-handler.help = ['kick', '-'].map(v => v + ' @user')
+handler.help = ['kick', '-'].map(v => v + ' @user') 
 handler.tags = ['admin']
 handler.command = /^(kick|sacar|\-)$/i
 handler.owner = false

fb4230b1017df3692b4be9ac932864f22f6f4eca
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-26 17:24:59 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/tomp3.js
+++ plugins/tomp3.js
@@ -4,7 +4,7 @@ const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-  if (!/video|audio/.test(mime)) throw `*🔰 Responda a un video o nota de voz con el comando* *${usedPrefix + command}*`
+  if (!/video|audio/.test(mime)) throw `❰ ❗️ ❱ *Responda a un video o nota de voz con el comando* *${usedPrefix + command}*`
   let media = await q.download()
   let audio = await toAudio(media, 'mp4')
   conn.sendMessage(m.chat, audio, MessageType.audio, {

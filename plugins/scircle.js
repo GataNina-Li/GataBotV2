53a10420e9fd01e5b0c2da165db8a24a44552f33
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-26 14:46:30 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/scircle.js
+++ plugins/scircle.js
@@ -11,16 +11,16 @@ let handler = async (m, { conn, text }) => {
   let img = await q.download()
   let url = await uploadImage(img)
   let wanted = global.API('dzx', '/api/canvas/circle', { url }) //`https://api.dhamzxploit.my.id/api/canvas/circle?url=${url}`
-  let stiker = await sticker(null, wanted, 'circle', '©The Shadow Brokers - Bot')
+  let stiker = await sticker(null, wanted, 'Circulo', 'GataBot 🐈 - Gata Dios')
   conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })
  } catch (e) {
-   m.reply('*_Conversion fallida_*\n\n*_Vuelva a intentarlo_*\n\n*_Recuerde enviar una imagen con el comando #circle o responder a la imagen con el comando_*')
+   m.reply('*_Conversion fallida_*\n\n*_Vuelva a intentarlo_*\n\n*_Recuerde responder a una imagen con el comando #circle_*')
   }
 }
 handler.help = ['circle']
 handler.tags = ['sticker']
-handler.command = /^circle$/i
+handler.command = /^circle|circulo$/i
 
 module.exports = handler

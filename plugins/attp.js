6bf44dc08593b22fc4d4975d5b82f51b0ee2f83e
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-26 15:24:01 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/attp.js
+++ plugins/attp.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
   conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
 //conn.reply(m.chat, `
 //*_EL SERVIDOR DONDE SE CREA LOS STICKERS ESTA CON FALLAS Y AVECES SOLO MANDA UN STICKER SIN NADA, ESTE ERROR NO TIENE SOLUCIÓN PRONTA QUE YO LE PUEDA DAR YA QUE NO TENO ACCESO A ESE SERVIDOR. AHORA SI QUE ES CUESTION DE SUERTE CUANDO FUNCIONE, LOS COMANDOS #attp2, #ttp2, #ttp4, #ttp5, #ttp6 y #ttpdark SIGUEN FUNCIONANDO CON NORMALIDAD_*
-//`.trim(), m)
+//`.trim(), m)  
 }
 handler.help = ['attp <teks>']
 handler.tags = ['sticker']

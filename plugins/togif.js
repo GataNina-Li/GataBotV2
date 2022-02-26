0841d7851fc238d2d017b92fc99e565bd6707c9c
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-26 17:33:49 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/togif.js
+++ plugins/togif.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     if (/webp/.test(mime)) {
         out = await webp2mp4(media)
     }
-    await conn.sendFile(m.chat, out, 'out.gif', '©The Shadow Brokers - Bot', m, 0, { mimetype: 'video/gif', thumbnail: Buffer.alloc(0) })
+    await conn.sendFile(m.chat, out, 'out.gif', '🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈', m, 0, { mimetype: 'video/gif', thumbnail: Buffer.alloc(0) })
 }
 handler.help = ['togif']
 handler.tags = ['sticker']

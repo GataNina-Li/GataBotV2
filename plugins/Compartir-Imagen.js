2066fb4e5e8c69b90dc5bba9ecf175a011e2d82f
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-26 02:51:00 -0500

    Update Compartir-Imagen.js

--- plugins/Compartir-Imagen.js
+++ plugins/Compartir-Imagen.js
@@ -4,7 +4,7 @@ let res = await fetch('https://caliphapi.com/api/ppcouple?apikey=BNeGhnOu')
 if (res.status != 200) throw await res.text()
 let json = await res.json()
 if (!json.status) throw json
-conn.sendButtonImg(m.chat, json.result.female,  'Chica', '🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈', 'Next', '.ppcp', m)
-conn.sendButtonImg(m.chat, json.result.male, 'Chico', '🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈', 'Next', '.ppcp', m)}
-handler.command = /^ppcp$/i
+conn.sendButtonImg(m.chat, json.result.female,  'Chica 🙆‍♀️', '🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈', 'Siguiente 🔄', '${command}', m)
+conn.sendButtonImg(m.chat, json.result.male, 'Chico 🙆‍♂️', '🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈', 'Siguiente 🔄', '${command}', m)}
+handler.command = /^compartirfoto|compfoto$/i
 module.exports = handler

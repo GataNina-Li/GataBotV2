071e1547eed21a4c4b2b0706db5fb35b4900b4ce
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 18:55:28 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/readmore.js
+++ plugins/readmore.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text }) => {
 }
 handler.help = ['readmore','spoiler'].map(v => v + ' <teks>|<teks>')
 handler.tags = ['tools']
-handler.command = /^(spoiler|hidetext|readmore|selengkapnya)$/i
+handler.command = /^(spoiler|hidetext|readmore|leermas)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false

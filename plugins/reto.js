dfd4fdbcf69fb9882f3b29cada1c1eef124d7cf1
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 03:38:57 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/reto.js
+++ plugins/reto.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-  conn.reply(m.chat,`*┌────「 RETO 」─*\n*“${pickRandom(global.bucin)}”*\n*└────「 𝐒𝐇𝐀𝐃𝐎𝐖 」─*`, m)
+  conn.reply(m.chat,`*┌────「 *RETO* 」─*\n*“${pickRandom(global.bucin)}”*\n*└────「 🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈 」─*`, m)
 }
 handler.help = ['reto']
 handler.tags = ['games']

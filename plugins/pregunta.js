5a15b64b9572a5bf35aa9e68045a12f65b8031e9
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 16:30:31 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/pregunta.js
+++ plugins/pregunta.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, command, text }) => {
 *❓ PREGUNTAS ❓*
   
 😺 *Pregunta:* ${text}
-🙀 *Respuesta:* ${pickRandom(['Si','Tal vez sí','Tal vez no'.'Posiblemente','Probablemente no','Probablemente no'.'Puede ser'.'No puede ser'.'No','Imposible'.'Depende'.'Creo que si'.'Creo que no'.'Claro, no lo dudes'.'Hasta yo lo dudo'.'No tengo palabras jajaja'.'Es altamente posible'.'Es bajamente posible'])}
+🙀 *Respuesta:* ${pickRandom(['Si','Tal vez sí','Tal vez no','Posiblemente','Probablemente no','Probablemente no','Puede ser','No puede ser','No','Imposible','Depende','Creo que si','Creo que no','Claro no lo dudes','Hasta yo lo dudo','No tengo palabras jajaja','Es altamente posible','Es bajamente posible'])}
 `.trim(), m, m.mentionedJid ? {
     contextInfo: {
       mentionedJid: m.mentionedJid

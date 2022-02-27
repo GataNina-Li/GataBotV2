c07c88a3286895d1d4b75224c94ec849fab047dc
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 04:34:25 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/kpop itzy.js
+++ plugins/kpop itzy.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 let handler = async(m, { conn }) => {
 let porn = await axios.get('https://meme-api.herokuapp.com/gimme/Itzy')
            conn.sendFile(m.chat, `${porn.data.url}`, '', `${porn.data.title}
- 🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈`, m)
+🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈`, m)
   }
 handler.help = ['kpopitzy']
 handler.tags = ['images']

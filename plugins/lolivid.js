c5f1313bfe6c8dd8c2766cd9b6afa16e6b825ee8
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 04:05:35 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/lolivid.js
+++ plugins/lolivid.js
@@ -5,6 +5,6 @@ return Math.floor(Math.random() * url)
 let handler  = async (m, { conn }) => {
 conn.sendFile(m.chat, `https://recoders-area.caliph.repl.co/api/lolivid`, '', ``, m)
 }
-handler.command = /^(lolivid|lolivideo)$/i
+handler.command = /^(lolivid|lolivideo)$/i 
 
 module.exports = handler

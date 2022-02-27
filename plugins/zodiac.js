d120f175038299f1ab1ea43e0604fc72d632aeb1
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 18:43:42 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/zodiac.js
+++ plugins/zodiac.js
@@ -2,7 +2,7 @@ let handler = (m, { usedPrefix, command, text }) => {
     if (!text) throw `Ejemplo:\n${usedPrefix + command} 2003 02 25`
 
     const date = new Date(text)
-    if (date == 'Fecha invalida, prueba con el siguiente formato AAAA MM DD Ejemplo: 2003 02 07 ') throw date
+    if (date == '*Fecha invalida, prueba con el siguiente formato AAAA MM DD Ejemplo: 2003 02 07*') throw date
     const d = new Date()
     const [tahun, bulan, tanggal] = [d.getFullYear(), d.getMonth() + 1, d.getDate()]
     const birth = [date.getFullYear(), date.getMonth() + 1, date.getDate()]
@@ -12,37 +12,38 @@ let handler = (m, { usedPrefix, command, text }) => {
     const age = ageD.getFullYear() - new Date(1970, 0, 1).getFullYear()
 
     const birthday = [tahun + (birth[1] < bulan), ...birth.slice(1)]
-    const cekusia = bulan === birth[1] && tanggal === birth[2] ? `${age} - Feliz cumpleaños 🥳` : age
+    const cekusia = bulan === birth[1] && tanggal === birth[2] ? `${age} - *¡Feliz cumpleaños!* 🥳` : age
 
     const teks = `
-Fecha de nacimiento: : ${birth.join('-')}
-Proximo cumpleaños : ${birthday.join('-')}
-Edad : ${cekusia}
-Signo zodical : ${zodiac}
+✨ Fecha de nacimiento: : ${birth.join('-')}
+✨ Proximo cumpleaños : ${birthday.join('-')}
+✨ Edad : ${cekusia}
+✨ Signo zodical : ${zodiac}
+🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈
 `.trim()
     m.reply(teks)
 }
 handler.help = ['zodiac *2002 02 25*']
 handler.tags = ['General']
 
-handler.command = /^zodia[kc]$/i
+handler.command = /^zodiac|zodiaco|zodiacal$/i
 
 module.exports = handler
 
 const zodiak = [
-    ["Capricornio", new Date(1970, 0, 1)],
-    ["Acuario", new Date(1970, 0, 20)],
-    ["Piscis", new Date(1970, 1, 19)],
-    ["Aries", new Date(1970, 2, 21)],
-    ["Tauro", new Date(1970, 3, 21)],
-    ["Geminis", new Date(1970, 4, 21)],
-    ["Cancer", new Date(1970, 5, 22)],
-    ["Leo", new Date(1970, 6, 23)],
-    ["Virgo", new Date(1970, 7, 23)],
-    ["Libra", new Date(1970, 8, 23)],
-    ["Scorpion", new Date(1970, 9, 23)],
-    ["Sagitario", new Date(1970, 10, 22)],
-    ["Capricornio", new Date(1970, 11, 22)]
+    ["Capricornio ♑️", new Date(1970, 0, 1)],
+    ["Acuario ♒️", new Date(1970, 0, 20)],
+    ["Piscis ♓️", new Date(1970, 1, 19)],
+    ["Aries ♈️", new Date(1970, 2, 21)],
+    ["Tauro ♉️", new Date(1970, 3, 21)],
+    ["Geminis ♊️", new Date(1970, 4, 21)],
+    ["Cancer ♋️", new Date(1970, 5, 22)],
+    ["Leo ♌️", new Date(1970, 6, 23)],
+    ["Virgo ♍️", new Date(1970, 7, 23)],
+    ["Libra ♎️", new Date(1970, 8, 23)],
+    ["Scorpion ♏️", new Date(1970, 9, 23)],
+    ["Sagitario ♐️", new Date(1970, 10, 22)],
+    ["Capricornio ♑️", new Date(1970, 11, 22)]
 ].reverse()
 
 function getZodiac(month, day) {

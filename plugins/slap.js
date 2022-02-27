b8ec9af0cf9bcf278a5fbdc07e854437bb6b7bd4
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 05:12:01 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/slap.js
+++ plugins/slap.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn}) => {
   let { 
 url
 } = json
-let stiker = await sticker(null, url, 'Slap', '©The Shadow Brokers - Bot')
+let stiker = await sticker(null, url, 'Slap', 'GataBot 🐈 - Gata Dios')
   conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })

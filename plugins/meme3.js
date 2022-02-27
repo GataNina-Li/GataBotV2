77e9f5f5de47b3cc20d5f756dd138e2b3fa73ccc
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-26 22:38:06 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/meme3.js
+++ plugins/meme3.js
@@ -1,7 +1,8 @@
 const axios = require('axios')
  let handler = async(m, { conn }) => {
 let les = await axios.get('https://meme-api.herokuapp.com/gimme/mememexico')
-            conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
+            conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}
+  🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈`, m)
   }
 handler.help = ['meme3']
 handler.tags = ['images']

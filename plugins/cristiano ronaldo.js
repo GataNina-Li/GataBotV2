d74b790e461c21b9f20a5f16b24c84de7b5014f1
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 15:58:05 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/cristiano ronaldo.js
+++ plugins/cristiano ronaldo.js
@@ -1,11 +1,12 @@
 const axios = require('axios')
  let handler = async(m, { conn }) => {
 let les = await axios.get('https://meme-api.herokuapp.com/gimme/Cristianoronaldo')
-            conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
+            conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}
+🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈`, m)
   }
 handler.help = ['CristianoRonaldo']
 handler.tags = ['images']
-handler.command = /^(cristianoronaldo)$/i
+handler.command = /^(cristianoronaldo|CristianoRonaldo|CRISTIANORONALDO)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false

e0291ed05d08ee6500f1fed3b0b68d158133fb35
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 04:38:35 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/navidad.js
+++ plugins/navidad.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 let handler = async(m, { conn }) => {
 let porn = await axios.get('https://meme-api.herokuapp.com/gimme/Christmas')
            conn.sendFile(m.chat, `${porn.data.url}`, '', `${porn.data.title}
-🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨`, m)
+🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈 `, m)
   }
 handler.help = ['Navidad']
 handler.tags = ['images']

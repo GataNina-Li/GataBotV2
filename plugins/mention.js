6eb327d77713cce3fb74abf3b4f7d7ced88725b2
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 16:36:00 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/mention.js
+++ plugins/mention.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text }) => {
-  if (!text) throw '*[❗] Agregue el texto que enviará el Bot*'
+  if (!text) throw '❰ ❗️ ❱ *Agregue el texto que enviará el Bot*'
   m.reply(text, false, {
     contextInfo: {
       mentionedJid: conn.parseMention(text)
@@ -9,6 +9,6 @@ let handler = async (m, { conn, text }) => {
 handler.help = ['mention <teks>']
 handler.tags = ['tools']
 
-handler.command = /^mention$/i
+handler.command = /^mention|mencionar|mensaje$/i
 
 module.exports = handler

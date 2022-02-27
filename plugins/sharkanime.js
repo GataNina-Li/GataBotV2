10968f837cd972cf33b94465bad58f398d961542
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 04:51:55 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/sharkanime.js
+++ plugins/sharkanime.js
@@ -2,7 +2,7 @@ const axios = require('axios')
  let handler = async(m, { conn }) => {
 let les = await axios.get('https://meme-api.herokuapp.com/gimme/GawrGura')
             conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}
-🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨`🐈, m)
+🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨🐈`, m)
   }
 handler.help = ['gawrgura']
 handler.tags = ['images']

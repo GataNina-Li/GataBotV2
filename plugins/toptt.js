4c164880af7c53ec2361d1a0deb732d986f06009
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-28 14:28:32 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/toptt.js
+++ plugins/toptt.js
@@ -4,7 +4,7 @@ const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-  if (!/video|audio/.test(mime)) throw `Balas audio yang ingin diubah ke voice note dengan caption *${usedPrefix + command}*`
+  if (!/video|audio/.test(mime)) throw `*Responda a un audio que desea convertir a una nota de voz con ${usedPrefix + command}*`
   let media = await q.download()
   let audio = await toPTT(media, 'mp4')
   conn.sendMessage(m.chat, audio, MessageType.audio, {

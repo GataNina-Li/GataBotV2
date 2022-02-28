62ca3924eb3769a9bf40bb23a41db585671fec6b
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 22:47:35 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/cancion.js
+++ plugins/cancion.js
@@ -8,7 +8,7 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /Marica tu|cancion1|Marica quien/i
+handler.customPrefix = /Marica tu|cancion1|Marica quien/i 
 handler.command = new RegExp
 
 handler.fail = null

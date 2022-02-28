cb32e1bea2ba98aefe3cb10c4181a7c33907a711
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 20:33:18 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/hentairandom.js
+++ plugins/hentairandom.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
     heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/hentai?apikey=apirey`)
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '*Hentai By TheBrokers*', 'Gata Dios', 'SIGUIENTE 🔄', `${usedPrefix + command}`, m, false)
+   conn.sendButtonImg(m.chat, json, '*Hentai 🥵*', 'Gata Dios', 'SIGUIENTE 🔄', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(hentai|hentay)$/i
 

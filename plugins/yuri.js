ee926164edcc330b9486cd5da25fb48b24721422
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 20:46:29 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/yuri.js
+++ plugins/yuri.js
@@ -5,7 +5,7 @@ let les = await axios.get('https://meme-api.herokuapp.com/gimme/yuri')
   }
 handler.help = ['yuri']
 handler.tags = ['General']
-handler.command = /^(yuri)$/i
+handler.command = /^(yuri)$/i 
 handler.owner = false
 handler.mods = false
 handler.premium = false

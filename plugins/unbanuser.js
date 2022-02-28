d32575a338ffa735388a6aaf49addebcc20e2d83
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-28 13:09:01 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/unbanuser.js
+++ plugins/unbanuser.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, text}) => {
 }
 handler.help = ['ban']
 handler.tags = ['owner']
-handler.command = /^unbanuser$/i
+handler.command = /^unbanuser|unbanusuario$/i
 handler.rowner = true
 
 module.exports = handler

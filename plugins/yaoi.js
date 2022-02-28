e1b605c2b905ea82c47ce3286b998046be04105a
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 20:44:08 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/yaoi.js
+++ plugins/yaoi.js
@@ -5,7 +5,7 @@ let les = await axios.get('https://meme-api.herokuapp.com/gimme/yaoigif')
   }
 handler.help = ['yaoigif']
 handler.tags = ['images']
-handler.command = /^(yaoigif)$/i
+handler.command = /^(yaoigif)$/i 
 handler.owner = false
 handler.mods = false
 handler.premium = false

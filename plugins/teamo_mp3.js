89fa43a466d1973128056f6e92da37d8263c3f33
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 22:30:01 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/teamo_mp3.js
+++ plugins/teamo_mp3.js
@@ -8,7 +8,7 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /bot te amo|botteamo|boteamo|te quiero bot/i
+handler.customPrefix = /bot te amo|botteamo|boteamo|te quiero bot/i 
 handler.command = new RegExp
 
 handler.fail = null

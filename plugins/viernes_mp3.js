1cd31f73542a6130d4471cea1c4150dcfacbc418
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 21:50:19 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/viernes_mp3.js
+++ plugins/viernes_mp3.js
@@ -8,6 +8,6 @@ type: 'audioMessage',
 ptt: true 
 })
 }
-handler.customPrefix = /fiesta viernes|viernes|Viernes|viernes fiesta/
+handler.customPrefix = /fiesta viernes|viernes|Viernes|viernes fiesta/ 
 handler.command = new RegExp
 module.exports = handler

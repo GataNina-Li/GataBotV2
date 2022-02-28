c0258893bf96a876fcf59647f4be1b9f987b7b90
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 21:49:24 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/fiesta.js
+++ plugins/fiesta.js
@@ -8,7 +8,7 @@ type: 'audioMessage',
 ptt: true 
 })
 }
-handler.customPrefix = /fiesta del admin 3|atención grupo|atencion grupo|aviso importante|fiestadeladmin3|fiesta en casa de uriel/i
+handler.customPrefix = /fiesta del admin 3|atención grupo|atencion grupo|aviso importante|fiestadeladmin3|fiesta en casa de uriel/i 
 handler.command = new RegExp
 
 

ee0f40cb264f812ab0ae525dd2f9ca6c137aa8ea
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 23:02:30 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/elmo.js
+++ plugins/elmo.js
@@ -10,4 +10,4 @@ ptt: true
 }
 handler.customPrefix = /Pasa pack|vendes tu nudes|pasa video hot|pasa tu pack|pasa fotos hot|vendes tu pack|Vendes tu pack|Vendes tu pack?|vendes tu pack|Pasa Pack Bot|pasa pack Bot|pasa tu pack Bot|Pásame tus fotos desnudas|pásame tu pack|me pasas tu pak|me pasas tu pack|pasa pack/
 handler.command = new RegExp
-module.exports = handler
+module.exports = handler 

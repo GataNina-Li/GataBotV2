18caca19767fbb2b9497eaf7a4848ecc1e2165cd
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-28 01:06:41 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/anonymous_chat.js
+++ plugins/anonymous_chat.js
@@ -7,10 +7,10 @@ async function handler(m, { command }) {
         case 'next':
         case 'leave': {
             let room = Object.values(this.anonymous).find(room => room.check(m.sender))
-            if (!room) throw 'No estás en un chat anónimo'
-            m.reply('Ok.. ha salido exitosamente')
+            if (!room) throw 'No estás en un Chat Anónimo'
+            m.reply('Ha salido exitosamente del Chat Anónimo')
             let other = room.other(m.sender)
-            if (other) this.sendMessage(other, 'El otro usuario decidio abandonar el chat', MessageType.text)
+            if (other) this.sendMessage(other, 'El otro usuario ha abandonado el chat', MessageType.text)
             delete this.anonymous[room.id]
             if (command === 'leave') break
         }
@@ -18,10 +18,10 @@ async function handler(m, { command }) {
             if (Object.values(this.anonymous).find(room => room.check(m.sender))) throw 'Sigues en el chat anónimo'
             let room = Object.values(this.anonymous).find(room => room.state === 'WAITING' && !room.check(m.sender))
             if (room) {
-                this.sendMessage(room.a, 'Se conecto exitosamente con un chat anonimo!', MessageType.text)
+                this.sendMessage(room.a, '¡Se conectó exitosamente con un Chat Anónimo!', MessageType.text)
                 room.b = m.sender
                 room.state = 'CHATTING'
-                m.reply('Se conecto exitosamente con un chat anonimo!')
+                m.reply('¡Se conectó exitosamente con un Chat Anónimo!')
             } else {
                 let id = + new Date
                 this.anonymous[id] = {
@@ -36,7 +36,7 @@ async function handler(m, { command }) {
                         return who === this.a ? this.b : who === this.b ? this.a : ''
                     },
                 }
-                m.reply('En espera de que un usuario use el comando para poder vincularlos.. esto puede demorar si desea salir use #leave')
+                m.reply('En espera de que un usuario use el comando #start para poder vincularlos.. esto puede demorar si desea salir con #leave')
             }
             break
         }

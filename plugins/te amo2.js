cee7a191daaf7af7e51e593c4aa2c82bd37d0f22
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 22:31:09 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/te amo2.js
+++ plugins/te amo2.js
@@ -8,7 +8,7 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /te amo|teamo/i
+handler.customPrefix = /te amo|teamo/i 
 handler.command = new RegExp
 
 handler.fail = null

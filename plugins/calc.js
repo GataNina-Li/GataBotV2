1a1e814d5b498fec1285df6bc9f1f45b6710db49
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 19:06:17 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/calc.js
+++ plugins/calc.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
   if (id in global.math) {
     clearTimeout(global.math[id][3])
     delete global.math[id]
-    m.reply('Hmmm...ngecheat?')
+    m.reply('Hmmm...trampa?')
   }
   let val = text
     .replace(/[^0-9\-\/+*×÷πEe()piPI/]/g, '')
@@ -26,13 +26,13 @@ let handler = async (m, { conn, text }) => {
     if (!result) throw result
     m.reply(`*${format}* = _${result}_`)
   } catch (e) {
-    if (e == undefined) throw 'Isinya?'
+    if (e == undefined) throw '*Y la expresión Matemática?*'
     throw 'Format salah, hanya 0-9 dan Simbol -, +, *, /, ×, ÷, π, e, (, ) yang disupport'
   }
 }
 handler.help = ['calc <expression>']
 handler.tags = ['tools']
-handler.command = /^(calc(ulat(e|or))?|kalk(ulator)?)$/i
+handler.command = /^calc|calculadora|calculador$/i
 handler.exp = 5
 
 module.exports = handler

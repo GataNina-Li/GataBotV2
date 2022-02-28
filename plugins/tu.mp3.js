15f33af255de5851961ba2df3298c2210363a638
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 22:44:28 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/tu.mp3.js
+++ plugins/tu.mp3.js
@@ -10,4 +10,4 @@ ptt: true
 }
 handler.customPrefix = /Quién es tu senpai botsito 7u7|Quien es tu senpai botsito 7u7|Quién es tu sempai botsito 7u7|Quien es tu sempai botsito 7u7|Quién es tu senpai botsito 7w7|Quien es tu senpai botsito 7w7|quién es tu senpai botsito 7u7|quien es tu senpai botsito 7u7|Quién es tu sempai botsito 7w7|Quien es tu sempai botsito 7w7/
 handler.command = new RegExp
-module.exports = handler
+module.exports = handler 

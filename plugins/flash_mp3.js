4329f44a61dc06d7c3521ba7eb523b550ab1d5ae
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 23:14:25 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/flash_mp3.js
+++ plugins/flash_mp3.js
@@ -8,6 +8,6 @@ type: 'audioMessage',
 ptt: true 
 })
 }
-handler.customPrefix = /Me voy|me voy|ME VOY|Me fui|me fui|ME FUI|Se fue|se fue|SE FUE|Adios|adios|ADIOS|Chao|chao|CHAO/i
+handler.customPrefix = /Me voy|me voy|ME VOY|Me fui|me fui|ME FUI|Se fue|se fue|SE FUE|Adios|adios|ADIOS|Chao|chao|CHAO/i 
 handler.command = new RegExp
 module.exports = handler

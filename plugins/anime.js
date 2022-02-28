93db0ef17f70bb9a38a6a5cb64cb53d80538e0ac
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 19:28:50 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/anime.js
+++ plugins/anime.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 
 let handler = async(m, { conn, args, usedPrefix }) => {
-    if (args.length == 0) return conn.reply(m.chat, `*Usted esta usando mal el comando ${usedPrefix}anime*\n\n*Usted debe usarlo de la siguiente manera: ${usedPrefix}anime (categoría)*\n\n*Ejemplo: ${usedPrefix}anime random*\n\n*categorías disponibles:*\n*random, waifu, husbu, neko*`, m)
+    if (args.length == 0) return conn.reply(m.chat, ` ❰ ❗️ ❱ *Usted esta usando mal el comando ${usedPrefix}anime*\n\n✅ *Usted debe usarlo de la siguiente manera: ${usedPrefix}anime (categoría)*\n\n✨ *Ejemplo: ${usedPrefix}anime random*\n\n😌 *categorías disponibles:*\n*random, waifu, husbu, neko*`, m)
     if (args[0] == 'random' || args[0] == 'waifu' || args[0] == 'husbu' || args[0] == 'neko') {
   await m.reply('*Agurde un momento...*')
 
@@ -16,7 +16,7 @@ let handler = async(m, { conn, args, usedPrefix }) => {
                 conn.reply(m.chat, 'Hubo un error, disculpe... ', m)
             })
     } else {
-        conn.reply(m.chat, `*⚠️ Uso erróneo*\n\n*👉🏻 Uso correcto: ${usedPrefix}anime (categoría)*\n\n*🔰 Escriba ${usedPrefix}anime para ver la lista de categorías disponibles*`, m)
+        conn.reply(m.chat, `*❰ ⚠️ ❱ Uso erróneo*\n\n*👉 Uso correcto: ${usedPrefix}anime (categoría)*\n\n*😺 Escriba ${usedPrefix}anime para ver la lista de categorías disponibles*`, m)
     }
 
 }

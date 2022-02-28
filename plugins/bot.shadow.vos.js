8c4cae68c5a319316218c463750ce199ed3b5a22
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 22:59:18 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/bot.shadow.vos.js
+++ plugins/bot.shadow.vos.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
 let vn = './media/bot.mp3'
 conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, {
 type: 'audioMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v 
 })
 }
 handler.customPrefix = /the shadow brokers - bot|The Shadow Brokers - Bot|tsbb|TSBB|Shadow Brokers - Bot|shadow brokers - bot|Shadow bot|shadow bot|theshadowbrokersbot|Shadow Bot|Hola Bot|hola bot|Hola bot|hola bot|ShadowBot|shadowbot/

31b3db6ce333a6241b54d9fa2c807f9e357ee9f2
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 23:18:04 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/pasen-contexto_mp3.js
+++ plugins/pasen-contexto_mp3.js
@@ -10,4 +10,4 @@ ptt: true
 }
 handler.customPrefix = /Contexto|CONTEXTO|contexto|Pasen contexto|PASEN CONTEXTO|pasen contexto|Y el contexto|Y EL CONTEXTO|y el contexto/i
 handler.command = new RegExp
-module.exports = handler
+module.exports = handler 

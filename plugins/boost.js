b2eaa9a0dea9839ccdd5a01399ccaeb2527834de
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-28 12:50:20 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/boost.js
+++ plugins/boost.js
@@ -2,7 +2,7 @@ let { performance } = require('perf_hooks')
 
 let handler = async (m, { conn }) => {
 
- let start = `Empezando aceleración... aguarde un momento`
+ let start = `🚀 ¡¡Empezando aceleración!! 🚀`
  let boost = `${pickRandom(['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'])}%`
  let boost2 = `${pickRandom(['21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40'])}%`
  let boost3 = `${pickRandom(['41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60'])}%`
@@ -18,13 +18,13 @@ let handler = async (m, { conn }) => {
    let old = performance.now()
    let neww = performance.now()
    let speed = `${neww - old}`
-   let finish = `*_✔️ Bot acelerado con éxito_*\n\n*_Acelerado: ${speed} segundos!_*`
+   let finish = `*_✅ GataBot acelerada con éxito_*\n\n*_Aceleración: ${speed} segundos!_*`
 
      conn.reply(m.chat, finish, m)
 }
 handler.help = ['boost', 'refresh']
 handler.tags = ['info']
-handler.command = /^boost|refresh/i
+handler.command = /^boost|refresh|acelerar/i
 handler.mods = false
 handler.premium = false
 handler.group = false

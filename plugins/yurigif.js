ac7e63770899e8aec59f3bf1e59a0946989789c2
GataNina-Li <96250885+GataNina-Li@users.noreply.github.com>
2022-02-27 20:46:57 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/yurigif.js
+++ plugins/yurigif.js
@@ -5,7 +5,7 @@ let les = await axios.get('https://meme-api.herokuapp.com/gimme/yurigif')
   }
 handler.help = ['yurigif']
 handler.tags = ['General']
-handler.command = /^(yurigif)$/i
+handler.command = /^(yurigif)$/i 
 handler.owner = false
 handler.mods = false
 handler.premium = false

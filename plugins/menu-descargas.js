fc352b58a3cd0a99762e52534d53ee863b0aa73a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-04 02:19:43 -0500

    Update menu-descargas.js

--- plugins/menu-descargas.js
+++ plugins/menu-descargas.js
@@ -51,6 +51,6 @@ let mentionedJid = [who]
 conn.send3ButtonImg(m.chat, pp, menu, '𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨', 'Hola 😸', `Hola`, 'Menú de Audios 🔊', `#menuaudios`, 'Menú simple ⚡️', `#menusimple`, m, false, { contextInfo: { mentionedJid }})   
 }
 
-handler.command = /^(m4)$/i
+handler.command = /^(menudescarga|menudescargas|Menudescargas|Menúdescargas|Menúdescarga|menúdescarga)$/i
 handler.fail = null
 module.exports = handler

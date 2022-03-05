4a4dfc11442b79643960f464ef13a5d1ece63a31
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-05 18:46:31 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/menu-juegos.js
+++ plugins/menu-juegos.js
@@ -1,18 +1,15 @@
-let fs = require('fs')
-let fetch = require('node-fetch')
-let moment = require('moment-timezone')
-let path = require('path')
-let util = require('util')
-let handler = async (m, { conn, usedPrefix }) => {
-let pp = './Menu2.jpg'
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let username = conn.getName(who)
 //let vn = './media/mariana.mp3'
 //NO MODIFIQUES EL NÚMERO DE LA CREADORA NI EL NOMBRE.. SOLO AGREGA LA INFORMACIÓN QUE TU QUIERAS O EDITALO A TU MANERA PERO DEJANDO LOS CREDITOS
 //PUEDES AGREGAR OTRA FILAS DE PAYPAL, GRUPOS, PERO DEJA ALGUNOS CREDITOS, YA QUE ES LA UNICA MANERA DE INGRESOS DEL BOT
 
 //SI VAS A MODIFICAR TODO Y HACER PASAR COMO SI FUERA TU BOT (CREADO POR TI) SOLO TE PIDO QUE SI ESTA EN TUS POSIBILIDADES DONES UN POCO
-let menu =`
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+let fetch = require('node-fetch')
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let mentionedJid = [who]
+let username = conn.getName(who)
+let pp = 'https://i.imgur.com/BfsbCOR.jpg'
+let menu =` 
 ╭━〘 🐈⚡️🐈⚡️🐈⚡️🐈⚡️🐈 〙━╮
  ‖ ֎┉┉┉⊰ 𝙂𝙖𝙩𝙖𝘽𝙤𝙩 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 ⊱┉┉┉֍
  ‖ 💖 *¡𝗛ola! ${username}* 💖
@@ -49,11 +46,10 @@ let menu =`
  ‖⇛ 🎳 _${usedPrefix}prostituto *@tag / nombre*_
  ‖ ➥ ⧼ *_MENÚ DE JUEGOS_* ⧽ 
 ╰━〘 🐈🌟🐈🌟🐈🌟🐈🌟🐈 〙━╯
-`.trim()
-let mentionedJid = [who]
-conn.send3ButtonImg(m.chat, pp, menu, '𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨', '🚀 MENÚ DE DESCARGAS 🚀', `#menudescarga`, '🔐 GESTIÓN DE GRUPO 🔐', `#menugrupo`, '🎨 MENÚ DE CREACIÓN 🎨', `#menucreador`, m, false, { contextInfo: { mentionedJid }})   
-}
+𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨`.trim()
+conn.send3ButtonLoc(m.chat, (await fetch(pp)).buffer(), `
+⁖ᯓ፨҈༺ 𝙂𝙖𝙩𝙖𝘽𝙤𝙩 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 ༻፨҈ᯓ⁖
+`.trim(), menu, '🚀 𝙈𝙀𝙉𝙐 𝘿𝙀 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙎 🚀', `#menudescarga`, '🔐 𝙂𝙀𝙎𝙏𝙄𝙊𝙉 𝘿𝙀 𝙂𝙍𝙐𝙋𝙊 🔐', `#menugrupo`, '🎨 𝙈𝙀𝙉𝙐 𝘿𝙀 𝘾𝙍𝙀𝘼𝘾𝙄𝙊𝙉 🎨', `#menucreador`, m, false, { contextInfo: { mentionedJid }})}
 
 handler.command = /^(menujuego|menujuegos|Menújuego|Menújuegos|menújuegos)$/i
-handler.fail = null
 module.exports = handler

dbcd0d8ce35901b54b133d846f1c7403f933e6f2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-05 16:29:17 -0500

    Update menu-modificado.js

--- plugins/menu-modificado.js
+++ plugins/menu-modificado.js
@@ -354,8 +354,7 @@ let menu =`
 ┗━━━━━━━━━━━━━━━━━━┛
 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨`.trim()
 conn.send3ButtonLoc(m.chat, (await fetch(pp)).buffer(), `
-𝙂𝙖𝙩𝙖𝘽𝙤𝙩 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨
-*☆彡:･;.*:･*。･゜.。☆彡*
+*☆彡:･;.*: 𝙂𝙖𝙩𝙖𝘽𝙤𝙩 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 ･*。･゜.。☆彡*
 `.trim(), menu, '¡HOLA! 😸', `Hola`, 'MENÚ DE AUDIOS 🔊', `#menuaudios`, 'MENÚ NUEVO ⚡️', `#menu`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^prueba?$/i
 module.exports = handler

9891ec91d771883853ef2c2872f5c3f65ef87fb1
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-05 22:29:27 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('GATABOT\nPOR\nGATA DIOS', {
+CFonts.say('GATABOT V2\nPOR\nGATA DIOS', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']

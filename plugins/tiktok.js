335588622935bf8e7c81be7894a492cdf6583b6e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-05 20:27:51 -0500

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,14 +1,14 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'Uhm... where\'s the url?'
+  if (!args[0]) throw 'Uhm...url nya mana?'
   let res = await fetch(global.API('xteam', '/dl/tiktok', {
     url: args[0]
   }, 'APIKEY'))
   if (res.status !== 200) throw await res.text()
   let json = await res.json()
   if (!json.status) throw json
-  let url = json.server_1 || json.info[0].videoUrl || ''
-  if (!url) throw 'Failed to fetch download url'
+  /*let url = json.server_1 || json.info[0].videoUrl || ''
+  if (!url) throw 'Gagal mengambil url download'
   let txt = json.info[0].text
   for (let hashtag of json.info[0].hashtags) txt = txt.replace(hashtag, '*$&*')
   await conn.sendFile(m.chat, url, 'tiktok.mp4', `
@@ -19,7 +19,16 @@ let handler = async (m, { conn, args }) => {
 🎵 ${json.info[0].musicMeta.musicName} by ${json.info[0].musicMeta.musicAuthor}
 - *By:* ${json.info[0].authorMeta.nickName} (${json.info[0].authorMeta.name})
 - *Desc:*
-  `.trim(), m)
+${txt}
+  `.trim(), m)*/
+  let url = json.result.link_dl1 || json.result.link_dl2 || ''
+  if (!url) throw 'Gagal mengambil url download'
+  let txt = `
+  - *By:* ${json.result.name}
+  - *Caption:*
+  ${json.result.caption}
+    `
+    await conn.sendFile(m.chat, url, 'tiktok.mp4', txt.trim(), m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']

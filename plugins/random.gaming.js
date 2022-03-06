5bed8287003b4de48abfa12e29e3e9c061d00590
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-06 17:43:19 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/random.gaming.js
@@ -0,0 +1,8 @@
+let fetch = require('node-fetch')
+     let handler  = async (m, { conn, usedPrefix, command }) => {
+    heum = await fetch(`https://api-reysekha.herokuapp.com/api/wallpaper/gaming?apikey=APIKEY`)
+    json = await heum.buffer()
+   conn.sendButtonImg(m.chat, json, '🎮🎮🎮', 'Gata Dios', '🔄 SIGUIENTE', `${usedPrefix + command}`, m, false)
+}
+handler.command = /^(wpgaming|fondogaming)$/i
+module.exports = handler

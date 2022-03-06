1324fcb6728072a7f0493c4e76c1f970a84969bf
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-06 17:30:10 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/random-jeni.js
+++ plugins/random-jeni.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
     heum = await fetch(`https://api-reysekha.herokuapp.com/api/wallpaper/jeni?apikey=APIKEY`)
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '💫 *Como las estrellas sobre las nubes* 💫', 'Gata Dios', '🔄 SIGUIENTE', `${usedPrefix + command}`, m, false)
+   conn.sendButtonImg(m.chat, json, '💫 *Como las estrellas sobre las nubes*', 'Gata Dios', '🔄 SIGUIENTE', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(jeni|jennie|Jeni|Jennie)$/i
 module.exports = handler

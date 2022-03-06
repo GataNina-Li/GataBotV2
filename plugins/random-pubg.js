73416d5416d86bfc89cfed6bf24712786dc7f63d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-06 16:33:54 -0500

    Update and rename todoroki.js to random-pubg.js

--- plugins/random-pubg.js
+++ plugins/random-pubg.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
     heum = await fetch(`https://api-reysekha.herokuapp.com/api/wallpaper/pubg?apikey=APIKEY`)
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '*Incluso el más fuerte de los oponentes tiene siempre una debilidad*', 'Gata Dios', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+   conn.sendButtonImg(m.chat, json, 'Gata Dios', '🔄 SIGUIENTE', `${usedPrefix + command}`, m, false)
 }
-handler.command = /^(todoroki)$/i
+handler.command = /^(pubg|Pubg|pugb)$/i
 module.exports = handler

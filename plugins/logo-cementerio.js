d92a969bf579919a80b19a3cbcc6bf90efb17dc4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-06 00:01:31 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/logo-cementerio.js
@@ -0,0 +1,9 @@
+//CREDITOS: https://github.com/BrunoSobrino
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+response = args.join(' ').split('|')
+if (!args[0]) throw '*❰ ❗️ ❱ Ingrese un texto*\n*Ejemplo:*\n*#cementerio Gata*'        
+let res = `https://api-alc.herokuapp.com/api/photooxy/text-on-scary?texto=${response[0]}&apikey=ConfuMods`
+conn.sendFile(m.chat, res, 'error.jpg', `*Logo terminado*`, m, false)}
+handler.command = /^(cementerio|logocementerio)$/i
+module.exports = handler

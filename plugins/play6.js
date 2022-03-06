eb003c2c194d8d7e8c1a43093376091b73041c05
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-06 13:01:46 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/play6.js
+++ plugins/play6.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command, text }) => {
-if (!text) throw `*Formato de uso: ${usedPrefix + command} Nombre de la canción o video*\n*Ejemplo:*\n*${usedPrefix + command} Beret ojala*`
+if (!text) throw `*Formato de uso: ${usedPrefix + command} Nombre de la canción o video*\n*Ejemplo:*\n*${usedPrefix + command} Billie Eilish bored*`
 let res = await fetch("https://api-alc.herokuapp.com/api/download/play-mp4?query="+text+"&apikey=ConfuMods")
 let json = await res.json()
-conn.sendFile(m.chat, json.link, 'error.mp4', `   *Aqui tienes el video :D*\nGata Dios`, m)}
+conn.sendFile(m.chat, json.link, 'error.mp4', `*¡Aqui tienes el video!*\n🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈`, m)}
 handler.command = /^(play6)$/i
 module.exports = handler

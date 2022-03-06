9f2c6d3c975e2729e2ad929e23f7ab0257a7a2ef
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-06 01:14:07 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/logo-taza.js
@@ -0,0 +1,9 @@
+//CREDITOS: https://github.com/BrunoSobrino
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+response = args.join(' ').split('|')
+if (!args[0]) throw '*❰ ❗️ ❱ Ingrese un texto*\n*Ejemplo:*\n*#logotaza Gata*'        
+let res = `https://api-alc.herokuapp.com/api/photooxy/teks-cup?texto=${response[0]}&apikey=ConfuMods`
+conn.sendFile(m.chat, res, 'error.jpg', `✅ *¡Logo terminado!*`, m, false)}
+handler.command = /^(taza|logotaza)$/i
+module.exports = handler

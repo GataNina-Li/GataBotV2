48875304a4653ed9ffb64d859de92abd8e767311
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-06 16:23:09 -0500

    Update todoroki.js

--- plugins/todoroki.js
+++ plugins/todoroki.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
-    heum = await fetch(`https://api-alc.herokuapp.com/api/chicas/brasil?&apikey=ConfuMods`)
+    heum = await fetch(`https://api-reysekha.herokuapp.com/api/wallpaper/pubg?apikey=APIKEY`)
     json = await heum.buffer()
    conn.sendButtonImg(m.chat, json, '*Incluso el más fuerte de los oponentes tiene siempre una debilidad*', 'Gata Dios', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
 }

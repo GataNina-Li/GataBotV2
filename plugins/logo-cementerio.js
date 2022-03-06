d26e5d752415f4702419be2d3b7b6aec57c4579e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-06 00:08:04 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/logo-cementerio.js
+++ plugins/logo-cementerio.js
@@ -4,6 +4,6 @@ let handler = async (m, { conn, args }) => {
 response = args.join(' ').split('|')
 if (!args[0]) throw '*❰ ❗️ ❱ Ingrese un texto*\n*Ejemplo:*\n*#cementerio Gata*'        
 let res = `https://api-alc.herokuapp.com/api/photooxy/text-on-scary?texto=${response[0]}&apikey=ConfuMods`
-conn.sendFile(m.chat, res, 'error.jpg', `*Logo terminado*`, m, false)}
-handler.command = /^(cementerio|logocementerio)$/i
+conn.sendFile(m.chat, res, 'error.jpg', `✅ *¡Logo terminado!*`, m, false)}
+handler.command = /^(cementerio|logocementerio|Cementerio|Logocementerio)$/i
 module.exports = handler

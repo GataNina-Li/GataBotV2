b4e39fbb16b9d3b92171a218c0f6b0a3282e25ae
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-07 06:31:26 -0500

    Create videos4.js

---
+++ plugins/videos4.js
@@ -0,0 +1,23 @@
+let handler = async(m, { conn, usedPrefix, command }) => {
+await conn.sendButtonVid(m.chat, pickRandom(asupan), '🔥🔥🔥🔥', 'Gata Dios', 'SIGUIENTE 🔄🥵', `${usedPrefix + command}`, m, false)
+}
+handler.command = /^(pornovid|pornovideo|Pornovid|Pornovideo|Pornvid|Ponrvid|pornvid|ponrvid|pornov)$/i
+module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+const asupan = [
+https://l.top4top.io/m_2257y4pyl0.mp4
+  https://k.top4top.io/m_2257pdwjy0.mp4
+  https://a.top4top.io/m_2257qulmx0.mp4
+  https://a.top4top.io/m_2257vxzr62.mp4
+  https://b.top4top.io/m_2257wjmbh3.mp4
+  https://b.top4top.io/m_2257sen2a1.mp4
+  https://c.top4top.io/m_2257hpo9v3.mp4
+  https://e.top4top.io/m_2257pye7u1.mp4
+  https://c.top4top.io/m_2257p7xg14.mp4
+"https://b.top4top.io/m_22571xiss0.mp4",
+"https://a.top4top.io/m_2257tgfkz0.mp4",
+]

83772cb10d02ea2bbdd32ae9e0163e05779bbc00
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-07 06:46:34 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/videos4.js
+++ plugins/videos4.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn, usedPrefix, command }) => {
 await conn.sendButtonVid(m.chat, pickRandom(asupan), '🔥🔥🔥🔥', 'Gata Dios', 'SIGUIENTE 🔄🥵', `${usedPrefix + command}`, m, false)
 }
-handler.command = /^(pornovid|pornovideo|Pornovid|Pornovideo|Pornvid|Ponrvid|pornvid|ponrvid|pornov)$/i
+handler.command = /^(pornolesbivid|pornolesbianavid|pornolesbiv|pornolesbianav|Pornolesbivid|Pornolesbianavid|pornolesv|Pornolesv)$/i
 module.exports = handler
 
 function pickRandom(list) {
@@ -9,15 +9,21 @@ function pickRandom(list) {
 }
 
 const asupan = [
-https://l.top4top.io/m_2257y4pyl0.mp4
-  https://k.top4top.io/m_2257pdwjy0.mp4
-  https://a.top4top.io/m_2257qulmx0.mp4
-  https://a.top4top.io/m_2257vxzr62.mp4
-  https://b.top4top.io/m_2257wjmbh3.mp4
-  https://b.top4top.io/m_2257sen2a1.mp4
-  https://c.top4top.io/m_2257hpo9v3.mp4
-  https://e.top4top.io/m_2257pye7u1.mp4
-  https://c.top4top.io/m_2257p7xg14.mp4
-"https://b.top4top.io/m_22571xiss0.mp4",
-"https://a.top4top.io/m_2257tgfkz0.mp4",
+"https://l.top4top.io/m_2257y4pyl0.mp4",
+"https://k.top4top.io/m_2257pdwjy0.mp4",
+"https://a.top4top.io/m_2257qulmx0.mp4",
+"https://a.top4top.io/m_2257vxzr62.mp4",
+"https://b.top4top.io/m_2257wjmbh3.mp4",
+"https://b.top4top.io/m_2257sen2a1.mp4",
+"https://c.top4top.io/m_2257hpo9v3.mp4",
+"https://e.top4top.io/m_2257pye7u1.mp4",
+"https://c.top4top.io/m_2257p7xg14.mp4",
+"https://c.top4top.io/m_2257p4v9i3.mp4",
+"https://l.top4top.io/m_2257jvkrv3.mp4",
+"https://b.top4top.io/m_2257pl7wh1.mp4",
+"https://e.top4top.io/m_2257fiwnp2.mp4",
+"https://b.top4top.io/m_22578b1nk1.mp4",
+"https://k.top4top.io/m_22572gv7q1.mp4",
+"https://i.top4top.io/m_2257pu90l2.mp4",
+"https://d.top4top.io/m_2257vcwiw3.mp4",
 ]

aca509eb55728208deec11a95df33edfcaaf9a38
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-07 05:01:05 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/videos3.js
@@ -0,0 +1,27 @@
+let handler = async(m, { conn, usedPrefix, command }) => {
+await conn.sendButtonVid(m.chat, pickRandom(asupan), '🔥🔥🔥🔥', 'Gata Dios', 'SIGUIENTE 🔄🥵', `${usedPrefix + command}`, m, false)
+}
+handler.command = /^(pornogayvid|pornogayv|porngay|Pornogayvid|pornovidg|Pornogayv|porngayvid)$/i
+module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+const asupan = [
+"https://g.top4top.io/m_2257kulna0.mp4",
+"https://b.top4top.io/m_2257l6p450.mp4",
+"https://h.top4top.io/m_2257pvfm60.mp4",
+"https://l.top4top.io/m_2257dwqiz4.mp4",
+"https://l.top4top.io/m_22570gq4r0.mp4",
+"https://e.top4top.io/m_22575tzit0.mp4",
+"https://j.top4top.io/m_2257pf8mh0.mp4",
+"https://f.top4top.io/m_2257qiec20.mp4",
+"https://b.top4top.io/m_22573zdwj3.mp4",
+"https://h.top4top.io/m_22572j8n84.mp4",
+"https://f.top4top.io/m_2257hturm2.mp4",
+"https://c.top4top.io/m_2257ler770.mp4",
+"https://l.top4top.io/m_2257p6e4y3.mp4",
+"https://d.top4top.io/m_2257j8tk80.mp4",
+"https://k.top4top.io/m_2257sam501.mp4",
+]

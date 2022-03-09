1764124370723f33498c0b4ea59def7b1a0e1625
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-09 16:08:33 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,7 +2,7 @@ function handler(m) {
   this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
   this.sendContact(m.chat, '14509777159', 'Gata Dios', m)
   this.sendContact(m.chat, '593993684821', 'OFICIAL - GataBot', m)
-  this.sendContact(m.chat, '994407312387', 'OFICIAL - GataBot', m)
+  this.sendContact(m.chat, '14504003616', 'OFICIAL - GataBot', m)
   }
 handler.help = ['contacto']
 handler.tags = ['info']

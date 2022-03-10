68c09d8cd168986a921972768f96a0f9c2bfe9c8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-10 11:55:19 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/la-voz-de-hombre_mp3.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/la-voz-de-hombre.mp3.mp3'
+conn.sendFile(m.chat, vn, 'la-voz-de-hombre.mp3.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /La voz de hombre|la voz de hombre|La voz del hombre|la voz del hombre|La voz|la voz/ 
+handler.command = new RegExp
+module.exports = handler

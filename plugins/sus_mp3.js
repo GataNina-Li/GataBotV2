2384c44e8796cbcd6750b3c81c1744cf7bab2732
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-10 11:46:50 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/sus_mp3.js
@@ -0,0 +1,16 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/sus.mp3'
+conn.sendFile(m.chat, vn, 'sus.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /Sus|sus|Amongos|among us|Among us|Among Us|Us/i 
+handler.command = new RegExp 
+
+handler.fail = null
+handler.exp = 100
+module.exports = handler

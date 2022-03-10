b4cd2898122a7558339c5ba4c6d4b329e0da838b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-10 11:07:17 -0500

    Create fino-senores_mp3.js

---
+++ plugins/fino-senores_mp3.js
@@ -0,0 +1,16 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/fino-senores.mp3'
+conn.sendFile(m.chat, vn, 'fino-senores.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /Fino señores|fino señores|Fino senores|fino senores/i 
+handler.command = new RegExp
+
+handler.fail = null
+handler.exp = 100
+module.exports = handler

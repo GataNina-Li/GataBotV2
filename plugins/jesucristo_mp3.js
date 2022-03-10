545f7b3ffbbc7be6f4e8ae23c98c65e78ca6b238
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-10 12:31:12 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/jesucristo_mp3.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/jesucristo.mp3'
+conn.sendFile(m.chat, vn, 'jesucristo.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /Jesucristo|jesucristo|Jesús|jesús|Auronplay|Auron|Dios|dios/ 
+handler.command = new RegExp
+module.exports = handler

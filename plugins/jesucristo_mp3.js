380ca609d3de3ebc5fe9886ff0bc40b38090c61b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-10 14:39:10 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/jesucristo_mp3.js
+++ plugins/jesucristo_mp3.js
@@ -8,6 +8,6 @@ type: 'audioMessage',
 ptt: true 
 })
 }
-handler.customPrefix = /Jesucristo|jesucristo|Jesús|jesús|Auronplay|Auron|Dios|dios/ 
+handler.customPrefix = /Jesucristo|jesucristo|Jesús|jesús|Auronplay|Auron|Dios/ 
 handler.command = new RegExp
 module.exports = handler

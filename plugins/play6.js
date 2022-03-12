74986a4f64778fcaca58dd1c2320e6e765a336aa
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-11 21:48:26 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/play6.js
+++ plugins/play6.js
@@ -1,10 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 if (!text) throw `*Formato de uso: ${usedPrefix + command} Nombre de la canción o video*\n*Ejemplo:*\n*${usedPrefix + command} Billie Eilish bored*`
-let res = await fetch("https://api-reysekha.herokuapp.com/api/download/ytmp4?url="+text+"&apikey=APIKEY")
+let res = await fetch("https://api-alc.herokuapp.com/api/download/play-mp4?query="+text+"&apikey=ConfuMods")
 let json = await res.json()
 conn.sendFile(m.chat, json.link, 'error.mp4', `*¡Aqui tienes el video!*\n🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈`, m)}
 handler.command = /^(play6)$/i
 module.exports = handler
-//https://api-reysekha.herokuapp.com/api/download/ytmp4?url=https://youtu.be/6l5V3BWDcMw&apikey=APIKEY
-//https://api-alc.herokuapp.com/api/download/play-mp4?query="+text+"&apikey=ConfuMods

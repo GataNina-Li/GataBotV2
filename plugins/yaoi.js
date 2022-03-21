0d7ea776993c60806d571dad73592953fa05138c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-21 18:25:41 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/yaoi.js
+++ plugins/yaoi.js
@@ -1,10 +1,17 @@
-const axios = require('axios')
- let handler = async(m, { conn }) => {
-let les = await axios.get('https://meme-api.herokuapp.com/gimme/yaoigif')
-            conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
-  }
-handler.help = ['yaoigif']
-handler.tags = ['images']
+let fetch = require("node-fetch")
+const { sticker } = require('../lib/sticker')
+const { MessageType } = require('@adiwajshing/baileys')
+let handler = async (m, { conn}) => {
+try {
+let res = await fetch('https://meme-api.herokuapp.com/gimme/yaoigif')
+let json = await res.json()
+let { url } = json
+let stiker = await sticker(null, url, 'Yaoigif', 'GataBot 🐈 - Gata Dios')
+conn.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m })
+} catch (e) { }}
+
+handler.help = ['porno2']
+handler.tags = ['General']
 handler.command = /^(yaoigif)$/i 
 handler.owner = false
 handler.mods = false

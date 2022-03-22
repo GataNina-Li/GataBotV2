1e07e39948c7512e5ab47d2e6a07e0c830b5ecf5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-22 08:32:15 -0500

    Create gatabot.js

---
+++ plugins/gatabot.js
@@ -0,0 +1,16 @@
+let fs = require("fs")
+let handler = async(m, { conn, args, text, usedPrefix: _p, usedPrefix, command }) => {
+let vn = './media/pikachu.mp3'
+const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) },
+message: { 
+orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'WhatsApp Bot Oficial', orderTitle: 'Bang', thumbnail: fs.readFileSync('./Menu2.jpg'), sellerJid: '0@s.whatsapp.net'    
+}}}
+const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) },
+message: { 
+"audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"   
+}}}  
+conn.sendButton(m.chat, `*Hola, como te puedo ayudar?*`, 'Gata Bot', '𝙼𝙴𝙽𝚄 𝙳𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂', `${usedPrefix}menu`, 'conversation', { sendEphemeral: true, quoted: estilo })
+await conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true, quoted: estiloaudio })
+}
+handler.command = /^(ot)$/i
+module.exports = handler

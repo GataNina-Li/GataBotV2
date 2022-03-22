8579943ebdc668f5bb5ca7c175386bd102ebf030
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-22 14:57:29 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/instalarbot.js
+++ plugins/instalarbot.js
@@ -3,6 +3,9 @@ let handler = async m => m.reply(`
 
 💥 𝙔𝘼 𝙋𝙐𝙀𝘿𝙀 𝙄𝙉𝙎𝙏𝘼𝙇𝘼𝙍 𝘼 𝙂𝘼𝙏𝘼𝘽𝙊𝙏 ❕
 
+*VÍDEO DE INSTALACIÓN*
+https://youtu.be/85xI8WFMIUY
+
 ✨ 𝙍𝙀𝙌𝙐𝙄𝙎𝙄𝙏𝙊𝙎 𝙋𝘼𝙍𝘼 𝙄𝙉𝙎𝙏𝘼𝙇𝘼𝙍 𝙂𝘼𝙏𝘼𝘽𝙊𝙏
 
 ✅ _1 GB de almacenamiento_

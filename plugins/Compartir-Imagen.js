500432a7c94f01da5ce121021c9acc79fd8f8909
Verseker45 <102321408+Verseker45@users.noreply.github.com>
2022-03-25 00:06:17 -0500

    Update Compartir-Imagen.js

--- plugins/Compartir-Imagen.js
+++ plugins/Compartir-Imagen.js
@@ -5,6 +5,6 @@ if (res.status != 200) throw await res.text()
 let json = await res.json()
 if (!json.status) throw json
 conn.sendButtonImg(m.chat, json.result.female,  'Chica 🙆‍♀️', '🎇 𝗖𝗮𝗺𝗶𝗹𝗼 𝗕𝗼𝘁 🎇', 'Siguiente 🔄', '.compartirfoto', m)
-conn.sendButtonImg(m.chat, json.result.male, 'Chico 🙆‍♂️', '🐈 𝙂𝙖𝙩𝙖 𝗕𝗼𝘁 🐈', 'Siguiente 🔄', '.compartirfoto', m)}
+conn.sendButtonImg(m.chat, json.result.male, 'Chico 🙆‍♂️', '🎇 𝗖𝗮𝗺𝗶𝗹𝗼 𝗕𝗼𝘁 🎇', 'Siguiente 🔄', '.compartirfoto', m)}
 handler.command = /^compartirfoto$/i
 module.exports = handler

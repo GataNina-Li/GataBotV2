4d179d78f79bd8c1aeeeba3584704fa540abadc1
Verseker45 <102321408+Verseker45@users.noreply.github.com>
2022-03-25 00:05:19 -0500

    Update Compartir-Imagen.js

--- plugins/Compartir-Imagen.js
+++ plugins/Compartir-Imagen.js
@@ -4,7 +4,7 @@ let res = await fetch('https://caliphapi.com/api/ppcouple?apikey=BNeGhnOu')
 if (res.status != 200) throw await res.text()
 let json = await res.json()
 if (!json.status) throw json
-conn.sendButtonImg(m.chat, json.result.female,  'Chica 🙆‍♀️', '🐈 𝙂𝙖𝙩𝙖 𝗕𝗼𝘁 🐈', 'Siguiente 🔄', '.compartirfoto', m)
+conn.sendButtonImg(m.chat, json.result.female,  'Chica 🙆‍♀️', '🎇 𝗖𝗮𝗺𝗶𝗹𝗼 𝗕𝗼𝘁 🎇', 'Siguiente 🔄', '.compartirfoto', m)
 conn.sendButtonImg(m.chat, json.result.male, 'Chico 🙆‍♂️', '🐈 𝙂𝙖𝙩𝙖 𝗕𝗼𝘁 🐈', 'Siguiente 🔄', '.compartirfoto', m)}
 handler.command = /^compartirfoto$/i
 module.exports = handler

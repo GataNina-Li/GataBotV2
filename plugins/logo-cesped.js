60e6937a54b2e2c14f04b3298ad8a47279230f40
Verseker45 <102321408+Verseker45@users.noreply.github.com>
2022-03-26 13:19:20 -0500

    Update logo-cesped.js

--- plugins/logo-cesped.js
+++ plugins/logo-cesped.js
@@ -2,7 +2,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 response = args.join(' ').split('|')
-if (!args[0]) throw '*❰ ❗️ ❱ Ingrese un texto*\n*Ejemplo:*\n*#logocesped Gata*'        
+if (!args[0]) throw '*❰ ❗️ ❱ Ingrese un texto*\n*Ejemplo:*\n*#logocesped Sasuke*'        
 let res = `https://api-alc.herokuapp.com/api/photooxy/under-grass?texto=${response[0]}&apikey=ConfuMods`
 conn.sendFile(m.chat, res, 'error.jpg', `✅ *¡Logo terminado!*`, m, false)}
 handler.command = /^(undergrass|logocesped|cesped)$/i

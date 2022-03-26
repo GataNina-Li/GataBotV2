c64a8cec7e13203d41058b59bc4c4c57871afc53
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-26 16:00:13 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/play.js
+++ plugins/play.js
@@ -3,7 +3,7 @@ const { servers, yta, ytv } = require('../lib/y2mate')
 let yts = require('yt-search')
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `*✳️ Uhm.. que estas buscando?*\n*👉🏻 Ingrese un texto o enlace de YT*\n\n*✅ Ejemplo:*\n*${usedPrefix + command} Begin you*`
+  if (!text) throw `🎁 *Ingrese un texto o enlace de YT*\n\n*✅ Ejemplo:*\n*${usedPrefix + command} Bellyache*`
 //  let chat = global.db.data.chats[m.chat]
   let results = await yts(text)
   let vid = results.all.find(video => video.seconds < 3600)
@@ -12,7 +12,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
   let yt = false
   let yt2 = false
   let usedServer = servers[0]
-  m.reply('*⏳Procesando⏳*\n\n*[❗] Si no obtiene ningun resultado o le sale algun error intente con otro nombre*')
+  m.reply('🔁 *Descargando...*\n\n*❰ ❗ ❱ Si no obtiene ningun resultado o le sale algun error intente con otro nombre*')
   for (let i in servers) {
     let server = servers[i]
     try {
@@ -28,10 +28,10 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
   if (yt2 === false) throw '*Todos los servidores fallaron*'
   let { dl_link, thumb, title, filesize, filesizeF } = yt
   await conn.send2ButtonLoc(m.chat, await (await fetch(thumb)).buffer(), `
-*🔥 Titulo:* _${title}_
-*📂 Peso del audio:* _${filesizeF}_
-*📂 Peso del video:* _${yt2.filesizeF}_
-`.trim(), 'Gata Dios', '🎵 AUDIO 💽 ', `.yta ${vid.url}`, '🎥 VIDEO 🎞️', `.yt ${vid.url}`)
+*💗 Titulo:* _${title}_
+*🎈 Peso del audio:* _${filesizeF}_
+*🎈 Peso del video:* _${yt2.filesizeF}_
+`.trim(), 'Gata Dios', '🎵 AUDIO', `.yta ${vid.url}`, '🎥 VIDEO', `.yt ${vid.url}`)
 }
 handler.help = ['play'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']

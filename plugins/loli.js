1c429c96373eeef5e9cdb960cf325bb8e93a74e1
Verseker45 <102321408+Verseker45@users.noreply.github.com>
2022-03-26 13:28:03 -0500

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -5,7 +5,7 @@ let res = await fetch('https://raw.githubusercontent.com/Xmell91/loli/master/lol
 if (!res.ok) throw await `${res.status} ${res.statusText}`;
 let json = await res.json();
 let url = json[Math.floor(Math.random() * json.length)]
-await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), '*¡¡LLAMEN A LA POLICÍA!! 🚨️*', 'Gata Bot', 'SIGUIENTE 🔄', '/loli', m)
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), '*¡¡LLAMEN A LA POLICÍA!! 🚨️*', 'Camilo Bot', 'SIGUIENTE 🔄', '/loli', m)
 }
 handler.command = /^(loli)$/i 
 handler.tags = ['fun']

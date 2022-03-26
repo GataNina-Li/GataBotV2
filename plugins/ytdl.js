6b0b24a5258d0e99c20e36fb746b6dc91dac18f8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-26 14:47:57 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/ytdl.js
+++ plugins/ytdl.js
@@ -2,7 +2,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const { servers, yta } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
 let chat = global.DATABASE.data.chats[m.chat]
-  if (!args || !args[0]) throw '*[❗] Inserte un enlace de YouTube*\n\n*Ejemplo:*\n*#ytmp3 https://www.youtube.com/watch?v=8jvDzEIVpjg*'
+  if (!args || !args[0]) throw '*❰ ❗ ❱ Inserte un enlace de YouTube*\n\n*Ejemplo:*\n*#dlaudio https://youtu.be/gBRi6aZJGj4*'
   let server = (args[1] || 'id4').toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : 'id4')
   //let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
@@ -10,7 +10,7 @@ let chat = global.DATABASE.data.chats[m.chat]
  let y = fs.readFileSync('./Menu2.jpg')
 
 
-conn.sendMessage(m.chat, `*⏯ ️Descargador By Gata Dios ⏯️*\n\n*🔥Titulo:* ${title}\n*📂Tamaño del archivo:* ${filesizeF}` , 'conversation', {quoted: m, thumbnail: global.thumb, contextInfo:{externalAdReply: {title: 'Simple WhatsAppp Bot', body: `© ${conn.user.name}`, sourceUrl: 'enviando...', thumbnail: y}}})
+conn.sendMessage(m.chat, `*🪄 ️Descargando por Gata Dios 🪄*\n\n*🔮 Titulo:* ${title}\n*🗄 Tamaño del archivo:* ${filesizeF}` , 'conversation', {quoted: m, thumbnail: global.thumb, contextInfo:{externalAdReply: {title: 'Simple WhatsAppp Bot', body: `${conn.user.name}`, sourceUrl: 'enviando...', thumbnail: y}}})
 conn.sendFile(m.chat, dl_link , `By ${conn.user.name}.mp3`, m, false, {ptt: true, duration: 999999999999, asDocument: chat.useDocument})
 conn.sendFile(m.chat, dl_link , `By ${conn.user.name}.mp3`, m)
 }

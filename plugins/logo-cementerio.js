5edec153bd341cdd6647c5c51b8cb3ec831df592
Verseker45 <102321408+Verseker45@users.noreply.github.com>
2022-03-26 13:20:50 -0500

    Update logo-cementerio.js

--- plugins/logo-cementerio.js
+++ plugins/logo-cementerio.js
@@ -2,7 +2,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 response = args.join(' ').split('|')
-if (!args[0]) throw '*❰ ❗️ ❱ Ingrese un texto*\n*Ejemplo:*\n*#cementerio Gata*'        
+if (!args[0]) throw '*❰ ❗️ ❱ Ingrese un texto*\n*Ejemplo:*\n*#cementerio Sasuke*'        
 let res = `https://api-alc.herokuapp.com/api/photooxy/text-on-scary?texto=${response[0]}&apikey=ConfuMods`
 conn.sendFile(m.chat, res, 'error.jpg', `✅ *¡Logo terminado!*`, m, false)}
 handler.command = /^(cementerio|logocementerio|Cementerio|Logocementerio)$/i

b9c28bfbf89dfd80c89d1bab05a7db80cd48c6d1
Verseker45 <102321408+Verseker45@users.noreply.github.com>
2022-03-26 13:58:53 -0500

    Update msos.js

--- plugins/msos.js
+++ plugins/msos.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
     heum = await fetch(`https://api-alc.herokuapp.com/api/chicas/muslos?&apikey=ConfuMods`)
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '*By ShadowBot and ConfuMods*\n*https://api-alc.herokuapp.com/api/chicas/muslos?&apikey=ConfuMods*', 'Gata Dios', 'SIGUIENTE 🔄', `${usedPrefix + command}`, m, false)
+   conn.sendButtonImg(m.chat, json, '*By ShadowBot and ConfuMods*\n*https://api-alc.herokuapp.com/api/chicas/muslos?&apikey=ConfuMods*', 'Camilo Bot', 'SIGUIENTE 🔄', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(muslitos)$/i
 

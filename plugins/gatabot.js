8e3bbd62026c8069763714537e3294f7da645ceb
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-25 20:17:10 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/gatabot.js
+++ plugins/gatabot.js
@@ -1,11 +1,11 @@
 let fs = require("fs")
 let handler = async(m, { conn, args, text, usedPrefix: _p, usedPrefix, command }) => {
 let vn = './media/pikachu.mp3'
-const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) },
+const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "593993684821-1625305606@g.us" } : {}) },
 message: { 
 orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'WhatsApp Bot Oficial', orderTitle: 'Bang', thumbnail: fs.readFileSync('./Menu2.jpg'), sellerJid: '0@s.whatsapp.net'    
 }}}
-const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) },
+const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "593993684821-1625305606@g.us" } : {}) },
 message: { 
 "audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"   
 }}}  

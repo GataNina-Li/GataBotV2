b487f551a2eb53e95307bfaaa974d8f313a691db
Verseker45 <102321408+Verseker45@users.noreply.github.com>
2022-03-26 14:43:32 -0500

    Update nsfwbdsm.js

--- plugins/nsfwbdsm.js
+++ plugins/nsfwbdsm.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
     heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/bdsm?apikey=apirey`)
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '*¡Disfrutalo!*', 'Gata Dios', 'SIGUIENTE 🔄', `${usedPrefix + command}`, m, false)
+   conn.sendButtonImg(m.chat, json, '*¡Disfrutalo!*', 'Camilo Bot', 'SIGUIENTE 🔄', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(bdsm|nsfwbdsm)$/i 
 

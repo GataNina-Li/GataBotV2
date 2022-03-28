ec335fdeee9a3c41c9874e75d29d87f36a773895
Verseker45 <102321408+Verseker45@users.noreply.github.com>
2022-03-27 21:50:58 -0500

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -1,17 +1,13 @@
 let handler = async (m, { conn, participants }) => {
-  // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
-    if (!(m.chat in global.DATABASE._data.chats)) return m.reply('*Este chat no está registrado en la base de datos!*')
-    let chat = global.DATABASE._data.chats[m.chat]
-    if (chat.isBanned) return m.reply('*Este chat ya estaba baneado previamente*\n*Si desea desbanearlo use /unbanchat*')
-    chat.isBanned = true
-    m.reply('✅ *Este chat ha sido baneado a desicion de un administrador del grupo*')
-  // } else m.reply('Aquí hay un número de un host...')
+// if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
+if (!(m.chat in global.DATABASE._data.chats)) return m.reply('*Este chat no está registrado en la base de datos!*')
+let chat = global.DATABASE._data.chats[m.chat]
+if (chat.isBanned) return m.reply('*Este chat ya estaba baneado previamente*\n*Si desea desbanearlo use /unbanchat*')
+chat.isBanned = true
+m.reply('*Este chat ha sido baneado a desicion de un administrador del grupo*')
+// } else m.reply('Aquí hay un número de un host...')
 }
-handler.help = ['banchat']
-handler.tags = ['owner']
 handler.command = /^banchat$/i
-handler.rowner = false
-
+handler.group = true
 handler.admin = true
-
 module.exports = handler

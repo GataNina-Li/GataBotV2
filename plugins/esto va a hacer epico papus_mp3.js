a8e0c506034d8f08ab6e938c078b4b7916cfd207
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-28 20:27:54 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/esto va a hacer epico papus_mp3.js
@@ -0,0 +1,16 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/esto va a hacer epico papus.mp3'
+conn.sendFile(m.chat, vn, 'esto va a hacer epico papus.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /Esto va ser épico papus|esto va ser épico papus|Esto va ser|Esto va a hacer|esto va ser|esto va a hacer/i 
+handler.command = new RegExp
+
+handler.fail = null
+handler.exp = 100
+module.exports = handler

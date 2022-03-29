3b4e1ea783f5c384e7e4073e713ca13f6bc57955
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-28 21:41:03 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/estado.js
+++ plugins/estado.js
@@ -2,11 +2,12 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, command, text, usedPrefix }) => {
   await conn.send3Button(m.chat, `
 ╭══✨• ೋ•✨๑๑💗๑๑✨•ೋ •✨══╮
+
 *ミ💖 ¡Hola! estimado/a usuario/a 💖彡*
 
-*ミ🤖 Estado del Bot 🤖彡*
-*=> ✅ Bot activo*
-*=> ✅ Bot uso público*
+*ミ🤖 Estado de GataBot 🤖彡*
+*=> ✅ Bot activo y de uso público*
+
 ╰══✨• ೋ•✨๑๑💗๑๑✨•ೋ •✨══╯
 `.trim(), 'Gata Dios', '🌀 MENÚ 🌀', `${usedPrefix}menu`, '🔆 MENÚ COMPLETO 🔆', `${usedPrefix}menucompleto`, '♨️ MENÚ DE AUDIOS ♨️', `${usedPrefix}menuaudios`)
 }

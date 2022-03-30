c1b87585367e1ba070f15fb0aee64f0e407babbb
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-29 21:46:25 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/no chu_mp3.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/no chu.mp3'
+conn.sendFile(m.chat, vn, 'no chu.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /No chupa la|No chupala|no chupala/ 
+handler.command = new RegExp
+module.exports = handler

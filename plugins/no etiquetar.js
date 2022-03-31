73d8fcb95d80d62f7404c18a266c064bd501dc20
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-30 21:07:32 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/no etiquetar.js
+++ plugins/no etiquetar.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-*❰ ❗️ ❱ No etiquetes a mí creadora, si es algo urgente ve al chat privado*
+*❰ ❗️ ❱ No etiquetes a mí creadora, Si es algo urgente ve al chat privado.*
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /@5939685853830/i
+handler.customPrefix = /@593968585383/i
 handler.command = new RegExp
 
 module.exports = handler

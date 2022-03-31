17709d35bfad2e05be061f3c18f616ec5dbd2537
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-30 19:00:27 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/banuser.js
+++ plugins/banuser.js
@@ -11,6 +11,16 @@ let handler = async (m, { conn, text}) => {
 handler.help = ['banuser']
 handler.tags = ['General']
 handler.command = /^banuser|banusuario|nobot|sinbot$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = true
+handler.private = false
+
 handler.admin = true
+handler.botAdmin = true
+
+handler.fail = null
+handler.limit = false
 
 module.exports = handler

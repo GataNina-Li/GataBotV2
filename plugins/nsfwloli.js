368370b0364585054e6a72d6b5cb6533cba90b8b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-31 15:07:10 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/nsfwloli.js
+++ plugins/nsfwloli.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
-    heum = await fetch(`https://server-api-rey.herokuapp.com/api/wallpaper/nsfwloli?apikey=apirey`)
+    heum = await fetch(`https://api.xteam.xyz/randomimage/nsfwneko?APIKEY=29d4b59a4aa687ca`)
     json = await heum.buffer()
    conn.sendButtonImg(m.chat, json, '*¡Disfrutalo!*', 'Gata Dios', 'SIGUIENTE 🔄', `${usedPrefix + command}`, m, false)
 }

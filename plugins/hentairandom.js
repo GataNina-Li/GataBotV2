a63f3beaeefe716fa2c4f9318ac5616b357ff48c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-31 14:38:39 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/hentairandom.js
+++ plugins/hentairandom.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
-    heum = await fetch(`https://melcanz.com/nsfwhentai?apikey=apikey`)
+    heum = await fetch(`https://api.xteam.xyz/randomimage/hentai?APIKEY=APIKEY`)
     json = await heum.buffer()
    conn.sendButtonImg(m.chat, json, '*Hentai 🥵*', 'Gata Dios', 'SIGUIENTE 🔄', `${usedPrefix + command}`, m, false)
 }

0eeba63dc44308c5d62c62aecd32701567b345e8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-30 21:03:30 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/unbanuser.js
+++ plugins/unbanuser.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, text}) => {
     if (!who) throw '*Etiquete a una persona con el @*'
     let users = global.DATABASE._data.users
     users[who].banned = false
-    conn.reply(m.chat, `*✅ Listo, usuario desbaneado*\n\nYa puede usar a GataBot`, m)
+    conn.reply(m.chat, `*✅ Listo, usuario desbaneado*\n\n*Ya puede usar a GataBot!*`, m)
 }
 handler.help = ['ban']
 handler.tags = ['owner']

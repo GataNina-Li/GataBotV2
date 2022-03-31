6a0bc36aa0e18fe66c0336ee51ca87159b2ed404
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-30 19:10:51 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/unbanuser.js
+++ plugins/unbanuser.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, text}) => {
 }
 handler.help = ['ban']
 handler.tags = ['owner']
-handler.command = /^unbanuser|unbanusuario|conbot|sibot|$/i
+handler.command = /^unbanuser|unbanusuario|conbot|sibot$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false

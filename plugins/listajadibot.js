a7e37f8ffd4aa0fb7067d4d51c22abddde2a98b0
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-03-31 22:48:43 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/listajadibot.js
+++ plugins/listajadibot.js
@@ -1,6 +1,6 @@
 async function handler(m, { usedPrefix }) {
   let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
-  m.reply( '*🤖 Lista de algunos sub bots (jadibot/serbot) 🤖️*\n\n*🔰 Puedes contactarlos para ver si se unen a tu grupo*\n\n*Considera por favor:*\n*✅ Ser amable*\n*✅ No insistir ni discutir*\n\n*✳ ️Si le aparece el siguiente texto en blanco es que no hay ningún sub bot disponible en este momento inténtelo mas tarde*\n\n✅ *Si quieres Ser Bot escribe #jadibot o #serbot*\n\n*❰ ❗️ ❱ _NOTA: ️SON PERSONAS QUE NO CONOCEMOS. EL EQUIPO DE GATA DIOS NO SE HACE RESPONSABLE DE LO QUE PUEDA OCURRIR._*')
+  m.reply( '*🚀 Lista de Sub bots por GataBot 🚀*\n\n*😸 Puedes contactarlos para solicitar que se unen a tu grupo*\n\n*Considera por favor:*\n*✅ Ser amable, no insistir ni discutir*\n\n*✳ ️Si el siguiente texto esta vacio es que no hay ningún Sub bot disponible en este momento, inténtelo mas tarde*\n\n✅ *Si quieres Ser Bot escribe #jadibot o #serbot*\n\n*❰ ❗️ ❱ _NOTA: ️SON PERSONAS QUE NO CONOCEMOS. EL EQUIPO DE GATA DIOS NO SE HACE RESPONSABLE DE LO QUE PUEDA OCURRIR._*')
   m.reply(users.map(v => '🐈 Wa.me/' + v.jid.replace(/[^0-9]/g, '') + ` ⬅️ escriba: ${usedPrefix}estado \n(${v.name})\n`).join('\n'))
 }
 handler.command = handler.help = ['listjadibot','bots','subsbots']

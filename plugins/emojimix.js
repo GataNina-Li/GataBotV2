fbe5f68b1d6475f5905d21edf1b080ad427941bd
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-06 17:06:48 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/emojimix.js
+++ plugins/emojimix.js
@@ -4,7 +4,7 @@ let fetch = require('node-fetch')
 let fs = require("fs")
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-if (!args[0]) throw `*Ingrese dos emojis*\n\n- Ejemplo: ${usedPrefix + command} 🐱+👻`
+if (!args[0]) throw `*Ingrese dos emojis y en medio el signo +*\n\n *Ejemplo:* ${usedPrefix + command} 🐱+👻`
 let [emoji1, emoji2] = text.split`+`
 let anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)).json()
 let emix = anu.results[0].media_formats.png_transparent.url

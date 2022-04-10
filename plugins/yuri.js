8f969b24c8c03e2493ef03aee546c9776577d891
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-10 14:52:56 -0500

    Update yuri.js

--- plugins/yuri.js
+++ plugins/yuri.js
@@ -2,8 +2,10 @@ const axios = require('axios')
  let handler = async(m, { conn }) => {
    //let chat = global.DATABASE._data.chats[m.chat]
   if (!(m.chat in global.DATABASE._data.chats))
-  let chat = (global.DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw 'Escriba *!enable* *nsfw* para activar esta característica'
+  let chat = global.DATABASE._data.chats[m.chat] throw 'Escriba *!enable* *nsfw* para activar esta característica'
   //if (!chat.data.chats[m.chat].nsfw && m.isGroup) throw 'Escriba *!enable* *nsfw* para activar esta característica'
+  chat.nsfw && m.isGroup = true
+  
 let les = await axios.get('https://meme-api.herokuapp.com/gimme/yuri')
             conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
   }

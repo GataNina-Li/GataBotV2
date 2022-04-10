da7e9b5428a3119e3422b487c4db3ecc91d8fe7e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-10 13:47:01 -0500

    Update yuri.js

--- plugins/yuri.js
+++ plugins/yuri.js
@@ -5,7 +5,7 @@ let les = await axios.get('https://meme-api.herokuapp.com/gimme/yuri')
             conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
   }
 handler.help = ['yuri']
-handler.tags = ['General']
+handler.tags = ['nsfw']
 handler.command = /^(yuri)$/i 
 handler.owner = false
 handler.mods = false

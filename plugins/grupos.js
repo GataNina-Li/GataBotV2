4837ca62335866563fe374162fc76a426e8a1be0
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-09 22:32:19 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -1,13 +1,10 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
 *_➡️ GRUPO(S) OFICIAL DE GATABOT_*
-✅ https://chat.whatsapp.com/Eg7m7mmb85IDLnSgFooDg6
+✅ https://chat.whatsapp.com/Hahc7UwSouH9jIDStkT5QW
 
 *_➡️ GRUPO(S)_*
 ✅ https://chat.whatsapp.com/BlasuG7z02d16wEaCf61pa
-
-*_➡️ GRUPO(S) DE COLABORACIÓN:_*
-✅ https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ
 `.trim() 
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈', 'status@broadcast')

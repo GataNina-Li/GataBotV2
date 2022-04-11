1ab14151655c60aa255943a42acc70ba16c318fb
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-11 02:06:25 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/x18-muslos.js
+++ plugins/x18-muslos.js
@@ -1,6 +1,7 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn }) => {
+if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'	
 	let url = muslos[Math.floor(Math.random() * muslos.length)]
 	await conn.sendMessage(m.chat, {
 		contentText: '❤️‍🔥❤️‍🔥❤️‍🔥',

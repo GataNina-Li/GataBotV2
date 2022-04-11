ddf384bf6832ac621716952f00fce27ab8bc8af2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-10 21:39:27 -0500

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -139,7 +139,7 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
         if (!m.isGroup) {
           if (!isOwner) {
             global.dfail('group', m, conn)
-            throw false
+            throw true
           }
         } else if (!(isAdmin || isOwner)) {
           global.dfail('admin', m, conn)

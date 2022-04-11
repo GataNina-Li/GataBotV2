b48004e9ffb5aff1b628a35e6b1ed7fa0d16a706
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-11 00:04:23 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/x18-video.js
+++ plugins/x18-video.js
@@ -1,12 +1,13 @@
 let util = require('util')
 let path = require('path')
 let fs = require('fs')
-if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'
+
 let caption = `
 🥵🔥🔥🔥🔥
 `.trim()
 
 let handler = async (m, { conn }) => {
+if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'
 let video = fs.readFileSync(`./videos/${pickRandom(['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41'])}.mp4`)
 
      conn.sendFile(m.chat, video, '1.mp4', caption, m)

d3bc5049ad67ab4cd58372e0451b021cc20cf990
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-10 23:11:55 -0500

    Update menu-nuevo.js

--- plugins/menu-nuevo.js
+++ plugins/menu-nuevo.js
@@ -8,7 +8,7 @@ let fetch = require('node-fetch')
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 let username = conn.getName(who)
-let pp = 'https://i.imgur.com/8fK4h6F.jpg'
+let kai = 'https://i.imgur.com/8fK4h6F.jpg'
 let vn = './media/menugatabot.mp3'
 let menu =`
 ╭━〘 🐈⚡️🐈⚡️🐈⚡️🐈⚡️🐈 〙━╮
@@ -60,7 +60,7 @@ let menu =`
  ‖
 ╰━〘 🐈🌟🐈🌟🐈🌟🐈🌟🐈 〙━╯
 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨`.trim()
-conn.send3ButtonLoc(m.chat, (await fetch(pp)).buffer(), `
+conn.send3ButtonLoc(m.chat, (await fetch(kai)).buffer(), `
 ⁖ᯓ፨҈༺ 𝙂𝙖𝙩𝙖𝘽𝙤𝙩 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 ༻፨҈ᯓ⁖
 `.trim(), menu, 'Ⓜ️ 𝙈𝙀𝙉𝙐 𝘾𝙊𝙈𝙋𝙇𝙀𝙏𝙊 Ⓜ️', `#mcompleto`, '🔖 𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝘾𝙄𝙊𝙉 𝘿𝙀𝙇 𝙈𝙀𝙉𝙐 🔖', `#menuinfobot`, '🎮 𝙈𝙀𝙉𝙐 𝘿𝙀 𝙅𝙐𝙀𝙂𝙊𝙎 🎮', `#menujuego`, m, false, { contextInfo: { mentionedJid }})
  await await await await await await conn.sendFile(m.chat, vn, 'menugatabot.mp3', null, m, true, {

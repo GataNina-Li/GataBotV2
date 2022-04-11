07ea56a991ee6d991c2f71a73b084252748b3eb7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-11 02:38:59 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/x18-random-orgy.js
+++ plugins/x18-random-orgy.js
@@ -1,9 +1,23 @@
 let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
+     if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'     
     heum = await fetch(`https://api.xteam.xyz/randomimage/orgy?APIKEY=29d4b59a4aa687ca`)
     json = await heum.buffer()
    conn.sendButtonImg(m.chat, json, '*¡Disfrutalo!*', 'Gata Dios', 'SIGUIENTE 🔄', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(orgy|orgi|nsfworgy|orgia|orgía)$/i 
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.register = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 200
+handler.limit = false
 
 module.exports = handler

716036533e50049b3cc9ef0d4b6dc0110af057c9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-11 02:00:13 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/x18-pack2.js
+++ plugins/x18-pack2.js
@@ -1,6 +1,7 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn }) => {
+if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'	
 	let url = packgirl[Math.floor(Math.random() * packgirl.length)]
 	await conn.sendMessage(m.chat, {
 		contentText: '*Pack xxx By TheBrokers*',

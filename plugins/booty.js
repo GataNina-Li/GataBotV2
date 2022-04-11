e79f374648f2dfcc52d207a5f5a3adc56269f653
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-11 02:21:29 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/booty.js
+++ plugins/booty.js
@@ -1,8 +1,22 @@
 const axios = require('axios')
  let handler = async(m, { conn }) => {
+ if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'
 let les = await axios.get('https://meme-api.herokuapp.com/gimme/booty')
             conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
   }
 handler.command = /^(booty|culo|culos|culo2|culos2)$/i 
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.register = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 200
+handler.limit = false
 
 module.exports = handler

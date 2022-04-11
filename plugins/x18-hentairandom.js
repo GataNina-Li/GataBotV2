761817dd40a6a1d5739c1f451a01859a47edcc20
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-11 01:06:39 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/x18-hentairandom.js
+++ plugins/x18-hentairandom.js
@@ -1,5 +1,6 @@
 let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
+     if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'     
     heum = await fetch(`https://api.xteam.xyz/randomimage/hentai?APIKEY=29d4b59a4aa687ca`)
     json = await heum.buffer()
    conn.sendButtonImg(m.chat, json, '*Hentai 🥵*', 'Gata Dios', 'SIGUIENTE 🔄', `${usedPrefix + command}`, m, false)

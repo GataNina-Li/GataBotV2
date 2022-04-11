9f24d34ca2aaccd2fd6f57345a4f03ffc9418b79
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-10 23:35:07 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/yuri.js
+++ plugins/yuri.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
  let handler = async(m, { conn }) => {
-if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw 'Feature Nsfw Disable\nType *!enable* *nsfw* to activate this feature'
+if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '*Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'
  let les = await axios.get('https://meme-api.herokuapp.com/gimme/yuri')
             conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
   }

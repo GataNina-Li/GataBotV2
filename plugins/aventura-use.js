13f52655cd280268abf8a1e5385e60122e7f8d71
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-12 17:40:24 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/aventura-use.js
+++ plugins/aventura-use.js
@@ -1,3 +1,4 @@
+/*
 let { MessageType } = require('@adiwajshing/baileys')
 let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) =>  {
     let msgerror = (pickRandom(['Error', 'astagfirullah error', 'Nice Error', 'Salah format keknya :v', 'error bro', 'Kocak error :v', 'wtf error :v', 'Ciaaa error', 'error cuyy', 'dahlah (emot batu) error']))
@@ -74,3 +75,4 @@ module.exports = handler
 function pickRandom(list) {
     return list[Math.floor(Math.random() * list.length)]
 }
+*/

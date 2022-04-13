7629bf832672838bec44d8e67fe594a4f996f738
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-13 14:49:16 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/facebook.js
+++ plugins/facebook.js
@@ -1,17 +1,19 @@
-let scraper = require('@bochilteam/scraper')
-
+const { facebook } = require('../lib/facebook')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://www.facebook.com/alanwalkermusic/videos/277641643524720`
-  await m.reply('Loading...')
-  let res = await scraper.facebookdl(args[0])
-  let data = res.result.filter(v => /true/.test(v.isVideo) && /mp4/.test(v.ext))
-  await conn.sendFile(m.chat, data[1].url, '', `HD: ${data[0].url}`, m)
+  if (!args[0].match(/https:\/\/.*(facebook.com|fb.watch)/gi)) throw `url salah`
+  facebook(args[0]).then(async res => {
+    let fb = JSON.stringify(res)
+    let json = JSON.parse(fb)
+    // m.reply(require('util').format(json))
+    if (!json.status) throw json
+    await m.reply(wait)
+    await conn.sendFile(m.chat, json.data[0].url, '', '©LynXzy💌', m)
+  }).catch(_ => _)
 }
 handler.help = ['fb'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
 handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
-
-handler.limit = true
 
 module.exports = handler

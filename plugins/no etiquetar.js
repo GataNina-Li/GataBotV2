307e69ac95cf361fdfe5bf70fe3fabe087fa3a16
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-15 16:54:14 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/no etiquetar.js
+++ plugins/no etiquetar.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text }) => {
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /@593968585383/i
+handler.customPrefix = /@5939685853835/i
 handler.command = new RegExp
 
 module.exports = handler

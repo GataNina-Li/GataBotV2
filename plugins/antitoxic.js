5abfec17ba6aaca2e5bc86b3135c7c9d02ef77a1
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-15 15:27:23 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/antitoxic.js
+++ plugins/antitoxic.js
@@ -3,7 +3,7 @@ let handler = m => m
 let linkRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole/i // tambahin sendiri
 
 handler.before = function (m, { isOwner, isBotAdmin }) {
-    if (m.isBaileys && m.fromMe) return true
+    if (m.isBaileys && m.fromMe) return !0
     let chat = global.DATABASE._data.chats[m.chat]
     let user = global.DATABASE._data.users[m.sender]
     let isGroupToxic = linkRegex.exec(m.text)
@@ -23,6 +23,6 @@ handler.before = function (m, { isOwner, isBotAdmin }) {
             }
         }
     }
-    return true
+    return !0
 }
 module.exports = handler

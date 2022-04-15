5cc6d16e902a736638aa0781297668c7e6b9b4dc
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-15 15:47:52 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/antitoxic.js
+++ plugins/antitoxic.js
@@ -1,3 +1,4 @@
+let = require('@adiwajshing/baileys')
 let handler = m => m
 
 let linkRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole/i // tambahin sendiri

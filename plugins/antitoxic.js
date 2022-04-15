c86f5bcf54d4bfcde46c188161d3c18ca6f8e166
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-04-15 14:25:45 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/antitoxic.js
+++ plugins/antitoxic.js
@@ -1,23 +1,33 @@
+let { GroupSettingChange } = require('@adiwajshing/baileys')
 let handler = m => m
 
-let linkRegex = /(a(su|nj(([ie])ng|([ie])r)?)|me?me?k|ko?nto?l|ba?bi|fu?ck|ta(e|i)k|bangsat|g([iueo])bl([iueo])(k|g)|g ([iueo]) b l ([iueo]) (k|g)|a (n j (i n g|i r)?)s u|col(i|ay)|an?jg|b([ia])ngs([ia])?t|t([iuo])l([iuo])l)/i
-handler.before = function (m, { user }) {
-  if (m.isBaileys && m.fromMe) return true
-  if (/masuk|lanjutkan|banjir|(per)?panjang/g.exec(m.text)) return true
-  let chat = global.DATABASE.data.chats[m.chat]
-  let isGroupToxic = linkRegex.exec(m.text)
+let antitoxicRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole/i // tambahin sendiri
 
-  if (chat.antiToxic && isGroupToxic) {
-    m.reply('*¡No seas tóxico/a!*\n' + readMore + '\n*Desactiva escribiendo #off antitoxic*')
-    if (global.opts['restrict']) {
-      // if (!user.isAdmin) return true
-      // this.groupRemove(m.chat, [m.sender])
+handler.before = function (m, { isOwner, isBotAdmin }) {
+    if (m.isBaileys && m.fromMe) return !0
+    let chat = global.DATABASE._data.chats[m.chat]
+    let user = global.DATABASE._data.users[m.sender]
+    let isGroupToxic = antitoxicRegex.exec(m.text)
+
+    if (!chat.antiToxic && !chat.isBanned && isGroupToxic) {
+        user.warn += 1
+        this.send2Button(m.chat, `*Malas palabras detectadas!*
+Warning: ${user.warn} / 5
+Si la advertencia llega a 5 serás baneado
+
+ketik *#on antitoxic* para activar antitoxic
+ketik *#astagfirullah* atau *#maaf* untuk mengurangi warning
+
+“gadrdfgsgfsh`, 'Antibadword', 'Nyalakan Antibadword', '.on antibadword', 'Astaghfirullah', '#maaf', m)
+        if (user.warn >= 5) {
+            user.banned = true
+            if (m.isGroup) {
+                if (isBotAdmin) {
+                    this.groupSettingChange(m.chat, GroupSettingChange.messageSend, true)
+                }
+            }
+        }
     }
-  }
-  return true
+    return !0
 }
-
 module.exports = handler
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)

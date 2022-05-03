993dd5a384c804e36cb098e349c6986ffabf77f3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-03 00:59:25 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/so-Todo bien_mp3.js
@@ -0,0 +1,16 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/Todo bien.mp3'
+conn.sendFile(m.chat, vn, 'Todo bien.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /Todo bien|Todo bien 🧐/i 
+handler.command = new RegExp
+
+handler.fail = null
+handler.exp = 100
+module.exports = handler

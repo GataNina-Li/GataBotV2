c68e93c1949e2012443300d552242cc111195b8b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-03 03:09:55 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/so-A bueno adios master_mp3.js
@@ -0,0 +1,16 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/A bueno adios master.mp3'
+conn.sendFile(m.chat, vn, 'A bueno adios master.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /A bueno adios master|A bueno adiós master|🫂/i 
+handler.command = new RegExp
+
+handler.fail = null
+handler.exp = 100
+module.exports = handler

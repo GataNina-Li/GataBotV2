c23427998c5a2e53d8a35a801745c0b1549ba7e4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-03 02:43:54 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/so-Corte Corte_mp3.js
@@ -0,0 +1,16 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/Corte Corte.mp3'
+conn.sendFile(m.chat, vn, 'Corte Corte.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /Corte Corte/i 
+handler.command = new RegExp
+
+handler.fail = null
+handler.exp = 100
+module.exports = handler

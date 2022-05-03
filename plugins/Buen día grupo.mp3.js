7cdbba165843680cb5118493daf81f4c0aeadbd4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-03 04:38:37 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/Buen día grupo.mp3.js
@@ -0,0 +1,16 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/Buen día grupo.mp3'
+conn.sendFile(m.chat, vn, 'Buen día grupo.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /Buen día grupo.mp3|Buen dia grupo.mp3|🙌/i 
+handler.command = new RegExp
+
+handler.fail = null
+handler.exp = 100
+module.exports = handler

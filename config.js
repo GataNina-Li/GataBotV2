2e0bf6348d624a312388194345bf99daf17dafa9
sosa091 <105473688+sosa091@users.noreply.github.com>
2022-06-05 08:48:53 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 kperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ' , 'https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs' , 'https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F' , 'https://chat.whatsapp.com/FwEUGxkvZD85fIIp0gKyFC' , 'https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb'] // No tiene utilidad 
 global.channelYT = ['https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'] // No tiene utilidad
-global.owner = ['5219531068159' , '5219531627349' , '5219531393404'] // Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
+global.owner = ['5219531393404' , '5219531627349' , '5219531068159'] // Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
 global.mods = ['5219984879204' , '5219532423626'] // No tiene utilidad
 global.prems = ['5217571021436' , '5219531393404'] // No tiene utilidad
 

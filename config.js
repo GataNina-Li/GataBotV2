ce532a5d03a7407a609794b202a8fe234fdbe924
lucasalchapar <54723464+lucasalchapar@users.noreply.github.com>
2022-10-17 05:51:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -49,8 +49,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'GataBot 🐈'
-global.author = 'Gata Dios'
+global.packname = 'naco'
+global.author = 'miaumiau'
 
 
 //global.wait = '*Esperé un momento..*'

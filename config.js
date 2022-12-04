3d7763f8e4d5d845ebe9f56810e4967452fd50a0
Jesús mod <119769438+19822002@users.noreply.github.com>
2022-12-03 23:20:43 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ' , 'https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs' , 'https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F' , 'https://chat.whatsapp.com/FwEUGxkvZD85fIIp0gKyFC' , 'https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb'] // No tiene utilidad 
 global.channelYT = ['https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'] // No tiene utilidad
-global.owner = ['593993684821' , '51993042301' , '529996125657'] // Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
+global.owner = ['593993684821' , '59898582429' , '59891070699'] // Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
 global.mods = ['5219992095479' , '972557048356'] // No tiene utilidad
 global.prems = ['5219996125657' , '5219991402134'] // No tiene utilidad
 
-// Nota: Puedes contactarme si necesitas ayuda con algo al +994407312387 (Solo temas de la instalación)
-// => Solo dudas sobre la instalación, temas generales contactarme al +593968585383 (Temas serios, si vas a intervenir al chat para molesrtar seras bloqueado/a) 
+// Nota: Puedes contactarme si necesitas ayuda con algo al +59898582429 (Solo temas de la instalación)
+// => Solo dudas sobre la instalación, temas generales contactarme al +59898582429 (Temas serios, si vas a intervenir al chat para molesrtar seras bloqueado/a) 
 // => NO ayudo a hacer Bots, ni crearlos, ni editarlos 
 // - Este es el Canal de The Shadow Brokers por si te interesa su canal de YouTube en https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA
 

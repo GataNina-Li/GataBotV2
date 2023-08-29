319f4f4887a5491f1c98b2ac93201a6672501d0c
PabloCesarbcrra11 <139091709+PabloCesarbcrra11@users.noreply.github.com>
2023-08-28 23:21:15 -0700

    Cesar Ventas

--- config.js
+++ config.js
@@ -1,14 +1,11 @@
-global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ' , 'https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs' , 'https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F' , 'https://chat.whatsapp.com/FwEUGxkvZD85fIIp0gKyFC' , 'https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb'] // No tiene utilidad 
-global.channelYT = ['https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'] // No tiene utilidad
-global.owner = ['593993684821' , '51993042301' , '529996125657'] // Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
-global.mods = ['5219992095479' , '972557048356'] // No tiene utilidad
-global.prems = ['5219996125657' , '5219991402134'] // No tiene utilidad
+global.DeveloperMode = 'false' //true 
+global.linkGC = [https://chat.whatsapp.com/CwBaAOVmpC34jzU2zMxVpC]
+global.owner = ['52 664 613 2112' , '52 664 588 8104'] //
 
-// Nota: Puedes contactarme si necesitas ayuda con algo al +994407312387 (Solo temas de la instalación)
+// Nota: Puedes contactarme si necesitas ayuda con algo al +52 664 588 8104 (Solo temas de la instalación)
 // => Solo dudas sobre la instalación, temas generales contactarme al +593968585383 (Temas serios, si vas a intervenir al chat para molesrtar seras bloqueado/a) 
 // => NO ayudo a hacer Bots, ni crearlos, ni editarlos 
-// - Este es el Canal de The Shadow Brokers por si te interesa su canal de YouTube en https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA
+
 
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -49,8 +46,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'GataBot 🐈'
-global.author = 'Gata Dios'
+global.packname = 'Cesar Ventas 💰'
+global.author = 'Cesar Creador'
 
 
 //global.wait = '*Esperé un momento..*'
